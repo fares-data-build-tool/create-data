9475fad8919a3422a0177cf1509d6c1f1123a959
Danny Davies <danny.davies@infinityworks.com>
2019-12-05 13:44:22 +0000

    Deleted .build and buckets directories and added these to .gitignore. Also removed comments in handler.ts

--- repos/fdbt-reference-data-service/data-aggregator/handler.ts
+++ repos/fdbt-reference-data-service/data-aggregator/handler.ts
@@ -1,9 +1,6 @@
 import { Handler } from "aws-lambda";
 
 export const s3hook: Handler = async (event: any) => {
-  console.log("THIS IS THE RESULT");
+  console.log("THIS IS THE RESULT OF THE EVENT");
   console.log(JSON.stringify(event));
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
