e5d4dd0d813763255947b8f3c7ba10c5de367b42
Rob Catton <robmcatton@gmail.com>
2019-12-06 16:25:50 +0000

    First unit test

--- repos/fdbt-reference-data-service/netex-convertor/handler.ts
+++ repos/fdbt-reference-data-service/netex-convertor/handler.ts
@@ -1,8 +1,6 @@
 import { Handler } from 'aws-lambda';
 
-export const s3hook: Handler = async (event: any) => {
-
+export const s3hook: Handler = (event: any) => {
   console.log("THIS IS MY RESULT ");
   console.log(JSON.stringify(event));
-
 };
