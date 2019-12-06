b7126e16b9a6d9f1ccbfc78ba09e5e3ae5893584
gilesdrinkall <giles.drinkall@infinityworks.com>
2019-12-06 16:43:12 +0000

    commented out lines removed from handler.ts

--- repos/fdbt-reference-data-service/ref-data-uploader/handler.ts
+++ repos/fdbt-reference-data-service/ref-data-uploader/handler.ts
@@ -4,12 +4,4 @@ export const s3hook: Handler = (event:any) => {
 
 console.log("==== This is my event object ====");
 console.log(JSON.stringify(event));
-
-// console.log("==== This is my context object ====");
-// console.log(JSON.stringify(context));
-
-// console.log("==== This is my process.env object ====");
-// console.log(JSON.stringify(process.env));
 };
-
-// export { s3hook};
