643c2fa7c4f2ce98ad3981d794945a9a144ada91
Adeolu Opeodu <ade.opeodu@infinityworks.com>
2019-12-18 15:14:33 +0000

    [TFN-85] - code to for forms how to submit, maintaining state in cookies and logically flow of screens

---
+++ fdbt-reference-data-service/site/pages/api/operator.ts
@@ -0,0 +1,34 @@
+import { OPERATOR_COOKIE } from '../../constants/index';
+import { NextApiRequest, NextApiResponse } from 'next';
+import v1 from 'uuid';
+import { operatorBusinessLogic } from './service/businessLogic';
+import { getDomain, setCookiOnResponseObject, getCookies } from './utils';
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+        console.log("**********************");
+        const cookies = getCookies(req);
+        console.log("=====");
+        const operatorCookie = cookies[OPERATOR_COOKIE]; 
+        if(operatorCookie){
+            res.writeHead(302, {
+                Location: '/service'
+              });
+        } else {
+            const operator = req.body.operator;
+            operatorBusinessLogic(operator);
+            const uuid = v1();
+            const cookieValue = JSON.stringify({operator, uuid})
+            const domain = getDomain(req); 
+            setCookiOnResponseObject(domain, OPERATOR_COOKIE, cookieValue, res);
+            res.writeHead(302, {
+                Location: '/service'
+            });
+        }
+    }catch (error) {
+        res.writeHead(302, {
+            Location: '/error'
+        });
+    }
+    res.end();
+}
