86fa60261d3e56c3637f76f8ce02782c9ee0c1eb
Rob Catton <robmcatton@gmail.com>
2019-12-23 16:12:47 +0000

    get cookie test completed

--- fdbt-reference-data-service/site/tests/api/utils/index.test.ts
+++ fdbt-reference-data-service/site/tests/api/utils/index.test.ts
@@ -9,16 +9,15 @@ describe("apiUtils", () => {
     describe("getCookies", () => {
         it("should return cookies when given a request", () => {
             const operatorCookieValue = "%7B%22operator%22%3A%22FirstBus%22%2C%22uuid%22%3A%22cbc0111a-e763-48e7-982b-ac25ecbe625c%22%7D"
-            const servcie
             const req = mockRequest({ 
                 body: {},
                 headers: {
                   host: "localhost",
-                  cookie: `${OPERATOR_COOKIE}=${cookieValue}; ${SERVICE_COOKIE}=%7B%22service%22%3A%22N1%22%2C%22uuid%22%3A%22d177b8a0-44ed-4e67-9fd0-2d581b5fa91a%22%7D`
+                  cookie: `${OPERATOR_COOKIE}=${operatorCookieValue}; ${SERVICE_COOKIE}=%7B%22service%22%3A%22N1%22%2C%22uuid%22%3A%22d177b8a0-44ed-4e67-9fd0-2d581b5fa91a%22%7D`
                 }
               });
             const result = getCookies(req);
-            expect(result[OPERATOR_COOKIE]).toEqual(cookieValue);
+            expect(result[OPERATOR_COOKIE]).toEqual(operatorCookieValue);
         });
     });
 })
