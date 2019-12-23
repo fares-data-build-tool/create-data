69e7701aa8691279d2bb0db531dcbf5825af88c0
Adeolu Opeodu <ade.opeodu@infinityworks.com>
2019-12-23 15:56:15 +0000

    [TFN-85] - added to deleteCookiesOnServerSide unit test to check that the set header gets called

--- repos/fdbt-reference-data-service/site/tests/utils/utils.test.ts
+++ repos/fdbt-reference-data-service/site/tests/utils/utils.test.ts
@@ -1,6 +1,8 @@
-import {getHost, deleteCookieOnServerSide } from "../../utils"; 
+import { getHost, deleteCookieOnServerSide } from "../../utils";
 import { IncomingMessage } from "http";
 import { NextPageContext } from "next";
+import { OPERATOR_COOKIE, SERVICE_COOKIE } from "../../constants";
+import { mockRequest, mockResponse } from "mock-req-res";
 
 var MockReq = require('mock-req');
 
@@ -31,16 +33,37 @@ describe("utils", () => {
     });
     describe("deleteCookiesOnServerSide", () => {
         it("should delete cookie", () => {
-            const props: NextPageContext = {
-                pathname: “”,
+            const req = mockRequest({
+                connection: {
+                    encrypted: false
+                },
+                body: {},
+                headers: {
+                    host: "localhost:5000",
+                    cookie: `${OPERATOR_COOKIE}=%7B%22operator%22%3A%22FirstBus%22%2C%22uuid%22%3A%22cbc0111a-e763-48e7-982b-ac25ecbe625c%22%7D; ${SERVICE_COOKIE}=%7B%22service%22%3A%22N1%22%2C%22uuid%22%3A%22d177b8a0-44ed-4e67-9fd0-2d581b5fa91a%22%7D`
+                }
+            });
+
+            const mockToAssessOn = jest.fn();
+            const res = mockResponse({
+                writeHead: jest.fn(),
+                cookies: {},
+                setHeader: {
+                    call: mockToAssessOn
+                },
+                set: undefined //important for mockToAssessOn to be used
+            });
+
+            const ctx: NextPageContext = {
+                pathname: "",
                 query: {},
                 req: req,
                 res: res,
                 AppTree: null
-              };
-              const cookieName = "test";
-            const result = deleteCookieOnServerSide(props, cookieName);  
-            const expected = 
-            expect(result).toEqual(expected);
+            };
+
+            deleteCookieOnServerSide(ctx, OPERATOR_COOKIE);
+            expect(mockToAssessOn).toBeCalled();
         });
-    }
+    });
+});
