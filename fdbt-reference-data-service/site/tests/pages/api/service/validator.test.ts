5693814c892e8063efca92d8d2e17d8d2d4163a1
gilesdrinkall <giles.drinkall@infinityworks.com>
2019-12-23 15:25:25 +0000

    added validator.test.ts

--- fdbt-reference-data-service/site/tests/pages/api/service/validator.test.ts
+++ fdbt-reference-data-service/site/tests/pages/api/service/validator.test.ts
@@ -1,16 +1,29 @@
 import validator from "../../../pages/api/service/";
+import { isSessionValid, isCookiesUUIDMatch } from "../../../../pages/api/service/validator";
 
-describe("uuid handler", () => {
-  it("should return a uuid", () => {
-    const req = null;
-    const val = {
-        json: jest.fn()
-    }
-    const res : any  = {
-        status: jest.fn().mockReturnValue(val)
-    };
-    uuidHandler(req , res);
-    expect(res.status).toHaveBeenCalled();
-    expect(val.json).toHaveBeenCalled();
+describe("isSessionvalid", () => {
+  it("should return a true when there is an operator cookie", () => {
+    const props: NextPageContext = {}
+    const expected = true;
+    const result = isSessionValid(req);
+    expect(result).toEqual(expected);
+  });
+  it("should return false when there is no operator cookie", () => {
+    const expected = false;
+    const result = isSessionValid(req);
+    expect(result).toEqual(expected);
+  });
+});
+
+describe("isCookiesUUIDMatch", () => {
+  it("should return a true if uuids match", () => {
+    const expected = true;
+    const result = isCookiesUUIDMatch(req);
+    expect(result).toEqual(expected);
+  });
+  it("should return false id uuids do  not match", () => {
+    const expected = false;
+    const result = isCookiesUUIDMatch(req);
+    expect(result).toEqual(expected);
   });
 });
