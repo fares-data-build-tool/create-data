fdc6ea2c24b9b7ba18a09e13f41c356532d0f6f9
Adeolu Opeodu <ade.opeodu@infinityworks.com>
2019-12-27 12:24:24 +0000

    [TFN-85] - moving apiUtils to correct location, and cleaning up unused []

--- repos/fdbt-reference-data-service/site/tests/pages/api/service.test.ts
+++ repos/fdbt-reference-data-service/site/tests/pages/api/service.test.ts
@@ -16,8 +16,6 @@ describe("service", () => {
       },
       body: {},
       headers: {
-        "set-cookie": [],
-        "Set-cookie": [],
         host: "localhost:5000",
         cookie: `${OPERATOR_COOKIE}=%7B%22operator%22%3A%22FirstBus%22%2C%22uuid%22%3A%22cbc0111a-e763-48e7-982b-ac25ecbe625c%22%7D; ${SERVICE_COOKIE}=%7B%22service%22%3A%22N1%22%2C%22uuid%22%3A%22d177b8a0-44ed-4e67-9fd0-2d581b5fa91a%22%7D`
       }
