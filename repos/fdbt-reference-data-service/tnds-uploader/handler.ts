d940b1f55e7e3b669d989da98da3af271cd1850f
Rob Catton <robmcatton@gmail.com>
2020-01-10 14:54:56 +0000

    Refactored to remove express, as it wasnt needed, and move some stuff into functions

--- repos/fdbt-reference-data-service/tnds-uploader/handler.ts
+++ repos/fdbt-reference-data-service/tnds-uploader/handler.ts
@@ -1,106 +1,91 @@
-import bodyParser from 'body-parser';
 import { Handler } from 'aws-lambda';
 import AWS from 'aws-sdk';
-import express from 'express';
 
-// this is the code that gets hit when the s3 event (object created) happens
-export const s3hook: Handler = async (event: any) => {
-
+async function fetchDataFromS3AsString(bucketName: string, eventKey: string): Promise<string> {
     const s3 = new AWS.S3();
-    const tndsCsvTableName = "";
-    const tndsXmlTableName = "";
-    const eventKey = event.Records[0].s3.object.key;
-    const bucketName = event.Records[0].s3.bucket.name;
-
-    // HEAD the bucket and see if it exists and we have permissions to access
-    try {
-        const head = await s3.headObject({
-            Bucket: bucketName,
-            Key: eventKey
-        }).promise();
-
-    } catch (err) {
-        return {
-            statusCode: err.statusCode || 400,
-            body: err.message || JSON.stringify(err.message)
-        }
-    }
-
     try {
         const data = await s3.getObject({
             Bucket: bucketName,
             Key: eventKey
         }).promise();
 
-        console.log(JSON.stringify(data));
+        const dataAsString = data.Body?.toString('utf-8')!;
+
+        console.log(dataAsString);
+
+        return dataAsString;
 
     } catch (err) {
-        return {
-            statusCode: err.statusCode || 400,
-            body: err.message || JSON.stringify(err.message)
-        }
+        throw new Error("Internal error - omething went wrong with the data collection.");
     }
+}
 
-    const filename = JSON.stringify(eventKey);
-    const fileExtension = filename.split('.').pop();
-
+function fileExtensionCheck(fileExtension: string, xmlTableName: string, csvTableName: string) {
     let table = "";
 
     if (fileExtension === "csv") {
 
-        table = tndsCsvTableName;
+        return table = csvTableName;
 
     } else if (fileExtension === "xml") {
 
-        table = tndsXmlTableName;
+        return table = xmlTableName;
 
     } else {
-        console.log("File is not of a supported format type.")
-    }
-
-    if (table === tndsCsvTableName) {
-
-        const { TNDS_CSV_TABLE, IS_OFFLINE } = process.env;
-        const app = express();
-
-        const dynamoDb = (IS_OFFLINE === 'true') ?
-            new AWS.DynamoDB.DocumentClient({
-                region: 'localhost',
-                endpoint: '',
-            }) :
-            new AWS.DynamoDB.DocumentClient();
-
-        app.use(bodyParser.json({ strict: false }));
-
-        app.put('/' + tndsCsvTableName, (req, res) => {
-
-            const { tndsCsvId, Columns } = req.body;
-
-            const params = {
-                TableName: tndsCsvTableName,
-                Key: eventKey,
-                UpdateExpression: 'DYNAMO-DB QUERY',
-                ExpressionAttributeNames: { 'DYNAMO-DB QUERY VALUES ATTRIBUTES': 'values' },
-                ExpressionAttributeValues: { 'DYNAMO-DB QUERY VALUES': 'values' },
-            };
-
-            dynamoDb.update(params, (error) => {
-                if (error) {
-                    console.log(`Error updating table with id ${tndsCsvId}: `, error);
-                    res.status(400).json({ error: 'Could not update table' });
-                }
-
-                res.json({ tndsCsvId, Columns });
-
-            })
-
-        });
-    } else if (table === tndsXmlTableName) {
-
-        //same as above but xml version
-
-        return null;
-
+        console.error(`File is not of a supported format type (${fileExtension})`);
+        throw new Error(`Unsupported file type ${fileExtension}`);
     }
 }
 
+function xmlToJsonParse(xmlData: string){
+    let xml = xmlData;
+    let json;
+
+    return json;
+}
+
+function csvToJsonParse(csvData: string){
+    let csv = csvData;
+    let json;
+
+    return json;
+}
+
+export const s3hook: Handler = async (event: any) => {
+
+    // Could pass in dynamodb connection string here (localhost / )
+    const { TNDS_CSV_TABLE = "", TNDS_XML_TABLE = "", IS_OFFLINE = "" } = process.env;
+    const eventKey = event.Records[0].s3.object.key;
+    const bucketName = event.Records[0].s3.bucket.name;
+    let parsedS3Data = null;
+
+    const filePath = JSON.stringify(eventKey);
+    const fileExtension = filePath.split('.').pop()!; // ! is used here as without it, the IDE warns that it might be undefined.
+
+    const stringifiedS3Data = await fetchDataFromS3AsString(bucketName, filePath);
+
+    const table = fileExtensionCheck(fileExtension, TNDS_XML_TABLE, TNDS_CSV_TABLE);
+
+    if(table===TNDS_XML_TABLE){
+        parsedS3Data = xmlToJsonParse(stringifiedS3Data);
+    } else if(table===TNDS_CSV_TABLE){
+        parsedS3Data = csvToJsonParse(stringifiedS3Data);
+    }
+
+    const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+    const params: AWS.DynamoDB.DocumentClient.PutItemInput = {
+        TableName: table,
+        Key: eventKey,
+        UpdateExpression: 'DYNAMO-DB QUERY',
+        ExpressionAttributeNames: { 'DYNAMO-DB QUERY VALUES ATTRIBUTES': 'values' },
+        ExpressionAttributeValues: { 'DYNAMO-DB QUERY VALUES': 'values' }
+        
+    };
+
+    const result = await dynamoDb.update(params, (error) => {
+        if (error) {
+            console.log(`Error updating table ${table}`, error);
+        }
+    }).promise();
+}
