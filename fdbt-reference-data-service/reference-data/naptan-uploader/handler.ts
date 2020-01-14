c2765e0d2804e9fa8f5b9d4fa184895554c90cb0
Danny Davies <danny.davies@infinityworks.com>
2020-01-14 17:06:11 +0000

    Finished naptan-uploader lambda handler using csvtojson blog

--- fdbt-reference-data-service/reference-data/naptan-uploader/handler.ts
+++ fdbt-reference-data-service/reference-data/naptan-uploader/handler.ts
@@ -38,39 +38,39 @@ interface dynamoDBParamsToPush {
 const tableName = "Stops";
 
 export const s3hook: Handler = (event:any, context: Context, callback: Callback) => {
-
     const bucketName: string = event.Records[0].s3.bucket.name;
-    const eventKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " ")); // Object key may have spaces or unicode non-ASCII characters
-    const filepath: string = JSON.stringify(eventKey);
+    const filepath = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " ")); // Object key may have spaces or unicode non-ASCII characters
 
     const params: s3GetObjectParameters = {Bucket: bucketName, Key: filepath};
 
     const s3 = new AWS.S3();
+    console.log("s3 set as: ", s3)
     const s3Stream = s3.getObject(params).createReadStream()
+    console.log("s3stream set as: ", s3Stream)
     csvtojson().fromStream(s3Stream).on('data', (row) => {
         let jsonContent = JSON.parse(row);
         console.log(JSON.stringify(jsonContent));
 
         let data: dynamoDBData = {
-            ATCOCode: jsonContent.ATCOCode,
-            NaptanCode: jsonContent.NaptanCode,
-            CommonName: jsonContent.CommonName,
-            Street: jsonContent.Street,
-            Indicator: jsonContent.Indicator,
-            IndicatorLang: jsonContent.IndicatorLang,
-            Bearing: jsonContent.Bearing,
-            NptgLocalityCode: jsonContent.NptgLocalityCode,
-            LocalityName: jsonContent.LocalityName,
-            ParentLocalityName: jsonContent.ParentLocalityName,
-            LocalityCentre: jsonContent.LocalityCentre,
-            GridType: jsonContent.GridType,
-            Easting: jsonContent.Easting,
-            Northing: jsonContent.Northing,
-            Longitude: jsonContent.Longitude,
-            Latitude: jsonContent.Latitude,
-            StopType: jsonContent.StopType,
-            BusStopType: jsonContent.BusStopType,
-            TimingStatus: jsonContent.TimingStatus
+            "ATCOCode": jsonContent.ATCOCode,
+            "NaptanCode": jsonContent.NaptanCode,
+            "CommonName": jsonContent.CommonName,
+            "Street": jsonContent.Street,
+            "Indicator": jsonContent.Indicator,
+            "IndicatorLang": jsonContent.IndicatorLang,
+            "Bearing": jsonContent.Bearing,
+            "NptgLocalityCode": jsonContent.NptgLocalityCode,
+            "LocalityName": jsonContent.LocalityName,
+            "ParentLocalityName": jsonContent.ParentLocalityName,
+            "LocalityCentre": jsonContent.LocalityCentre,
+            "GridType": jsonContent.GridType,
+            "Easting": jsonContent.Easting,
+            "Northing": jsonContent.Northing,
+            "Longitude": jsonContent.Longitude,
+            "Latitude": jsonContent.Latitude,
+            "StopType": jsonContent.StopType,
+            "BusStopType": jsonContent.BusStopType,
+            "TimingStatus": jsonContent.TimingStatus
         };
         let paramsToPush = {
             TableName: tableName,
