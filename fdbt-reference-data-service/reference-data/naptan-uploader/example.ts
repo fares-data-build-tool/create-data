7bfcaa80659977e076860faaf13a0a691b0fcdc6
Danny Davies <danny.davies@infinityworks.com>
2020-01-17 12:23:38 +0000

    Adapted 'run-local.ts' file to be generic for streaming to command line

--- fdbt-reference-data-service/reference-data/naptan-uploader/example.ts
+++
@@ -1,7 +0,0 @@
-import { WriteRequest } from "aws-sdk/clients/dynamodb";
-
-const parsedDataMapper = (parsedDataItem: any): WriteRequest => ({
-  PutRequest: { Item: parsedDataItem as any }
-});
-
-console.log(parsedDataMapper({ foo: "bu" }));
--- fdbt-reference-data-service/reference-data/naptan-uploader/run-local.ts
+++ fdbt-reference-data-service/reference-data/naptan-uploader/run-local.ts
@@ -2,8 +2,16 @@ import { csvParser, pushToDynamo } from "./handler";
 import fs from "fs";
 import path from "path";
 
-const main = async () => {
-  const data = fs.readFileSync(path.join(__dirname, "./data.csv"));
+/* 
+This file can be used to check the csv parsing and pushToDynamo functionality.
+It will run the code locally and not actually import data from S3 or push to DynamoDB.
+To run the below, make sure you install 'ts-node'. You can then navigate to the directory
+containing the run-local.ts file and run the command 'ts-node run-local.ts'.
+The <PATH_TO_CSV_FILE> will need to be relative to the location of run-local.ts.
+*/
+
+const streamOutputToCommandLine = async () => {
+  const data = fs.readFileSync(path.join(__dirname, "<PATH_TO_CSV_FILE>"));
   const stringifiedData = data.toString();
 
   console.log(stringifiedData);
@@ -13,4 +21,4 @@ const main = async () => {
   await pushToDynamo({ parsedLines: parsedCsvData, tableName: "danny-Stops" });
 };
 
-main();
+streamOutputToCommandLine();
