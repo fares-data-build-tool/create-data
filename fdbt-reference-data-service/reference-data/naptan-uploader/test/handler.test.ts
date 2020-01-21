be1b029dab33c9adb93dbb3c5feedcddf4ad9ec2
Danny Davies <danny.davies@infinityworks.com>
2020-01-21 21:44:14 +0000

    Added tests for setS3ObjectParams

--- fdbt-reference-data-service/reference-data/naptan-uploader/test/handler.test.ts
+++ fdbt-reference-data-service/reference-data/naptan-uploader/test/handler.test.ts
@@ -1,24 +1,36 @@
-import { S3Handler, S3Event } from "aws-lambda";
-import AWS from "aws-sdk";
-import { WriteRequest } from "aws-sdk/clients/dynamodb";
+import { mockS3Event } from "./mocks/aws_mocks";
 
-import util from "util";
 import csvParse from "csv-parse/lib/sync";
 
-describe('aws handler', () =>{
-    it('should call console.log', () =>{
-        const event = {
-            Name: "myName"
-        };
-        const context: Context = MockContext();
-        const callback: Callback = jest.fn();
-        const globalAny: any = global;
-        globalAny.console = {
-            log: jest.fn()
-        }
-        handler.s3hook(event, context, callback);
-        expect(globalAny.console.log).toHaveBeenCalledWith(
-            JSON.stringify(event)
-        );
-    });
+import { writeBatchesToDynamo, setS3ObjectParams } from "./../handler";
+
+let bucketName = "fdbt-test-naptan-s3-bucket";
+let fileName = "fdbt-test-naptan.csv";
+
+describe("setS3ObjectParams", () => {
+  it("sets s3BucketName from S3Event", () => {
+    const s3Event = mockS3Event(bucketName, fileName);
+
+    const s3ObjectParameters = setS3ObjectParams(s3Event);
+    expect(s3ObjectParameters.Bucket).toEqual(bucketName);
+  });
+
+  it("sets S3FileName from S3Event", () => {
+    const s3Event = mockS3Event(bucketName, fileName);
+
+    const s3ObjectParameters = setS3ObjectParams(s3Event);
+    expect(s3ObjectParameters.Key).toEqual(fileName);
+  });
+
+  it("removes spaces and unicode non-ASCII characters in the S3FileName", () => {
+    let fileName = "fdbt%2Ftest+%3A+naptan.csv"
+    const S3Event = mockS3Event(bucketName, fileName);
+    const params = {
+      Bucket: bucketName,
+      Key: "fdbt/test : naptan.csv"
+    }
+
+    const s3ObjectParameters = setS3ObjectParams(S3Event);
+    expect(s3ObjectParameters).toEqual(params);
+  });
 });
