c232da5329bf02fb831736c452993512755408e3
Danny Davies <danny.davies@infinityworks.com>
2020-01-28 11:37:42 +0000

    Amended serverless.yml files to match proper naming convention and use cross-stack referencing

--- fdbt-reference-data-service/netex-output/netex-convertor/handler.ts
+++ fdbt-reference-data-service/netex-output/netex-convertor/handler.ts
@@ -1,6 +1,6 @@
 import { Handler } from 'aws-lambda';
 
-export const s3hook: Handler = (event: any) => {
+export const s3NetexConvertorHandler: Handler = (event: any) => {
   console.log("THIS IS MY RESULT ");
   console.log(JSON.stringify(event));
 };
