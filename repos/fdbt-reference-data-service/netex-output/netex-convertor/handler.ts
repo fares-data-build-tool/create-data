50d42a2403686e693404060e3b2fde44be66d4a7
Danny Davies <danny.davies@infinityworks.com>
2020-01-28 15:32:29 +0000

    Removed node_modules from gitignore to remove from PR

--- repos/fdbt-reference-data-service/netex-output/netex-convertor/handler.ts
+++ repos/fdbt-reference-data-service/netex-output/netex-convertor/handler.ts
@@ -1,6 +1,8 @@
 import { Handler } from 'aws-lambda';
 
 export const s3NetexConvertorHandler: Handler = (event: any) => {
+  const tableName = process.env.NAPTAN_TABLE_NAME;
+  console.log(tableName);
   console.log("THIS IS MY RESULT ");
   console.log(JSON.stringify(event));
 };
