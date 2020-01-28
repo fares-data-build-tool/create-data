ba6353e92db128e1d9648c5a102fdc3125cad90c
Danny Davies <danny.davies@infinityworks.com>
2020-01-28 15:54:48 +0000

    Removed leftover console.log from netex-convertor lambda

--- fdbt-reference-data-service/netex-output/netex-convertor/handler.ts
+++ fdbt-reference-data-service/netex-output/netex-convertor/handler.ts
@@ -1,8 +1,6 @@
 import { Handler } from 'aws-lambda';
 
 export const s3NetexConvertorHandler: Handler = (event: any) => {
-  const tableName = process.env.NAPTAN_TABLE_NAME;
-  console.log(tableName);
   console.log("THIS IS MY RESULT ");
   console.log(JSON.stringify(event));
 };
