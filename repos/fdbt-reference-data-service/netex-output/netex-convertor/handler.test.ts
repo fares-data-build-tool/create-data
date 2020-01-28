9e83d53eafbc668fb1932106daf262cd567cb36d
Rob Catton <robmcatton@gmail.com>
2020-01-28 17:25:23 +0000

    Functionality done(i think) - tests to follow

--- repos/fdbt-reference-data-service/netex-output/netex-convertor/handler.test.ts
+++ repos/fdbt-reference-data-service/netex-output/netex-convertor/handler.test.ts
@@ -1,9 +1,10 @@
-import * as handler from "./handler"
+import * as s3NetexHandler from "./handler"
 import { Context, Callback } from "aws-lambda";
 import MockContext from 'aws-lambda-mock-context';
 
-describe('', () => {
-    it('', () =>{
-
+describe('s3NetexHandler', () => {
+    it('returns the JSON data put in the bucket', () =>{
+        const event = mockS3Event;
+        s3NetexHandler(event);
     })
 });
