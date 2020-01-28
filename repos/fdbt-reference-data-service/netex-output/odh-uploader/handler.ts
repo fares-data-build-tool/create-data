19d327480af2fd3419b31149b2ea51e7b32190fb
Danny Davies <danny.davies@infinityworks.com>
2020-01-28 11:37:42 +0000

    Amended serverless.yml files to match proper naming convention and use cross-stack referencing

--- repos/fdbt-reference-data-service/netex-output/odh-uploader/handler.ts
+++ repos/fdbt-reference-data-service/netex-output/odh-uploader/handler.ts
@@ -1,9 +1,8 @@
 import { Handler, Context, Callback } from 'aws-lambda';
 
-const s3hook: Handler = (event: any, context: Context, callback: Callback) => {
+export const s3OdhUploaderHandler: Handler = (event: any, context: Context, callback: Callback) => {
   console.log(JSON.stringify(event));
   console.log(JSON.stringify(context));
   console.log(JSON.stringify(process.env));
 };
 
-export { s3hook }
