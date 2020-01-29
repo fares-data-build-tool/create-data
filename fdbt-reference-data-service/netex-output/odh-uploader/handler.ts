cf1a17c07f93ed3cd8766e81730968a2def5fe79
Laurence Jones <laurence.jones@infinityworks.com>
2020-01-29 22:05:03 +0000

    Refactored to move netex and site components to own repo

--- fdbt-reference-data-service/netex-output/odh-uploader/handler.ts
+++
@@ -1,8 +0,0 @@
-import { Handler, Context, Callback } from 'aws-lambda';
-
-export const s3OdhUploaderHandler: Handler = (event: any, context: Context, callback: Callback) => {
-  console.log(JSON.stringify(event));
-  console.log(JSON.stringify(context));
-  console.log(JSON.stringify(process.env));
-};
-
