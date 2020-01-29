1dc3f2f74497e7cc4081890e7823e330310acdf8
Rob Catton <robmcatton@gmail.com>
2020-01-29 15:48:13 +0000

    Finished off IT and unit tests

--- repos/fdbt-reference-data-service/netex-output/netex-convertor/handler.test.ts
+++ repos/fdbt-reference-data-service/netex-output/netex-convertor/handler.test.ts
@@ -1,10 +1,40 @@
-import * as s3NetexHandler from "./handler"
-import { Context, Callback } from "aws-lambda";
-import MockContext from 'aws-lambda-mock-context';
+import { s3NetexHandler } from "./handler";
+import AWS from "aws-sdk";
+import * as mocks from "./test-data/test-data";
 
-describe('s3NetexHandler', () => {
-    it('returns the JSON data put in the bucket', () =>{
-        const event = mockS3Event;
-        s3NetexHandler(event);
-    })
+jest.mock("aws-sdk");
+
+describe("s3 handler with csv event", () => {
+    const mockS3GetObject = jest.fn();
+
+    beforeEach(() => {
+
+        (AWS.S3 as any) = jest.fn().mockImplementation(() => {
+            return {
+                getObject: mockS3GetObject
+            };
+        });
+
+        mockS3GetObject.mockImplementation(() => ({
+            promise() {
+                return Promise.resolve({ Body: { data: "This is the data abcdefghijklmnop" }});
+            }
+        }));
+
+    });
+
+    afterEach(() => {
+        mockS3GetObject.mockReset();
+    });
+
+    it("sends the data to dynamo when a csv is created", async () => {
+        const event = mocks.mockS3Event(
+            "thisIsMyBucket",
+            "andThisIsTheNameOfTheThing.csv"
+        );
+
+        const result = await s3NetexHandler(event);
+        
+        expect(JSON.stringify(result)).toContain("abcdefghijklmnop");
+    });
 });
