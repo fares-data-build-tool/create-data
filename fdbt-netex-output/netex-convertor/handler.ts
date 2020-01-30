5c239d3ae4f02520411501e246ef0d717c823da5
gilesdrinkall <giles.drinkall@infinityworks.com>
2020-01-30 09:18:18 +0000

    first commit

--- fdbt-netex-output/netex-convertor/handler.ts
+++ fdbt-netex-output/netex-convertor/handler.ts
@@ -1,6 +1,37 @@
-import { Handler } from 'aws-lambda';
+import { Handler, AttributeValue} from 'aws-lambda';
+import AWS from "aws-sdk";
 
-export const s3NetexConvertorHandler: Handler = (event: any) => {
-  console.log("THIS IS MY RESULT ");
-  console.log(JSON.stringify(event));
+export async function retrieveItemDataFromDynamoDBTable 
+(tableName: string, primaryKey: string, primaryKeyValue: string) {
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const tableNm: string = tableName;
+
+const primKey: string = primaryKey;
+
+const primKeyVal: string = primaryKeyValue;
+
+const params: AWS.DynamoDB.DocumentClient.QueryInput = {
+    TableName : tableNm,
+    KeyConditionExpression: "#atNm = :pkVal",
+    ExpressionAttributeNames: {
+        "#atNm": primKey
+    },
+    ExpressionAttributeValues: {
+        ":pkVal": primKeyVal
+    }
+  };
+docClient.query(params, function(err, data) {
+    if (err) {
+        console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Query succeeded.");
+        return data;
+    }
+  });
+}
+
+export const s3hook: Handler = async () => {
+    retrieveItemDataFromDynamoDBTable("Stops", "ATCOCode", "077072787B");
 };
