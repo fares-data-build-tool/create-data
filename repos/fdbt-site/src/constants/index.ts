c271d88d421fbdcf473161c91ad730288c845335
Rob Catton <robmcatton@gmail.com>
2020-02-06 11:29:57 +0000

    Removed unneeded constants and updated placeholder page for csv upload

--- repos/fdbt-site/src/constants/index.ts
+++ repos/fdbt-site/src/constants/index.ts
@@ -7,9 +7,3 @@ export const FARETYPE_COOKIE: string = 'fdbt-faretype-cookie';
 export const FEEDBACK_LINK: string = 'mailto:fdbt@transportforthenorth.com';
 
 export const GOVUK_LINK: string = 'https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/';
-
-export const CSV_INPUT_METHOD_COOKIE: string = 'fdbt-csv-input-method-cookie';
-
-export const MANUAL_INPUT_METHOD_COOKIE: string = 'fdbt-manual-input-method-cookie';
-
-export const INT_MAP_INPUT_METHOD_COOKIE: string = 'fdbt-interactive-map-input-method-cookie';
