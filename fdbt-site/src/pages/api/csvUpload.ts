c5b71d8b4965aa58bf827d174862502e1666e615
Rob Catton <robmcatton@gmail.com>
2020-02-10 15:44:33 +0000

    Functionality finished, unit tests still to do

--- fdbt-site/src/pages/api/csvUpload.ts
+++ fdbt-site/src/pages/api/csvUpload.ts
@@ -1,9 +1,9 @@
-import { getCookies } from './apiUtils/index';
-import util from 'util';
 import { NextApiRequest, NextApiResponse } from 'next';
-import formidable from 'formidable';
+import formidable, { Files } from 'formidable';
 import fs from 'fs';
 import AWS from 'aws-sdk';
+import { getCookies } from './apiUtils/index';
+import { OPERATOR_COOKIE } from '../../constants';
 
 export type FareTriangle = FareTriangleData;
 
@@ -22,94 +22,166 @@ export const config = {
     },
 };
 
+export const formParse = async (req: NextApiRequest) => {
+    return new Promise<Files>((resolve, reject) => {
+        const form = new formidable.IncomingForm();
+        form.parse(req, (err, _fields, file) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(file);
+        });
+    });
+};
+
+
+export const putDataInS3 = async (data: FareTriangleData | string, key: string, processed: boolean) => {
+    const s3 = new AWS.S3();
+
+    let bucketfolder = '';
+
+    if (processed) {
+        bucketfolder = '/processed';
+    } else {
+        bucketfolder = '/unprocessed';
+    }
+
+    const request: AWS.S3.Types.PutObjectRequest = {
+        Bucket: `fdbt-user-data-${process.env.STAGE}${bucketfolder}`,
+        Key: key,
+        Body: data,
+        ContentType: 'application/json; charset=utf-8',
+    };
+
+    await s3.putObject(request).promise();
+};
+
+export const faresTriangleDataMapper = (dataToMap: string): FareTriangleData => {
+    const fareTriangle: FareTriangleData = {
+        fareStages: [
+            {
+                stageName: '',
+                prices: [],
+            },
+        ],
+    };
+
+    const dataAsLines: string[] = dataToMap.split('\n');
+
+    for(let t = 0; t < dataAsLines.length; t += 1){
+        if(fareTriangle.fareStages[t]){
+            fareTriangle.fareStages[t].stageName = dataAsLines[t].split(',')[t + 1];
+        } else {
+            fareTriangle.fareStages[t] = {
+                stageName: dataAsLines[t].split(',')[t + 1],
+                prices: []
+            }
+        }
+    }
+
+    for (let i = 1; i < dataAsLines.length; i += 1) {
+        const items = dataAsLines[i].split(',');
+        for (let j = 0; j < i + 1; j += 1) {
+            if (fareTriangle.fareStages[j] && items[j + 1] && j !== i) {
+                if (items[j + 1] !== '') {
+                    fareTriangle.fareStages[j].prices.push(items[j + 1]);
+                }
+            }
+        }
+    }
+    return fareTriangle;
+};
+
+export const getUuidFromCookie = (req: NextApiRequest) => {
+    const cookies = getCookies(req);
+
+    const operatorCookie = unescape(decodeURI(cookies[OPERATOR_COOKIE]));
+
+    return JSON.parse(operatorCookie).uuid;
+};
+
+export const fileChecks = (res: NextApiResponse, formData: formidable.Files, fileContent: string): boolean => {
+    const fileSize = formData['file-upload-1'].size;
+
+    const fileType = formData['file-upload-1'].type;
+
+    if (!fileContent) {
+        console.log('No file attached.');
+        res.writeHead(302, {
+            Location: '/csvUpload',
+        });
+        res.end();
+        return false;
+    }
+
+    if (fileSize > 5242880) {
+        console.log('File is too large.');
+        res.writeHead(302, {
+            Location: '/error',
+        });
+        res.end();
+        return false;
+    }
+
+    if (fileType !== 'text/csv') {
+        console.log('File is not a csv.');
+        res.writeHead(302, {
+            Location: '/error',
+        });
+        res.end();
+        return false;
+    }
+
+    return true;
+};
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     try {
-        const form = new formidable.IncomingForm();
+        const formData = await formParse(req);
 
-        form.parse(req, function (_err, _fields, file) {
-            fs.readFile(file['file-upload-1'].path, 'utf-8', async (_error, data) => {
-                if (!data) {
-                    res.writeHead(302, {
-                        Location: '/csvUpload',
-                    });
-                    res.end();
-                    return;
-                }
+        const fileContent = await fs.promises.readFile(formData['file-upload-1'].path, 'utf-8');
 
-                const cookies = getCookies(req);
+        const isValid = fileChecks(res, formData, fileContent);
 
-                const uuid = cookies.uuid;
+        if (!isValid) {
+            return;
+        }
 
-                await putDataInS3(data, uuid, false);
+        if (fileContent) {
+            try {
+                const uuid = getUuidFromCookie(req);
 
-                const fareTriangle: FareTriangleData = {
-                    fareStages: [
-                        {
-                            stageName: '',
-                            prices: [],
-                        },
-                    ],
-                };
+                await putDataInS3(fileContent, uuid, false);
 
-                const dataAsLines: string[] = data.split('\n');
+                const fareTriangleData = faresTriangleDataMapper(fileContent);
 
-                dataAsLines.map((item, index) => {
-                    if (fareTriangle.fareStages[index]) {
-                        fareTriangle.fareStages[index].stageName = item.split(',')[index + 1];
-                    } else {
-                        fareTriangle.fareStages[index] = {
-                            stageName: item.split(',')[index + 1],
-                            prices: [],
-                        };
-                    }
+                await putDataInS3(fareTriangleData, uuid, true);
+
+                res.writeHead(302, {
+                    Location: '/matching',
                 });
 
-                for (let i = 1; i < dataAsLines.length; i += 1) {
-                    const items = dataAsLines[i].split(',');
-                    for (let j = 0; j < i + 1; j += 1) {
-                        if (fareTriangle.fareStages[j] && items[j + 1] && j != i) {
-                            if (items[j + 1] !== '') {
-                                fareTriangle.fareStages[j].prices.push(items[j + 1]);
-                            }
-                        }
-                    }
-                }
-                console.log(util.inspect(fareTriangle, false, null, true));
+                res.end();
+                
+            } catch (error) {
+                console.log(error);
 
-                await putDataInS3(fareTriangle, uuid, true)
-            });
+                res.writeHead(302, {
+                    Location: '/error',
+                });
+                res.end();
+                return;
+            }
+        }
 
-            res.writeHead(302, {
-                Location: '/matching',
-            });
-
-            res.end();
+        res.writeHead(302, {
+            Location: '/csvUpload',
         });
+
+        res.end();
     } catch (error) {
         res.writeHead(302, {
             Location: '/error',
         });
     }
 };
-
-export async function putDataInS3(data: FareTriangleData | string, key: string, processed: boolean) {
-
-    const s3 = new AWS.S3();
-
-    let bucketfolder = "";
-
-    if (processed) {
-        bucketfolder = "/processed"
-    } else {
-        bucketfolder = "/unprocessed"
-    }
-
-    const request: AWS.S3.Types.PutObjectRequest = {
-        Bucket: "fdbt-user-data-" + process.env.STAGE + bucketfolder,
-        Key: key,
-        Body: data,
-        ContentType: 'application/json; charset=utf-8'
-    }
-
-    await s3.putObject(request).promise();
-}
