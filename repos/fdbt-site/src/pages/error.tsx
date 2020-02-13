796c54ff4cb7ce3b07df1f113e47dec40f6391f5
gilesdrinkall <giles.drinkall@infinityworks.com>
2020-02-13 14:38:47 +0000

    HTML from govuk-design-system site copied in and amended.

--- repos/fdbt-site/src/pages/error.tsx
+++ repos/fdbt-site/src/pages/error.tsx
@@ -8,7 +8,26 @@ const description = 'Error page of the Fares data build tool';
 const Error = (): ReactElement => (
     <Layout title={title} description={description}>
         <main className="govuk-main-wrapper app-main-class" id="main-content" role="main">
-            <h1 className="govuk-heading-xl">ERROR</h1>
+            <div className="govuk-grid-row">
+                <div className="govuk-grid-column-two-thirds">
+                    <h1 className="govuk-heading-xl">Sorry, there is a problem with the service.</h1>
+                    <p className="govuk-body">Try again later.</p>
+                    <p className="govuk-body">Your answers have not been saved, click continue to start again.</p>
+                    <p className="govuk-body">
+                        Contact us with the feedback button at the top of this page for assistance.
+                    </p>
+                    <br />
+                    <a
+                        href="/"
+                        role="button"
+                        draggable="false"
+                        className="govuk-button govuk-button--start"
+                        data-module="govuk-button"
+                    >
+                        Continue
+                    </a>
+                </div>
+            </div>
         </main>
     </Layout>
 );
