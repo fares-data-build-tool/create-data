cda47122eb9b6a353c5043ba514b10bb011f91b3
Danny Davies <danny.davies@infinityworks.com>
2020-02-20 15:08:17 +0000

    Fixed tests for direction.test.ts

--- repos/fdbt-site/tests/pages/api/direction.test.ts
+++ repos/fdbt-site/tests/pages/api/direction.test.ts
@@ -2,33 +2,12 @@ import { mockRequest, mockResponse } from 'mock-req-res';
 import direction from '../../../src/pages/api/direction';
 import { OPERATOR_COOKIE } from '../../../src/constants';
 
-describe('service', () => {
+describe('direction', () => {
     beforeEach(() => {
         jest.resetAllMocks();
     });
 
-    it('should return 302 redirect to /direction when the session is valid, there is no journey cookie BUT one can be set', () => {
-        const writeHeadMock = jest.fn();
-        const req = mockRequest({
-            connection: {
-                encrypted: false,
-            },
-            body: { direction: 'test_direction' },
-            headers: {
-                host: 'localhost:5000',
-                cookie: `${OPERATOR_COOKIE}=%7B%22operator%22%3A%22FirstBus%22%2C%22uuid%22%3A%22cbc0111a-e763-48e7-982b-ac25ecbe625c%22%7D`,
-            },
-        });
-        const res = mockResponse({
-            writeHead: writeHeadMock,
-        });
-        direction(req, res);
-        expect(writeHeadMock).toBeCalledWith(302, {
-            Location: '/direction',
-        });
-    });
-
-    it('should return 302 redirect to /direction when session is valid but there is neither a service cookie nor can one be set', () => {
+    it('should return 302 redirect to /direction (i.e. itself) when the session is valid, but there is no request body', () => {
         const writeHeadMock = jest.fn();
         const req = mockRequest({
             connection: {
@@ -46,6 +25,27 @@ describe('service', () => {
         direction(req, res);
         expect(writeHeadMock).toBeCalledWith(302, {
             Location: '/direction',
+        });
+    });
+
+    it('should return 302 redirect to /inputMethod when session is valid and request body is present', () => {
+        const writeHeadMock = jest.fn();
+        const req = mockRequest({
+            connection: {
+                encrypted: false,
+            },
+            body: { journeyPattern: 'test_journey' },
+            headers: {
+                host: 'localhost:5000',
+                cookie: `${OPERATOR_COOKIE}=%7B%22operator%22%3A%22FirstBus%22%2C%22uuid%22%3A%22cbc0111a-e763-48e7-982b-ac25ecbe625c%22%7D`,
+            },
+        });
+        const res = mockResponse({
+            writeHead: writeHeadMock,
+        });
+        direction(req, res);
+        expect(writeHeadMock).toBeCalledWith(302, {
+            Location: '/inputMethod',
         });
     });
 
