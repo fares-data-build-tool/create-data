d45592240574f9db7529d86bc7fdebd280882d4e
Rob Catton <robmcatton@gmail.com>
2020-02-20 12:45:14 +0000

    Fixed tests as we pass the number of stages as a string

--- repos/fdbt-site/tests/pages/api/chooseStages.test.ts
+++ repos/fdbt-site/tests/pages/api/chooseStages.test.ts
@@ -12,16 +12,20 @@ describe('chooseStages', () => {
             writeHead: writeHeadMock,
         });
     });
-
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const cases: any[] = [
-        // eslint-disable-line @typescript-eslint/no-explicit-any
         [{}, { Location: '/chooseStages' }],
         [{ fareStageInput: 'abcdefghijk' }, { Location: '/error' }],
+        [{ fareStageInput: '1.2' }, { Location: '/error' }],
         [{ fareStageInput: 1.2 }, { Location: '/error' }],
+        [{ fareStageInput: '21' }, { Location: '/error' }],
         [{ fareStageInput: 21 }, { Location: '/error' }],
+        [{ fareStageInput: '0' }, { Location: '/error' }],
         [{ fareStageInput: 0 }, { Location: '/error' }],
-        [{ fareStageInput: '1' }, { Location: '/error' }],
+        [{ fareStageInput: '1' }, { Location: '/stageNames' }],
         [{ fareStageInput: "[]'l..33" }, { Location: '/error' }],
+        [{ fareStageInput: '6' }, { Location: '/stageNames' }],
+        [{ fareStageInput: 6 }, { Location: '/stageNames' }],
     ];
 
     test.each(cases)('given %p as request, redirects to %p', (testData, expectedLocation) => {
