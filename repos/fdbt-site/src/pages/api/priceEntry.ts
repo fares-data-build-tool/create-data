bc1cec75134820c2f6089afda59a9720592f55de
gilesdrinkall <giles.drinkall@infinityworks.com>
2020-02-24 17:25:38 +0000

    index.ts: changed name of STAGE_NAMES_COOKIE, Pages.css: updated rule-sets so that left-sided labels align, priceEntry.tsx: introduced span for left-sided labels to enable correct styling, priceEntry.ts: added interface for FaresTriangleItem

--- repos/fdbt-site/src/pages/api/priceEntry.ts
+++ repos/fdbt-site/src/pages/api/priceEntry.ts
@@ -1,36 +1,43 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { STAGENAMES_COOKIE } from '../../constants/index';
-import { getCookies, redirectToError, redirectTo } from './apiUtils';
-import { putStringInS3 } from '../../data/s3';
+// import { STAGE_NAMES_COOKIE } from '../../constants/index';
+// import { getCookies, redirectToError, redirectTo } from './apiUtils';
+import { redirectToError, redirectTo } from './apiUtils';
+// import { putStringInS3 } from '../../data/s3';
+
+// interface FaresTriangleItem {
+//     key: string;
+//     value: number;
+// }
 
 export default (req: NextApiRequest, res: NextApiResponse): void => {
     try {
-        const cookies = getCookies(req);
+        // const items: FaresTriangleItem [] = Object.entries(req.body).map((k, v) => ({k, v}));
+        // if (!items) {
+        //     redirectTo(res, '/priceEntry');
+        //     return;
+        // }
+        // const stringifiedBodyValues = JSON.stringify(items);
 
-        const { priceEntry } = req.body;
+        // const cookies = getCookies(req);
+        // const stageNamesCookie = unescape(decodeURI(cookies[STAGE_NAMES_COOKIE]));
+        // const stageNamesObject = JSON.parse(stageNamesCookie);
+        // const { uuid } = stageNamesObject;
+        // if (!uuid) {
+        //     throw new Error('No UUID found');
+        // }
 
-        if (!priceEntry) {
-            redirectTo(res, '/priceEntry');
-            return;
-        }
-
-        const stageNamesCookie = unescape(decodeURI(cookies[STAGENAMES_COOKIE]));
-        const stageNamesObject = JSON.parse(stageNamesCookie);
-        const { uuid } = stageNamesObject;
-
-        if (!uuid) {
-            throw new Error('No UUID found');
-        }
+        // const uuid = "gd1234";
 
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const bucketName: string = process.env.USER_DATA_BUCKET_NAME!;
-        const key = `${uuid}.json`;
-        const contentType = 'application/json; charset=utf-8';
-        putStringInS3(bucketName, key, priceEntry, contentType);
+        // const bucketName: string = process.env.USER_DATA_BUCKET_NAME!;
+        // const key = `${uuid}.json`;
+        // const contentType = 'application/json; charset=utf-8';
+        // putStringInS3(bucketName, key, stringifiedBodyValues, contentType);
 
         redirectTo(res, '/matching');
     } catch (error) {
         redirectToError(res);
     }
+
     res.end();
 };
