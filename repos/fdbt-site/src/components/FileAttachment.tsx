041703ae16a84b0f15a6d2b65242d19b1f309d84
Rob Catton <robmcatton@gmail.com>
2020-02-25 16:43:36 +0000

    Finished page, including arrangement changes

--- repos/fdbt-site/src/components/FileAttachment.tsx
+++ repos/fdbt-site/src/components/FileAttachment.tsx
@@ -5,9 +5,15 @@ export interface FileAttachmentProps {
     displayName: string;
     attachmentUrl: string;
     imageUrl: string;
+    size: string;
 }
 
-const FileAttachment: FC<FileAttachmentProps> = ({ displayName, attachmentUrl, imageUrl }: FileAttachmentProps) => (
+const FileAttachment: FC<FileAttachmentProps> = ({
+    displayName,
+    attachmentUrl,
+    imageUrl,
+    size,
+}: FileAttachmentProps) => (
     <section className="file-attachment">
         <div className="file-attachment-thumbnail">
             <a aria-hidden="true" href={attachmentUrl}>
@@ -15,11 +21,13 @@ const FileAttachment: FC<FileAttachmentProps> = ({ displayName, attachmentUrl, i
             </a>
         </div>
         <div>
-            <a href={attachmentUrl} className="govuk-link govuk-!-font-size-27">
+            <a href={attachmentUrl} className="govuk-link govuk-!-font-size-14">
                 {displayName}
             </a>
-            <p className="file-attachment-metadata">
-                <abbr title="Portable Document Format">PDF</abbr>
+            <p className="file-attachment-metadata govuk-!-font-size-10">
+                <abbr title="File">
+                    {attachmentUrl.substr(attachmentUrl.length - 3).toUpperCase()} - {size}
+                </abbr>
             </p>
         </div>
     </section>
