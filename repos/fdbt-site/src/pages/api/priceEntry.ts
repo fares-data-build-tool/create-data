65c22e870978b1c0730e4289af5b25280af8c411
gilesdrinkall <giles.drinkall@infinityworks.com>
2020-02-26 17:10:04 +0000

    priceEntry.tsx: increased size of hard-coded fare stages to 20, priceEntry.ts: added functionality to validate that correct number of inputs is received and that the inputs are correct prices

--- repos/fdbt-site/src/pages/api/priceEntry.ts
+++ repos/fdbt-site/src/pages/api/priceEntry.ts
@@ -4,24 +4,78 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { redirectToError, redirectTo } from './apiUtils';
 // import { putStringInS3 } from '../../data/s3';
 
-interface UserFareStages {
-    fareStages: {
-        stageName: string;
-        prices: {
-            price: string;
-            fareZones: string[];
-        }[];
-    }[];
-}
+// interface UserFareStages {
+//     fareStages: {
+//         stageName: string;
+//         prices: {
+//             price: string;
+//             fareZones: string[];
+//         }[];
+//     }[];
+// }
 
-interface FareTriangleData {
-    [stageName: string]: {
-        [price: string]: {
-            price: string;
-            fareZones: string[];
-        };
+// interface FareTriangleData {
+//     [stageName: string]: {
+//         [price: string]: {
+//             price: string;
+//             fareZones: string[];
+//         };
+//     };
+// }
+
+export const priceInputIsValid = (req: NextApiRequest): boolean => {
+    const cookies = getCookies(req);
+    const stageNamesCookie = unescape(decodeURI(cookies[STAGE_NAMES_COOKIE]));
+    const numberOfStagesInCookie = JSON.parse(stageNamesCookie).length;
+    const expectedNumberOfPriceInputs = numberOfStagesInCookie ** 2 / 2;
+    const numberofInputInApiRequest = Object.entries(req.body).length;
+    if (expectedNumberOfPriceInputs === numberofInputInApiRequest) {
+        return true;
+    }
+    return false;
+};
+
+export const faresTriangleDataMapper = (req: NextApiRequest): UserFareStages => {
+    const arrayOfFareItemArrays: string[][] = Object.entries(req.body);
+    const fareTriangle: FareTriangleData = {};
+
+    for (let itemNum = 0; itemNum < arrayOfFareItemArrays.length; itemNum += 1) {
+        const cellRef = arrayOfFareItemArrays[itemNum][0];
+        const splitCellRefAsArray = cellRef.split('-');
+        const destinationFareStageName = splitCellRefAsArray[0];
+        const originFareStageName = splitCellRefAsArray[1];
+        const price = arrayOfFareItemArrays[itemNum][1];
+
+        if (!fareTriangle[originFareStageName]) {
+            fareTriangle[originFareStageName] = {};
+        }
+
+        if (price) {
+            if (!Number.isNaN(Number(price))) {
+                if (!fareTriangle[originFareStageName][price]) {
+                    fareTriangle[originFareStageName][price] = {
+                        price: (parseFloat(price) / 100).toFixed(2),
+                        fareZones: [],
+                    };
+                }
+
+                fareTriangle[originFareStageName][price].fareZones.push(destinationFareStageName);
+            }
+        }
+    }
+
+    const originStages = Object.entries(fareTriangle);
+    const mappedFareTriangle: UserFareStages = {
+        fareStages: originStages.map(kv => {
+            const pricesToDestinationStages = Object.values(kv[1]);
+            return {
+                stageName: kv[0],
+                prices: pricesToDestinationStages,
+            };
+        }),
     };
-}
+    return mappedFareTriangle;
+};
 
 // export const getUuidFromCookie = (req: NextApiRequest): string => {
 //     const cookies = getCookies(req);
@@ -45,47 +99,12 @@ interface FareTriangleData {
 
 export default (req: NextApiRequest, res: NextApiResponse): void => {
     try {
-        const arrayOfFareItemArrays: string[][] = Object.entries(req.body);
-
-        // TO DO: validate all input rows have prices
-
-        const fareTriangle: FareTriangleData = {};
-
-        for (let itemNum = 0; itemNum < arrayOfFareItemArrays.length; itemNum += 1) {
-            const cellRef = arrayOfFareItemArrays[itemNum][0];
-            const splitCellRefAsArray = cellRef.split('-');
-            const destinationFareStageName = splitCellRefAsArray[0];
-            const originFareStageName = splitCellRefAsArray[1];
-            const price = arrayOfFareItemArrays[itemNum][1];
-
-            if (!fareTriangle[originFareStageName]) {
-                fareTriangle[originFareStageName] = {};
-            }
-
-            if (!fareTriangle[originFareStageName][price]) {
-                fareTriangle[originFareStageName][price] = {
-                    price: (parseFloat(price) / 100).toFixed(2),
-                    fareZones: [],
-                };
-            }
-
-            fareTriangle[originFareStageName][price].fareZones.push(destinationFareStageName);
+        if (!priceInputIsValid(req)) {
+            return;
         }
-
-        const originStages = Object.entries(fareTriangle);
-        const mappedFareTriangle: UserFareStages = {
-            fareStages: originStages.map(kv => {
-                const pricesToDestinationStages = Object.values(kv[1]);
-                return {
-                    stageName: kv[0],
-                    prices: pricesToDestinationStages,
-                };
-            }),
-        };
-        console.log(mappedFareTriangle);
-
+        // const mappedData = faresTriangleDataMapper(req);
         // const uuid = getUuidFromCookie(req);
-        // putStringInS3(uuid, text);
+        // putStringInS3(uuid, mappedData);
         redirectTo(res, '/matching');
     } catch (error) {
         console.error(`There was a problem generating the priceEntry JSON: ${error.stack}`);
