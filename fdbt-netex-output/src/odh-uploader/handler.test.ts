13d7951a5fad1603a8815c4d0b2a0e8ec8ceea64
Rob Catton <robmcatton@gmail.com>
2020-02-28 10:13:15 +0000

    First attempt at working emails. One small unit test for handler

---
+++ fdbt-netex-output/src/odh-uploader/handler.test.ts
@@ -0,0 +1,57 @@
+import { createMailTransporter, odhUploaderHandler } from './handler';
+import AWS from 'aws-sdk';
+import { S3Event } from 'aws-lambda';
+import * as testData from './testData/testData'
+import nodemailer from 'nodemailer';
+
+jest.mock('aws-sdk');
+
+describe('odhHandler SES emailer', () => {
+
+    const mockS3GetObject = jest.fn();
+    const mockMailTransporter = jest.fn();
+
+    beforeEach(() => {
+
+        (AWS.S3 as {}) = jest.fn().mockImplementation(() => {
+            return {
+                getObject: mockS3GetObject,
+            };
+        });
+
+        mockS3GetObject.mockImplementation(() => ({
+            promise(): Promise<{}> {
+                return Promise.resolve({ Body: "body"});
+            },
+        }));
+
+        (createMailTransporter as {}) = jest.fn().mockImplementation(() => {
+            return {
+                sendMail: mockMailTransporter,
+            };
+        });
+
+        mockMailTransporter.mockImplementation(() => {
+            return nodemailer.createTransport({
+                streamTransport: true,
+                newline: 'unix',
+                buffer: true
+            });
+        })
+    });
+
+    afterEach(() => {
+        mockS3GetObject.mockReset();
+        mockMailTransporter.mockReset();
+    });
+
+    it('sends an email', async () => {
+        const key = 'NameOfNetexFile.xml';
+        const event: S3Event = testData.testS3Event('thisIsMyBucket', key);
+
+        await odhUploaderHandler(event);
+
+        expect(mockMailTransporter).toBeCalledTimes(1);
+
+    });
+});
--- fdbt-netex-output/src/odh-uploader/handler.ts
+++ fdbt-netex-output/src/odh-uploader/handler.ts
@@ -1,12 +1,21 @@
 import { S3Event } from 'aws-lambda';
 import AWS from 'aws-sdk';
-
-const ses = new AWS.SES({ region: 'eu-west-1' });
+import nodemailer, { SentMessageInfo } from 'nodemailer';
+import Mail from 'nodemailer/lib/mailer';
+import { promises as fs } from 'fs';
 
 export interface S3ObjectParameters {
     Bucket: string;
     Key: string;
 }
+
+export const createMailTransporter = (): Mail => {
+    return nodemailer.createTransport({
+        SES: new AWS.SES({
+            apiVersion: '2010-12-01',
+        }),
+    });
+};
 
 export const setS3ObjectParams = (event: S3Event): S3ObjectParameters => {
     const s3BucketName: string = event.Records[0].s3.bucket.name;
@@ -18,31 +27,52 @@ export const setS3ObjectParams = (event: S3Event): S3ObjectParameters => {
     return params;
 };
 
-export const odhUploaderHandler = async (event: S3Event): Promise<void> => {
-    const s3ObjectParams = setS3ObjectParams(event);
-    const emailParams = {
-        Destination: {
-            ToAddresses: ['tfn@infinityworks.com', 'fdbt@transportforthenorth.com'],
-        },
-        Message: {
-            Body: {
-                Text: {
-                    Data: `There was a file uploaded to '${s3ObjectParams.Bucket}' [Filename: '${s3ObjectParams.Key}']`,
-                },
+export const setMailOptions = (params: S3ObjectParameters, pathToNetex: string): Mail.Options => {
+    return {
+        from: 'tfn@infinityworks.com',
+        to: ['tfn@infinityworks.com', 'fdbt@transportforthenorth.com'],
+        subject: `NeTEx created for ${params.Key}`,
+        text: `There was a file uploaded to '${params.Bucket}' [Filename: '${params.Key} ']`,
+        attachments: [
+            {
+                path: pathToNetex,
             },
-            Subject: {
-                Data: 'Test Email for using SES',
-            },
-        },
-        Source: 'tfn@infinityworks.com',
+        ],
     };
+};
+
+export const getNetexFileFromS3 = async (params: S3ObjectParameters): Promise<string> => {
+    const s3 = new AWS.S3();
+    const data = await s3.getObject(params).promise();
+    return data.Body?.toString('utf-8') ?? '';
+};
+
+export const odhUploaderHandler = async (event: S3Event): Promise<void> => {
     try {
-        // THIS NEEDS CHANGING FROM 'ses.sendEmail' to 'ses.sendRawEmail' to enable attachments.
-        const sendPromise = await ses.sendEmail(emailParams).promise();
-        console.log(sendPromise.MessageId)
+        const s3ObjectParams = setS3ObjectParams(event);
+        const pathToSavedNetex = `/tmp/${s3ObjectParams.Key}`;
+        const netexFile = await getNetexFileFromS3(s3ObjectParams);
+        await fs.writeFile(netexFile, pathToSavedNetex);
+
+        const mailOptions = setMailOptions(s3ObjectParams, pathToSavedNetex);
+
+        const mailTransporter = createMailTransporter();
+
+        const info: SentMessageInfo = await mailTransporter.sendMail(mailOptions);
+
+        console.log(info);
+
+        if(info.message){
+            console.log(`Email sent: ${info.message}`);
+        } else {
+            console.log(`Email sent.`)
+        }
+
+        
+
     } catch (err) {
         throw new Error(
-            `SES SendEmail failed. Error: ${err.name}, Error Message: ${err.message}. Parameters sent via SES: ${emailParams}`,
+            `SES SendEmail failed. Error: ${err.stack}`,
         );
     }
 };
