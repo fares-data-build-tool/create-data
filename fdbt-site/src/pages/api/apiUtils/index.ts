3d10c36a8eb32a1b3f3291aecc8e71e1b8a8cdd7
Danny Davies <danny.davies@infinityworks.com>
2020-02-28 11:43:20 +0000

    Added tests to check cookies are set

--- fdbt-site/src/pages/api/apiUtils/index.ts
+++ fdbt-site/src/pages/api/apiUtils/index.ts
@@ -32,11 +32,7 @@ export const setCookieOnResponseObject = (
 
 export const getUuidFromCookie = (req: NextApiRequest, res: NextApiResponse): string => {
     const cookies = new Cookies(req, res);
-    console.log({ req });
-    console.log({ cookies });
     const operatorCookie = unescape(decodeURI(cookies.get(OPERATOR_COOKIE) || ''));
-    console.log({ operatorCookie });
-    console.log(JSON.parse(operatorCookie).uuid);
     return JSON.parse(operatorCookie).uuid;
 };
 
