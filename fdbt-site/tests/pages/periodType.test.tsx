fe20767edad6325863a75e41c784515bf1b105a1
Tej Powar <tejwant.powar@infinityworks.com>
2020-03-05 14:11:08 +0000

    [TFN-269] add period type page

---
+++ fdbt-site/tests/pages/periodType.test.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable global-require */
+
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import PeriodType from '../../src/pages/periodType';
+
+describe('pages', () => {
+    describe('periodtype', () => {
+        it('should render correctly', () => {
+            const tree = shallow(<PeriodType />);
+            expect(tree).toMatchSnapshot();
+        });
+    });
+});
