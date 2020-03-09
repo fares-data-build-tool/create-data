7e64da48eac9cc70e995c8348df53f73248bd34b
gilesdrinkall <giles.drinkall@infinityworks.com>
2020-03-09 08:49:16 +0000

    csvZoneUpload.test.tsx: snapshot test added

---
+++ repos/fdbt-site/tests/pages/csvZoneUpload.test.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable global-require */
+
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import CsvZoneUpload from '../../src/pages/csvZoneUpload';
+
+describe('pages', () => {
+    describe('csvzoneupload', () => {
+        it('should render correctly', () => {
+            const tree = shallow(<CsvZoneUpload />);
+            expect(tree).toMatchSnapshot();
+        });
+    });
+});
