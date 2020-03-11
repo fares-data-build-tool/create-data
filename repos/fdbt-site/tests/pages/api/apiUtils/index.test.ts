a0f75998c1c693700f2d1a631982ea8707ca9e5b
Danny Davies <danny.davies@infinityworks.com>
2020-03-11 15:58:55 +0000

    Added extra functionality from dev of TFN-255 which was missing after previous merge of old commit

--- repos/fdbt-site/tests/pages/api/apiUtils/index.test.ts
+++ repos/fdbt-site/tests/pages/api/apiUtils/index.test.ts
@@ -1,18 +1,20 @@
-/* eslint-disable global-require */
-
-import { mockRequest, mockResponse } from 'mock-req-res';
 import MockReq from 'mock-req';
 import Cookies from 'cookies';
-import { getDomain, setCookieOnResponseObject } from '../../../../src/pages/api/apiUtils';
-
-const mockCookiesSet = jest.fn();
-jest.mock('cookies', () => {
-    return jest.fn().mockImplementation(() => {
-        return { set: mockCookiesSet };
-    });
-});
+import { getDomain, setCookieOnResponseObject, getUuidFromCookie } from '../../../../src/pages/api/apiUtils';
+import * as s3 from '../../../../src/data/s3';
+import { getMockRequestAndResponse } from '../../../testData/mockData';
 
 describe('apiUtils', () => {
+    beforeEach(() => {
+        jest.spyOn(s3, 'putStringInS3');
+
+        Cookies.prototype.set = jest.fn();
+    });
+
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
     describe('getDomain', () => {
         it('should return the domain without a port number', () => {
             const expected = 'localhost';
@@ -33,20 +35,28 @@ describe('apiUtils', () => {
             const domain = 'localhost';
             const cookieName = 'test';
             const cookieValue = 'cookieValue';
-            const req = mockRequest();
-            const res = mockResponse();
+            const { req, res } = getMockRequestAndResponse();
             // mock the library and its implementation of the set cookie method
 
             // call our method with the consts we set above
             setCookieOnResponseObject(domain, cookieName, cookieValue, req, res);
             // making sure our mock is being called with what we passed it
-            expect(Cookies).toBeCalled();
-            expect(mockCookiesSet).toBeCalledWith(cookieName, cookieValue, {
+            expect(Cookies.prototype.set).toBeCalledWith(cookieName, cookieValue, {
                 domain,
                 path: '/',
                 maxAge: 1000 * (3600 * 24),
                 sameSite: 'strict',
             });
+        });
+    });
+
+    describe('getUuidFromCookie', () => {
+        it('should get the uuid from the cookie', () => {
+            const { req, res } = getMockRequestAndResponse({}, null, {
+                operatorUuid: '780e3459-6305-4ae5-9082-b925b92cb46c',
+            });
+            const result = getUuidFromCookie(req, res);
+            expect(result).toBe('780e3459-6305-4ae5-9082-b925b92cb46c');
         });
     });
 });
