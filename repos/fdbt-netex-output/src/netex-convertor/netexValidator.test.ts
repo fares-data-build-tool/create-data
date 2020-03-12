fda8bd1e92d2fad17c1a732e49d271fab8eca901
Rob Catton <robmcatton@gmail.com>
2020-03-12 10:34:01 +0000

    First push up

---
+++ repos/fdbt-netex-output/src/netex-convertor/netexValidator.test.ts
@@ -0,0 +1,12 @@
+import { validateNetex } from './netexValidator';
+import fs from 'fs';
+import path from 'path';
+
+describe('Netex Validator', () => {
+    it('', () => {
+        const result = validateNetex(fs.readFileSync(path.resolve(__dirname, '../netex-convertor/testdata/currentNetex.xml'),'utf-8'));
+
+        expect(result).toBe({})
+        
+    });
+});
