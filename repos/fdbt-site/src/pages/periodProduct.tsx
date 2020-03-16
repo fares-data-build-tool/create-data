8299d7ba508e05dae5da34e019fb91cb5329eb70
Danny Davies <danny.davies@infinityworks.com>
2020-03-16 17:07:40 +0000

    Added hint text to periodProduct page for Product Name

--- repos/fdbt-site/src/pages/periodProduct.tsx
+++ repos/fdbt-site/src/pages/periodProduct.tsx
@@ -24,57 +24,62 @@ const PeriodProduct = ({ product, operator, zoneName }: PeriodProduct): ReactEle
         <Layout title={title} description={description}>
             <main className="govuk-main-wrapper app-main-class" id="main-content" role="main">
                 <form action="/api/periodProduct" method="post">
-                    <fieldset className="govuk-fieldset" aria-describedby="period-product-page-heading">
-                        <legend className="govuk-fieldset__legend govuk-fieldset__legend--xl">
-                            <h1 className="govuk-fieldset__heading" id="period-product-page-heading">
-                                Enter your product details
-                            </h1>
-                        </legend>
-                        <span className="govuk-hint" id="service-operator-hint">
-                            {operator} - {zoneName}
-                        </span>
-                    </fieldset>
-                    <div className={`govuk-form-group ${productNameError ? 'govuk-form-group--error' : ''}`}>
-                        <label className="govuk-label" htmlFor="periodProductName">
-                            Product Name
-                        </label>
-                        <span id="product-price-error" className="govuk-error-message">
-                            <span className={productNameError ? '' : 'govuk-visually-hidden'}>
-                                Product name is a required field
+                    <div className="govuk-form-group">
+                        <fieldset className="govuk-fieldset" aria-describedby="period-product-page-heading">
+                            <legend className="govuk-fieldset__legend govuk-fieldset__legend--xl">
+                                <h1 className="govuk-fieldset__heading" id="period-product-page-heading">
+                                    Enter your product details
+                                </h1>
+                            </legend>
+                            <span className="govuk-hint" id="service-operator-hint">
+                                {operator} - {zoneName}
                             </span>
-                        </span>
-                        <input
-                            className={`govuk-input govuk-input--width-30 stage-name-input-field govuk-currency-input__inner__input ${
-                                productNameError ? 'govuk-input--error' : ''
-                            } `}
-                            id="periodProductName"
-                            name="periodProductNameInput"
-                            type="text"
-                            maxLength={30}
-                            defaultValue={productName}
-                        />
-                    </div>
-                    <div className={`govuk-form-group ${productPriceError ? 'govuk-form-group--error' : ''}`}>
-                        <label className="govuk-label" htmlFor="periodProductName">
-                            Product Price
-                        </label>
-                        <span className="govuk-hint" id="product-price-hint">
-                            Please enter the price in pence for example £1.20 would be 120
-                        </span>
-                        <span id="product-price-error" className="govuk-error-message">
-                            <span className={productPriceError ? '' : 'govuk-visually-hidden'}>
-                                Product price is a required field
+                        </fieldset>
+                        <div className={`govuk-form-group ${productNameError ? 'govuk-form-group--error' : ''}`}>
+                            <label className="govuk-label" htmlFor="periodProductName">
+                                Product Name
+                            </label>
+                            <span className="govuk-hint" id="product-price-hint">
+                                Please enter the name of your product
                             </span>
-                        </span>
-                        <input
-                            className={`govuk-input govuk-input--width-10 govuk-currency-input__inner__input ${
-                                productPriceError ? 'govuk-input--error' : ''
-                            } `}
-                            id="periodProductPrice"
-                            name="periodProductPriceInput"
-                            type="text"
-                            defaultValue={productPrice}
-                        />
+                            <span id="product-price-error" className="govuk-error-message">
+                                <span className={productNameError ? '' : 'govuk-visually-hidden'}>
+                                    Product name is a required field
+                                </span>
+                            </span>
+                            <input
+                                className={`govuk-input govuk-input--width-30 govuk-currency-input__inner__input ${
+                                    productNameError ? 'govuk-input--error' : ''
+                                } `}
+                                id="periodProductName"
+                                name="periodProductNameInput"
+                                type="text"
+                                maxLength={30}
+                                defaultValue={productName}
+                            />
+                        </div>
+                        <div className={`govuk-form-group ${productPriceError ? 'govuk-form-group--error' : ''}`}>
+                            <label className="govuk-label" htmlFor="periodProductName">
+                                Product Price
+                            </label>
+                            <span className="govuk-hint" id="product-price-hint">
+                                Please enter the price in pence for example £1.20 would be 120
+                            </span>
+                            <span id="product-price-error" className="govuk-error-message">
+                                <span className={productPriceError ? '' : 'govuk-visually-hidden'}>
+                                    Product price is a required field
+                                </span>
+                            </span>
+                            <input
+                                className={`govuk-input govuk-input--width-10 govuk-currency-input__inner__input ${
+                                    productPriceError ? 'govuk-input--error' : ''
+                                } `}
+                                id="periodProductPrice"
+                                name="periodProductPriceInput"
+                                type="text"
+                                defaultValue={productPrice}
+                            />
+                        </div>
                     </div>
                     <input
                         type="submit"
