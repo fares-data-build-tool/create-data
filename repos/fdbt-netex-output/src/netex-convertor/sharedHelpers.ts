ea0eeb6a090aac8713cf8f3f85693b840d83a9cb
Laurence Jones <laurence.jones@infinityworks.com>
2020-03-25 11:50:12 +0000

    Update audit step in pipeline

--- repos/fdbt-netex-output/src/netex-convertor/sharedHelpers.ts
+++ repos/fdbt-netex-output/src/netex-convertor/sharedHelpers.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
-export const getCleanWebsite = (nocEmail: string): string => {
-    const splitEmail = nocEmail.split('#');
+export const getCleanWebsite = (nocWebsite: string): string => {
+    const splitWebsite = nocWebsite.split('#');
 
-    return splitEmail.length > 1 && splitEmail[1] ? splitEmail[1] : splitEmail[0];
+    return splitWebsite.length > 1 && splitWebsite[1] ? splitWebsite[1] : splitWebsite[0];
 };
