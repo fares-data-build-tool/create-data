bfe7baf4b5c529e856d83e52a1717550a3b5435f
Rob Catton <robmcatton@gmail.com>
2020-03-30 11:37:40 +0100

    Deleted old uploaders

--- fdbt-reference-data-service/src/uploaders/handlers/nptg-uploader/handler.ts
+++
@@ -1,8 +0,0 @@
-import { Handler, S3Event } from 'aws-lambda';
-
-const s3NptgHandler: Handler = (event: S3Event) => {
-    console.log('==== This is my event object ====');
-    console.log(JSON.stringify(event));
-};
-
-export default s3NptgHandler;
