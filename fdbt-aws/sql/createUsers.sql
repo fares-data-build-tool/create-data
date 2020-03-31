6ab85ea9851460bb2038de8c42368fc63a4b70ec
Laurence Jones <laurence.jones@infinityworks.com>
2020-03-31 10:59:04 +0100

    Updated mysql usernames

--- fdbt-aws/sql/createUsers.sql
+++ fdbt-aws/sql/createUsers.sql
@@ -1,11 +1,11 @@
-CREATE USER 'fdbt_reference_data' @'%' IDENTIFIED BY '<INSERT PASSWORD>';
+CREATE USER 'fdbt_ref_data' @'%' IDENTIFIED BY '<INSERT PASSWORD>';
 
-GRANT SELECT, INSERT, DELETE, DROP, REFERENCES, LOAD FROM S3 ON fdbt.* TO 'fdbt_reference_data' @'%';
+GRANT SELECT, INSERT, DELETE, DROP, REFERENCES, LOAD FROM S3 ON fdbt.* TO 'fdbt_ref_data' @'%';
 
 CREATE USER 'fdbt_site' @'%' IDENTIFIED BY '<INSERT PASSWORD>';
 
 GRANT SELECT ON fdbt.* TO 'fdbt_site' @'%';
 
-CREATE USER 'fdbt_netex_output' @'%' IDENTIFIED BY '<INSERT PASSWORD>';
+CREATE USER 'fdbt_netex' @'%' IDENTIFIED BY '<INSERT PASSWORD>';
 
-GRANT SELECT ON fdbt.* TO 'fdbt_netex_output' @'%';
+GRANT SELECT ON fdbt.* TO 'fdbt_netex' @'%';
