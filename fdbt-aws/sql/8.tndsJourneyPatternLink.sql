c87448a3b90c75e7de3ae3aebf0856ead0aa9bb2
dannydaviesIW <55277550+dannydaviesIW@users.noreply.github.com>
2020-04-06 11:48:22 +0100

    fix: Remove duplicate SQL files, fix use of Stage parameter (#2)

--- fdbt-aws/sql/8.tndsJourneyPatternLink.sql
+++ fdbt-aws/sql/8.tndsJourneyPatternLink.sql
@@ -10,7 +10,7 @@ CREATE TABLE tndsJourneyPatternLink(
     `toAtcoCode` varchar(255) NOT NULL,
     `toTimingStatus` varchar(255) DEFAULT NULL,
     `runtime` varchar(255) DEFAULT NULL,
-    `order` int(11) NOT NULL,
+    `orderInSequence` int(11) NOT NULL,
     INDEX idx_journeyPatternSectionId (journeyPatternSectionId),
     CONSTRAINT fk_tndsJourneyPatternLink_tndsJourneyPatternSection_id FOREIGN KEY (journeyPatternSectionId) REFERENCES tndsJourneyPatternSection(id),
     CONSTRAINT fk_tndsJourneyPatternLink_fromAtcoCode_naptanStop_atcoCode FOREIGN KEY (fromAtcoCode) REFERENCES naptanStop(atcoCode),
--- fdbt-aws/sql/tndsJourneyPatternSection.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS tndsJourneyPatternSection;
-
-CREATE TABLE tndsJourneyPatternSection(
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `operatorServiceId` int(11) NOT NULL,
-    INDEX idx_operatorServiceId (operatorServiceId),
-    CONSTRAINT fk_tndsJourneyPatternSection_tndsOperatorService_id FOREIGN KEY (operatorServiceId) REFERENCES tndsOperatorService(id),
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB CHARACTER SET = utf8;
