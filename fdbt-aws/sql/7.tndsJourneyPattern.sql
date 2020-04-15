f5dd3c67152edee3108fc56b416d9a12e9ef4857
Laurence Jones <laurence.jones@infinityworks.com>
2020-04-15 09:16:13 +0100

    fix: updated DB schema to use journeyPattern in place of journeyPatternSection

--- fdbt-aws/sql/7.tndsJourneyPattern.sql
+++ fdbt-aws/sql/7.tndsJourneyPattern.sql
@@ -1,12 +1,14 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 
-DROP TABLE IF EXISTS tndsJourneyPatternSection;
+DROP TABLE IF EXISTS tndsJourneyPattern;
 
-CREATE TABLE tndsJourneyPatternSection(
+CREATE TABLE tndsJourneyPattern(
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `operatorServiceId` int(11) NOT NULL,
+    `destinationDisplay` varchar(255) DEFAULT NULL,
+    `direction` varchar(50) DEFAULT NULL,
     INDEX idx_operatorServiceId (operatorServiceId),
-    CONSTRAINT fk_tndsJourneyPatternSection_tndsOperatorService_id FOREIGN KEY (operatorServiceId) REFERENCES tndsOperatorService(id),
+    CONSTRAINT fk_tndsJourneyPattern_tndsOperatorService_id FOREIGN KEY (operatorServiceId) REFERENCES tndsOperatorService(id),
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB CHARACTER SET = utf8;
 
--- fdbt-aws/sql/8.tndsJourneyPatternLink.sql
+++ fdbt-aws/sql/8.tndsJourneyPatternLink.sql
@@ -4,15 +4,15 @@ DROP TABLE IF EXISTS tndsJourneyPatternLink;
 
 CREATE TABLE tndsJourneyPatternLink(
     `id` int(11) NOT NULL AUTO_INCREMENT,
-    `journeyPatternSectionId` int(11) NOT NULL,
+    `journeyPatternId` int(11) NOT NULL,
     `fromAtcoCode` varchar(255) NOT NULL,
     `fromTimingStatus` varchar(255) DEFAULT NULL,
     `toAtcoCode` varchar(255) NOT NULL,
     `toTimingStatus` varchar(255) DEFAULT NULL,
     `runtime` varchar(255) DEFAULT NULL,
     `orderInSequence` int(11) NOT NULL,
-    INDEX idx_journeyPatternSectionId (journeyPatternSectionId),
-    CONSTRAINT fk_tndsJourneyPatternLink_tndsJourneyPatternSection_id FOREIGN KEY (journeyPatternSectionId) REFERENCES tndsJourneyPatternSection(id),
+    INDEX idx_journeyPatternId (journeyPatternId),
+    CONSTRAINT fk_tndsJourneyPatternLink_tndsJourneyPattern_id FOREIGN KEY (journeyPatternId) REFERENCES tndsJourneyPattern(id),
     CONSTRAINT fk_tndsJourneyPatternLink_fromAtcoCode_naptanStop_atcoCode FOREIGN KEY (fromAtcoCode) REFERENCES naptanStop(atcoCode),
     CONSTRAINT fk_tndsJourneyPatternLink_toAtcoCode_naptanStop_atcoCode FOREIGN KEY (toAtcoCode) REFERENCES naptanStop(atcoCode),
     PRIMARY KEY (`id`)
