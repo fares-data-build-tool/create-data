d21282838a02ea74c0b4d6624280842ddc667302
Laurence Jones <laurence.jones@infinityworks.com>
2020-04-20 14:11:50 +0100

    feat: Move db cleardown to retriever rather than unzipper

--- repos/fdbt-reference-data-service/src/retrievers/tnds-ftp-retriever/unzipper.py
+++ repos/fdbt-reference-data-service/src/retrievers/tnds-ftp-retriever/unzipper.py
@@ -1,32 +1,15 @@
 import urllib.parse
 import boto3
 import os
-import pymysql
 import logging
 from zipfile import ZipFile
 
 file_dir = '/tmp/file.zip'
 
 s3 = boto3.client('s3')
-ssm = boto3.client('ssm')
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
-rds_host = os.getenv('RDS_HOST')
-db_name = "fdbt"
-
-username = ssm.get_parameter(
-    Name='fdbt-rds-reference-data-username',
-    WithDecryption=True
-)['Parameter']['Value']
-
-password = ssm.get_parameter(
-    Name='fdbt-rds-reference-data-password',
-    WithDecryption=True
-)['Parameter']['Value']
-
-db_connection = pymysql.connect(rds_host, user=username, passwd=password, db=db_name, connect_timeout=5)
 
 queries = [
     'SET FOREIGN_KEY_CHECKS=0',
@@ -35,20 +18,6 @@ queries = [
     'TRUNCATE TABLE tndsJourneyPatternLink',
     'SET FOREIGN_KEY_CHECKS=1'
 ]
-
-
-def cleardown_tnds_tables():
-    try:
-        with db_connection.cursor() as cursor:
-            for query in queries:
-                cursor.execute(query)
-
-        db_connection.commit()
-
-    except Exception as e:
-        logger.error('ERROR: Failed to truncate tables')
-        logger.error(e)
-        raise e
 
 
 def lambda_handler(event, context):
@@ -60,8 +29,6 @@ def lambda_handler(event, context):
         zipfile = ZipFile(file_dir)
 
         key_base = os.path.splitext(key)[0]
-
-        cleardown_tnds_tables()
 
         for filename in zipfile.namelist():
             if filename.endswith('.xml'):
