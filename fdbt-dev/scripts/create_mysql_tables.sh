b0a0ed4add380b8b2d2c6cc62daeddc77d06c2f9
Laurence Jones <laurence.jones@infinityworks.com>
2020-04-22 11:24:36 +0100

    fix: corrected load from s3 replacement script

--- fdbt-dev/scripts/create_mysql_tables.sh
+++ fdbt-dev/scripts/create_mysql_tables.sh
@@ -3,8 +3,8 @@
 set -e
 
 for i in $(ls -1 $FDBT_ROOT/fdbt-aws/sql/*.sql); do
-    removed_s3=$(sed 's/, LOAD FROM S3//g' $i)
-    updated_password=$(echo $removed_s3 | sed 's/<INSERT PASSWORD>/password/g')
+    removed_s3=$(sed "s/GRANT LOAD FROM S3 ON *.* TO 'fdbt_ref_data'@'%';//g" $i)
+    updated_password=$(echo "$removed_s3" | sed 's/<INSERT PASSWORD>/password/g')
     mysql -h127.0.0.1 -uroot -proot fdbt -e "$updated_password" 2>/dev/null
 done
 
