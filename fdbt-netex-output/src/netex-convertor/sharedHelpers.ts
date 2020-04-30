79395acf4c1295f7cac2968a41a3ce9cf1a84bca
Giles Drinkall <55278762+gilesdrinkall@users.noreply.github.com>
2020-04-30 10:44:48 +0100

    feat: NeTEX creation for period tickets across multiple services.
    * Reorganised structure so that there is now just the one period ticket generator
    
    * add ability to run generator locally

--- fdbt-netex-output/src/netex-convertor/sharedHelpers.ts
+++ fdbt-netex-output/src/netex-convertor/sharedHelpers.ts
@@ -1,3 +1,10 @@
+import parser from 'xml2json';
+import fs from 'fs';
+
+export interface NetexObject {
+    [key: string]: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+}
+
 // eslint-disable-next-line import/prefer-default-export
 export const getCleanWebsite = (nocWebsite: string): string => {
     if (nocWebsite !== null) {
@@ -6,4 +13,22 @@ export const getCleanWebsite = (nocWebsite: string): string => {
         return splitWebsite.length > 1 && splitWebsite[1] ? splitWebsite[1] : splitWebsite[0];
     }
     return '';
+};
+
+export const getNetexTemplateAsJson = async (filepath: string): Promise<NetexObject> => {
+    try {
+        const fileData = await fs.promises.readFile(`${__dirname}/${filepath}`, { encoding: 'utf8' });
+        const json = JSON.parse(parser.toJson(fileData, { reversible: true, trim: true }));
+
+        return json;
+    } catch (error) {
+        throw new Error(`Error converting NeTEx template to JSON: ${error.stack}`);
+    }
+};
+
+export const convertJsonToXml = (netexFileAsJsonObject: NetexObject): string => {
+    const netexFileAsJsonString = JSON.stringify(netexFileAsJsonObject);
+    const netexFileAsXmlString = parser.toXml(netexFileAsJsonString, { sanitize: true, ignoreNull: true });
+
+    return netexFileAsXmlString;
 };
--- fdbt-netex-output/src/netex-convertor/testdata/test-data.ts
+++ fdbt-netex-output/src/netex-convertor/testdata/test-data.ts
@@ -46,351 +46,6 @@ export const mockS3Event = (bucketName: string, fileName: string): S3Event => ({
     ],
 });
 
-export const mockS3ObjectDataAsString =
-    '[\n' +
-    '  {\n' +
-    '    "stopName": "Piccadilly",\n' +
-    '    "naptanCode": 32900103,\n' +
-    '    "fareStage": "Piccadilly (York)"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Low Ousegate",\n' +
-    '    "naptanCode": 32900100,\n' +
-    '    "fareStage": "Piccadilly (York)"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Rougier Street",\n' +
-    '    "naptanCode": 32900922,\n' +
-    '    "fareStage": "Piccadilly (York)"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Rail Station",\n' +
-    '    "naptanCode": 32900136,\n' +
-    '    "fareStage": "Rail Station (York)"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Blossom Street",\n' +
-    '    "naptanCode": 32900149,\n' +
-    '    "fareStage": "Blossom Street"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Cambridge Street",\n' +
-    '    "naptanCode": 32900193,\n' +
-    '    "fareStage": "Cambridge Street (York)"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Holgate Hill",\n' +
-    '    "naptanCode": 32900192,\n' +
-    '    "fareStage": "Cambridge Street (York)"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Holly Bank Road",\n' +
-    '    "naptanCode": 32901609,\n' +
-    '    "fareStage": "Holl Bank/Beech Ave"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Hob Moor Drive",\n' +
-    '    "naptanCode": 32901611,\n' +
-    '    "fareStage": "Holl Bank/Beech Ave"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Collingwood Avenue",\n' +
-    '    "naptanCode": 32901607,\n' +
-    '    "fareStage": "Holl Bank/Beech Ave"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Campbell Avenue",\n' +
-    '    "naptanCode": 32901666,\n' +
-    '    "fareStage": "Nursery Drive"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Mattison Way",\n' +
-    '    "naptanCode": 32900359,\n' +
-    '    "fareStage": "Acomb Green Lane"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Queenswood Grove",\n' +
-    '    "naptanCode": 32903623,\n' +
-    '    "fareStage": "Acomb Green Lane"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Kingsthorpe",\n' +
-    '    "naptanCode": 32900077,\n' +
-    '    "fareStage": "Acomb Green Lane"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Green Lane",\n' +
-    '    "naptanCode": 32900928,\n' +
-    '    "fareStage": "Acomb Green Lane"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Green Lane",\n' +
-    '    "naptanCode": 32900075,\n' +
-    '    "fareStage": "Acomb Green Lane"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Harold Court",\n' +
-    '    "naptanCode": 32900076,\n' +
-    '    "fareStage": "Acomb Green Lane"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Kingsthorpe",\n' +
-    '    "naptanCode": 32900809,\n' +
-    '    "fareStage": "Acomb Green Lane"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Queenswood Grove",\n' +
-    '    "naptanCode": 32900870,\n' +
-    '    "fareStage": "Acomb Green Lane"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Mattison Way",\n' +
-    '    "naptanCode": 32900358,\n' +
-    '    "fareStage": "Acomb Green Lane"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Campbell Avenue",\n' +
-    '    "naptanCode": 32900357,\n' +
-    '    "fareStage": "Nursery Drive"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Collingwood Avenue",\n' +
-    '    "naptanCode": 32901606,\n' +
-    '    "fareStage": "Holl Bank/Beech Ave"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Hob Moor Drive",\n' +
-    '    "naptanCode": 32901610,\n' +
-    '    "fareStage": "Holl Bank/Beech Ave"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Holly Bank Road",\n' +
-    '    "naptanCode": 32901608,\n' +
-    '    "fareStage": "Holl Bank/Beech Ave"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Barbara Grove",\n' +
-    '    "naptanCode": 32903583,\n' +
-    '    "fareStage": "Cambridge Street (York)"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Holgate Hill",\n' +
-    '    "naptanCode": 32900195,\n' +
-    '    "fareStage": "Cambridge Street (York)"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Cambridge Street",\n' +
-    '    "naptanCode": 32900194,\n' +
-    '    "fareStage": "Blossom Street"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Blossom Street",\n' +
-    '    "naptanCode": 32900152,\n' +
-    '    "fareStage": "Blossom Street"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Rail Station",\n' +
-    '    "naptanCode": 32900141,\n' +
-    '    "fareStage": "Rail Station (York)"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Rougier Street",\n' +
-    '    "naptanCode": 32900924,\n' +
-    '    "fareStage": "Piccadilly (York)"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Micklegate",\n' +
-    '    "naptanCode": 32900099,\n' +
-    '    "fareStage": "Piccadilly (York)"\n' +
-    '  },\n' +
-    '  {\n' +
-    '    "stopName": "Merchantgate",\n' +
-    '    "naptanCode": 32900107,\n' +
-    '    "fareStage": "Piccadilly (York)"\n' +
-    '  }\n' +
-    ']';
-
-export const mockS3ObjectDataAsJson = [
-    {
-        stopName: 'Piccadilly',
-        naptanCode: 32900103,
-        fareStage: 'Piccadilly (York)',
-    },
-    {
-        stopName: 'Low Ousegate',
-        naptanCode: 32900100,
-        fareStage: 'Piccadilly (York)',
-    },
-    {
-        stopName: 'Rougier Street',
-        naptanCode: 32900922,
-        fareStage: 'Piccadilly (York)',
-    },
-    {
-        stopName: 'Rail Station',
-        naptanCode: 32900136,
-        fareStage: 'Rail Station (York)',
-    },
-    {
-        stopName: 'Blossom Street',
-        naptanCode: 32900149,
-        fareStage: 'Blossom Street',
-    },
-    {
-        stopName: 'Cambridge Street',
-        naptanCode: 32900193,
-        fareStage: 'Cambridge Street (York)',
-    },
-    {
-        stopName: 'Holgate Hill',
-        naptanCode: 32900192,
-        fareStage: 'Cambridge Street (York)',
-    },
-    {
-        stopName: 'Holly Bank Road',
-        naptanCode: 32901609,
-        fareStage: 'Holl Bank/Beech Ave',
-    },
-    {
-        stopName: 'Hob Moor Drive',
-        naptanCode: 32901611,
-        fareStage: 'Holl Bank/Beech Ave',
-    },
-    {
-        stopName: 'Collingwood Avenue',
-        naptanCode: 32901607,
-        fareStage: 'Holl Bank/Beech Ave',
-    },
-    {
-        stopName: 'Campbell Avenue',
-        naptanCode: 32901666,
-        fareStage: 'Nursery Drive',
-    },
-    {
-        stopName: 'Mattison Way',
-        naptanCode: 32900359,
-        fareStage: 'Acomb Green Lane',
-    },
-    {
-        stopName: 'Queenswood Grove',
-        naptanCode: 32903623,
-        fareStage: 'Acomb Green Lane',
-    },
-    {
-        stopName: 'Kingsthorpe',
-        naptanCode: 32900077,
-        fareStage: 'Acomb Green Lane',
-    },
-    {
-        stopName: 'Green Lane',
-        naptanCode: 32900928,
-        fareStage: 'Acomb Green Lane',
-    },
-    {
-        stopName: 'Green Lane',
-        naptanCode: 32900075,
-        fareStage: 'Acomb Green Lane',
-    },
-    {
-        stopName: 'Harold Court',
-        naptanCode: 32900076,
-        fareStage: 'Acomb Green Lane',
-    },
-    {
-        stopName: 'Kingsthorpe',
-        naptanCode: 32900809,
-        fareStage: 'Acomb Green Lane',
-    },
-    {
-        stopName: 'Queenswood Grove',
-        naptanCode: 32900870,
-        fareStage: 'Acomb Green Lane',
-    },
-    {
-        stopName: 'Mattison Way',
-        naptanCode: 32900358,
-        fareStage: 'Acomb Green Lane',
-    },
-    {
-        stopName: 'Campbell Avenue',
-        naptanCode: 32900357,
-        fareStage: 'Nursery Drive',
-    },
-    {
-        stopName: 'Collingwood Avenue',
-        naptanCode: 32901606,
-        fareStage: 'Holl Bank/Beech Ave',
-    },
-    {
-        stopName: 'Hob Moor Drive',
-        naptanCode: 32901610,
-        fareStage: 'Holl Bank/Beech Ave',
-    },
-    {
-        stopName: 'Holly Bank Road',
-        naptanCode: 32901608,
-        fareStage: 'Holl Bank/Beech Ave',
-    },
-    {
-        stopName: 'Barbara Grove',
-        naptanCode: 32903583,
-        fareStage: 'Cambridge Street (York)',
-    },
-    {
-        stopName: 'Holgate Hill',
-        naptanCode: 32900195,
-        fareStage: 'Cambridge Street (York)',
-    },
-    {
-        stopName: 'Cambridge Street',
-        naptanCode: 32900194,
-        fareStage: 'Blossom Street',
-    },
-    {
-        stopName: 'Blossom Street',
-        naptanCode: 32900152,
-        fareStage: 'Blossom Street',
-    },
-    {
-        stopName: 'Rail Station',
-        naptanCode: 32900141,
-        fareStage: 'Rail Station (York)',
-    },
-    {
-        stopName: 'Rougier Street',
-        naptanCode: 32900924,
-        fareStage: 'Piccadilly (York)',
-    },
-    {
-        stopName: 'Micklegate',
-        naptanCode: 32900099,
-        fareStage: 'Piccadilly (York)',
-    },
-    {
-        stopName: 'Merchantgate',
-        naptanCode: 32900107,
-        fareStage: 'Piccadilly (York)',
-    },
-];
-
-export const mockDynamoDBItemDataObjectWithAttributeValueAsString: AWS.DynamoDB.DocumentClient.QueryOutput = {
-    Items: [{ testattribute: 'test' }],
-    Count: 1,
-    ScannedCount: 1,
-};
-
-export const mockDynamoDBItemDataObjectWithAttributeValueAsStringArray: AWS.DynamoDB.DocumentClient.QueryOutput = {
-    Items: [{ testattribute: ['test1', 'test2'] }],
-    Count: 1,
-    ScannedCount: 1,
-};
-
-export const mockDynamoDBItemDataObjectWithAttributeValueAsObjectArray: AWS.DynamoDB.DocumentClient.QueryOutput = {
-    Items: [{ testattribute: [{ test1: 'aaaa', test2: 'bbbb' }] }],
-    Count: 1,
-    ScannedCount: 1,
-};
-
 export const fareZoneList = {
     lineName: '354',
     nocCode: 'TLCT',
@@ -739,6 +394,63 @@ export const expectedScheduledStopPointsList = [
     },
 ];
 
+export const expectedLinesList = [
+    {
+        version: '1.0',
+        id: 'op:4',
+        Name: {
+            $t: 'Line 4',
+        },
+        Description: { $t: 'Bognor - Crawley - Horsham' },
+        Url: { $t: 'abc.com' },
+        PublicCode: { $t: '4' },
+        PrivateCode: { type: 'noc', $t: 'BLAC_4' },
+        OperatorRef: { version: '1.0', ref: 'noc:BLAC' },
+        LineType: { $t: 'local' },
+    },
+    {
+        version: '1.0',
+        id: 'op:17',
+        Name: {
+            $t: 'Line 17',
+        },
+        Description: { $t: 'Crewkerne - Yeovil - Sherborne' },
+        Url: { $t: 'abc.com' },
+        PublicCode: { $t: '17' },
+        PrivateCode: { type: 'noc', $t: 'BLAC_17' },
+        OperatorRef: { version: '1.0', ref: 'noc:BLAC' },
+        LineType: { $t: 'local' },
+    },
+    {
+        version: '1.0',
+        id: 'op:2C',
+        Name: {
+            $t: 'Line 2C',
+        },
+        Description: { $t: 'Harrogate - Otley - Leeds' },
+        Url: { $t: 'abc.com' },
+        PublicCode: { $t: '2C' },
+        PrivateCode: { type: 'noc', $t: 'BLAC_2C' },
+        OperatorRef: { version: '1.0', ref: 'noc:BLAC' },
+        LineType: { $t: 'local' },
+    },
+];
+
+export const expectedLineRefList = [
+    {
+        version: '1.0',
+        ref: 'op:4',
+    },
+    {
+        version: '1.0',
+        ref: 'op:17',
+    },
+    {
+        version: '1.0',
+        ref: 'op:2C',
+    },
+];
+
 export const expectedTopographicProjectionsList = [
     {
         versionRef: 'nptg:EXTERNAL',
@@ -1046,7 +758,7 @@ export const mockSingleTicketMatchingDataUpload = {
     ],
 };
 
-export const mockPeriodTicketMatchingDataUpload = {
+export const mockPeriodGeoZoneTicketMatchingDataUpload = {
     operatorName: 'Pilkington Bus',
     type: 'periodGeoZone',
     zoneName: 'Test Town Centre',
@@ -1239,6 +951,59 @@ export const mockPeriodTicketMatchingDataUpload = {
     nocCode: 'NWBT',
 };
 
+export const mockPeriodMultiServicesTicketMatchingDataUpload = {
+    operatorName: 'Blackpool Transport',
+    type: 'periodMultipleServices',
+    productName: 'dfvdfvd',
+    productPrice: '123',
+    daysValid: '4',
+    expiryRules: 'endOfCalendarDay',
+    nocCode: 'BLAC',
+    selectedServices: [
+        {
+            lineName: '4',
+            startDate: '05/04/2020',
+            serviceDescription: 'BLACKPOOL - LYTHAM'
+        },
+        {
+            lineName: '17',
+            startDate: '05/04/2020',
+            serviceDescription: 'BLACKPOOL - LYTHAM'
+        },
+        {
+            line: '2C',
+            startDate: '05/04/2020',
+            serviceDescription: 'BLACKPOOL - LYTHAM'
+        },
+    ],
+};
+
+export const multiServicesPeriodData = {
+    operatorName: 'Blackpool Transport',
+    productName: 'dfvdfvd',
+    productPrice: '123',
+    daysValid: '4',
+    expiryRules: 'endOfCalendarDay',
+    nocCode: 'BLAC',
+    selectedServices: [
+        {
+            lineName: '4',
+            startDate: '05/04/2020',
+            serviceDescription: 'Bognor - Crawley - Horsham',
+        },
+        {
+            lineName: '17',
+            startDate: '05/04/2020',
+            serviceDescription: 'Crewkerne - Yeovil - Sherborne',
+        },
+        {
+            lineName: '2C',
+            startDate: '05/04/2020',
+            serviceDescription: 'Harrogate - Otley - Leeds',
+        },
+    ],
+};
+
 export const mockMatchingDataUploadWithNoType = {
     operatorName: 'IW Bus',
     zoneName: 'Test Town Centre',
@@ -1259,4 +1024,15 @@ export const mockMatchingDataUploadWithNoType = {
     daysValid: '31',
     expiryRules: 'endOfCalendarDay',
     nocCode: 'IW1234',
+};
+
+export const operatorData = {
+    website: 'abc.com',
+    ttrteEnq: 'info@abc.com',
+    operatorPublicName: 'ABC Ltd',
+    opId: '123123',
+    vosaPsvLicenseName: 'ABC Ltd',
+    fareEnq: 'info@abc.com',
+    complEnq: 'info@abc.com',
+    mode: 'bus'
 };
