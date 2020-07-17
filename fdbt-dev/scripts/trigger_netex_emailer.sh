aea9aaa1df84a6a9f3227237d1ddf3215b125ea5
Laurence Jones <laurence.jones@infinityworks.com>
2020-07-17 15:00:35 +0100

    fix: use correct bucket for triggering email

--- fdbt-dev/scripts/trigger_netex_emailer.sh
+++ fdbt-dev/scripts/trigger_netex_emailer.sh
@@ -3,14 +3,14 @@
 set -e
 
 FILE_NAME=$1
-UNVALIDATED_NETEX_BUCKET=fdbt-unvalidated-netex-data-dev
+NETEX_BUCKET=fdbt-netex-data-dev
 MATCHING_DATA_BUCKET=fdbt-matching-data-dev
 
-EVENT_DATA=$(cat $FDBT_ROOT/fdbt-dev/data/s3Events/putEvent.json | sed s/KEY_HERE/$FILE_NAME.json/g | sed s/BUCKET_HERE/$MATCHING_DATA_BUCKET/g)
+EVENT_DATA=$(cat $FDBT_ROOT/fdbt-dev/data/s3Events/putEvent.json | sed s/KEY_HERE/$FILE_NAME.xml/g | sed s/BUCKET_HERE/$NETEX_BUCKET/g)
 
 cd $FDBT_ROOT/repos/fdbt-netex-output/src/netex-emailer/
 
 rm -rf build
 
-NODE_ENV=development UNVALIDATED_NETEX_BUCKET=$UNVALIDATED_NETEX_BUCKET MATCHING_DATA_BUCKET=$MATCHING_DATA_BUCKET ts-node run-local.ts "$EVENT_DATA"
+NODE_ENV=development MATCHING_DATA_BUCKET=$MATCHING_DATA_BUCKET ts-node run-local.ts "$EVENT_DATA"
 
