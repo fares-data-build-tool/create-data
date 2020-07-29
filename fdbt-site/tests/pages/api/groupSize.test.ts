af621529597d010d5bea3099c6cb82456ed523ae
dannydaviesIW <55277550+dannydaviesIW@users.noreply.github.com>
2020-07-29 15:17:07 +0100

    feat: allow users to define the size of a group

---
+++ fdbt-site/tests/pages/api/groupSize.test.ts
@@ -0,0 +1,61 @@
+import groupSize, {
+    groupSizeSchema,
+    GroupTicketAttributeWithErrors,
+    GroupTicketAttribute,
+} from '../../../src/pages/api/groupSize';
+import { getMockRequestAndResponse } from '../../testData/mockData';
+import * as sessionUtils from '../../../src/utils/sessions';
+import { GROUP_TICKET_ATTRIBUTE } from '../../../src/constants';
+
+describe('groupSize', () => {
+    const updateSessionAttributeSpy = jest.spyOn(sessionUtils, 'updateSessionAttribute');
+
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    describe('groupSizeSchema', () => {
+        it.each([
+            ['', false],
+            ['  ', false],
+            ['-12', false],
+            ['1.23', false],
+            ['0', false],
+            ['1', true],
+            ['30', true],
+            ['31', false],
+            ['not a number', false],
+            [' 1    2   ', true],
+        ])('should validate that %s is %s', (candidate, validity) => {
+            const result = groupSizeSchema.isValidSync(candidate);
+            expect(result).toEqual(validity);
+        });
+    });
+
+    it('should update the GROUP_TICKET_ATTRIBUTE and redirect to itself (i.e. /groupSize) when there are validation errors', async () => {
+        const { req, res } = getMockRequestAndResponse({ body: { maxGroupSize: 'wrong input' } });
+        const groupTicketInfoWithErrors: GroupTicketAttributeWithErrors = {
+            maxGroupSize: 'wronginput',
+            errors: [
+                {
+                    errorMessage: 'Enter a whole number between 1 and 30',
+                    id: 'max-group-size',
+                    userInput: 'wronginput',
+                },
+            ],
+        };
+        await groupSize(req, res);
+        expect(updateSessionAttributeSpy).toHaveBeenCalledWith(req, GROUP_TICKET_ATTRIBUTE, groupTicketInfoWithErrors);
+        expect(res.writeHead).toBeCalledWith(302, { Location: '/groupSize' });
+    });
+
+    it('should update the GROUP_TICKET_ATTRIBUTE and redirect to /groupPassengerTypes when there are validation errors', async () => {
+        const { req, res } = getMockRequestAndResponse({ body: { maxGroupSize: '5' } });
+        const groupTicketInfo: GroupTicketAttribute = {
+            maxGroupSize: '5',
+        };
+        await groupSize(req, res);
+        expect(updateSessionAttributeSpy).toHaveBeenCalledWith(req, GROUP_TICKET_ATTRIBUTE, groupTicketInfo);
+        expect(res.writeHead).toBeCalledWith(302, { Location: '/groupPassengerTypes' });
+    });
+});
