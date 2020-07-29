9abfcc1e9d88563e78f7a505c5ce7aa7b2da02de
dannydaviesIW <55277550+dannydaviesIW@users.noreply.github.com>
2020-07-29 11:37:19 +0100

    fix: removed excess commas from predefined SOPs

--- fdbt-site/tests/pages/selectSalesOfferPackage.test.tsx
+++ fdbt-site/tests/pages/selectSalesOfferPackage.test.tsx
@@ -3,6 +3,10 @@ import { shallow } from 'enzyme';
 import SelectSalesOfferPackage, {
     SelectSalesOfferPackageProps,
     getServerSideProps,
+    defaultSalesOfferPackageOne,
+    defaultSalesOfferPackageTwo,
+    defaultSalesOfferPackageThree,
+    defaultSalesOfferPackageFour,
 } from '../../src/pages/selectSalesOfferPackage';
 import { getMockContext } from '../testData/mockData';
 import { getSalesOfferPackagesByNocCode } from '../../src/data/auroradb';
@@ -12,30 +16,46 @@ jest.mock('../../src/data/auroradb');
 
 describe('pages', () => {
     const selectSalesOfferPackagePropsInfoNoError: SelectSalesOfferPackageProps = {
-        salesOfferPackagesList: [],
-        error: [],
+        salesOfferPackagesList: [
+            defaultSalesOfferPackageOne,
+            defaultSalesOfferPackageTwo,
+            defaultSalesOfferPackageThree,
+            defaultSalesOfferPackageFour,
+        ],
+        errors: [],
     };
 
     const selectSalesOfferPackagePropsInfoWithError: SelectSalesOfferPackageProps = {
-        salesOfferPackagesList: [],
-        error: [{ errorMessage: 'Choose at least one service from the options', id: 'sales-offer-package-error' }],
+        salesOfferPackagesList: [
+            defaultSalesOfferPackageOne,
+            defaultSalesOfferPackageTwo,
+            defaultSalesOfferPackageThree,
+            defaultSalesOfferPackageFour,
+        ],
+        errors: [{ errorMessage: 'Choose at least one service from the options', id: 'sales-offer-package-error' }],
     };
 
     describe('serviceList', () => {
         it('should render correctly', () => {
-            // eslint-disable-next-line react/jsx-props-no-spreading
             const tree = shallow(
-                // eslint-disable-next-line react/jsx-props-no-spreading
-                <SelectSalesOfferPackage {...selectSalesOfferPackagePropsInfoNoError} csrfToken="" pageProps={[]} />,
+                <SelectSalesOfferPackage
+                    salesOfferPackagesList={selectSalesOfferPackagePropsInfoNoError.salesOfferPackagesList}
+                    errors={selectSalesOfferPackagePropsInfoNoError.errors}
+                    csrfToken=""
+                    pageProps={[]}
+                />,
             );
             expect(tree).toMatchSnapshot();
         });
 
         it('should render an error when an error message is passed through to props', () => {
-            // eslint-disable-next-line react/jsx-props-no-spreading
             const tree = shallow(
-                // eslint-disable-next-line react/jsx-props-no-spreading
-                <SelectSalesOfferPackage {...selectSalesOfferPackagePropsInfoWithError} csrfToken="" pageProps={[]} />,
+                <SelectSalesOfferPackage
+                    salesOfferPackagesList={selectSalesOfferPackagePropsInfoWithError.salesOfferPackagesList}
+                    errors={selectSalesOfferPackagePropsInfoWithError.errors}
+                    csrfToken=""
+                    pageProps={[]}
+                />,
             );
             expect(tree).toMatchSnapshot();
         });
@@ -75,7 +95,7 @@ describe('pages', () => {
                         };
                     },
                 );
-                expect(result.props.error.length).toBe(0);
+                expect(result.props.errors.length).toBe(0);
                 expect(result.props.salesOfferPackagesList).toEqual(expectedSalesOfferPackageList);
             });
 
