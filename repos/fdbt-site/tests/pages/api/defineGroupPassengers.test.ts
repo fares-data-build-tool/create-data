58f51965f5962f5fa641715c70003fead3506790
RobCatton <41433657+RobCatton@users.noreply.github.com>
2020-08-05 16:17:36 +0100

    feat: add page to allow defining a group ticket

---
+++ repos/fdbt-site/tests/pages/api/defineGroupPassengers.test.ts
@@ -0,0 +1,71 @@
+import defineGroupPassengers from '../../../src/pages/api/defineGroupPassengers';
+import { getMockRequestAndResponse } from '../../testData/mockData';
+import * as sessionUtils from '../../../src/utils/sessions';
+import { GROUP_PASSENGER_TYPES } from '../../../src/constants';
+
+describe('defineGroupPassengers', () => {
+    const writeHeadMock = jest.fn();
+    const updateSessionAttributeSpy = jest.spyOn(sessionUtils, 'updateSessionAttribute');
+
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    it('should return 302 redirect to /defineGroupPassengers if passengerType is missing from req.body', () => {
+        const { req, res } = getMockRequestAndResponse({
+            body: null,
+            uuid: {},
+            mockWriteHeadFn: writeHeadMock,
+        });
+        defineGroupPassengers(req, res);
+
+        expect(writeHeadMock).toBeCalledWith(302, {
+            Location: '/defineGroupPassengers',
+        });
+        expect(updateSessionAttributeSpy).toBeCalledWith(req, GROUP_PASSENGER_TYPES, {
+            errors: [
+                { errorMessage: 'Choose one or two passenger types from the options', id: 'passenger-types-error' },
+            ],
+        });
+    });
+
+    it('should return 302 redirect to /defineGroupPassengers with error on session if passengerType array size is greater than 2', () => {
+        const { req, res } = getMockRequestAndResponse({
+            body: {
+                passengerTypes: ['adult', 'child', 'student'],
+            },
+            uuid: {},
+            mockWriteHeadFn: writeHeadMock,
+        });
+        defineGroupPassengers(req, res);
+
+        expect(writeHeadMock).toBeCalledWith(302, {
+            Location: '/defineGroupPassengers',
+        });
+        expect(updateSessionAttributeSpy).toBeCalledWith(req, GROUP_PASSENGER_TYPES, {
+            errors: [
+                {
+                    errorMessage: 'Choose one or two passenger types - you cannot exceed this limit',
+                    id: 'passenger-types-error',
+                },
+            ],
+        });
+    });
+
+    it('should return 302 redirect to /defineGroupPassengers with error on session if passengerType array size is greater than 2', () => {
+        const input = ['adult', 'child'];
+        const { req, res } = getMockRequestAndResponse({
+            body: {
+                passengerTypes: input,
+            },
+            uuid: {},
+            mockWriteHeadFn: writeHeadMock,
+        });
+        defineGroupPassengers(req, res);
+
+        expect(writeHeadMock).toBeCalledWith(302, {
+            Location: '/definePassengerType',
+        });
+        expect(updateSessionAttributeSpy).toBeCalledWith(req, GROUP_PASSENGER_TYPES, input);
+    });
+});
