d79d2eb47134a8c5131683d35a16a83ea9c28908
RobCatton <41433657+RobCatton@users.noreply.github.com>
2020-08-25 11:41:25 +0100

    feat: allow a user to register with multiple NOCs

--- repos/fdbt-site/src/pages/api/register.ts
+++ repos/fdbt-site/src/pages/api/register.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import isArray from 'lodash/isArray';
 import { redirectTo, redirectToError, setCookieOnResponseObject, checkEmailValid } from './apiUtils';
 import { USER_COOKIE } from '../../constants';
 import { InputCheck } from '../../interfaces';
@@ -85,7 +86,17 @@ export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> 
             if (ChallengeName === 'NEW_PASSWORD_REQUIRED' && ChallengeParameters?.userAttributes && Session) {
                 const parameters = JSON.parse(ChallengeParameters.userAttributes);
 
-                if (!parameters['custom:noc'] || parameters['custom:noc'] !== nocCode) {
+                const cognitoNocs: string[] | string = parameters['custom:noc'].split('|');
+
+                let valid = false;
+
+                if (isArray(cognitoNocs)) {
+                    valid = cognitoNocs.includes(nocCode);
+                } else {
+                    valid = cognitoNocs === nocCode;
+                }
+
+                if (cognitoNocs.length === 0 || !valid) {
                     logger.warn('', {
                         context: 'api.register',
                         message: 'NOC does not match',
