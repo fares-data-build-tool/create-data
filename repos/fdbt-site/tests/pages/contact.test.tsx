447aba8f0b893bf5145fb1c45cfba82b0b4248ba
Tej Powar <tej@powtech.co.uk>
2020-09-09 12:01:20 +0100

    feat: help text for all pages apart from contact

--- repos/fdbt-site/tests/pages/contact.test.tsx
+++ repos/fdbt-site/tests/pages/contact.test.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Contact from '../../src/pages/contact';
 
 describe('contact', () => {
     it('should render correctly', () => {
         const tree = shallow(<Contact />);
         expect(tree).toMatchSnapshot();
+    });
+
+    it('should not render the help text on the page', () => {
+        const wrapper = mount(<Contact />);
+        expect(wrapper.find('.govuk-link')).toHaveLength(0);
     });
 });
