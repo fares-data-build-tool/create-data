bfdf1d921e90d73be2271db2e9ae5e0f7c26bb36
RobCatton <41433657+RobCatton@users.noreply.github.com>
2021-02-09 10:45:17 +0000

    feat: change file save path for CSV zones

--- repos/fdbt-site/src/pages/api/csvZoneUpload.ts
+++ repos/fdbt-site/src/pages/api/csvZoneUpload.ts
@@ -2,7 +2,7 @@ import { NextApiResponse } from 'next';
 import csvParse from 'csv-parse/lib/sync';
 import { FARE_TYPE_ATTRIBUTE, FARE_ZONE_ATTRIBUTE } from '../../constants/index';
 import { getSessionAttribute, updateSessionAttribute } from '../../utils/sessions';
-import { getUuidFromCookie, redirectToError, redirectTo } from './apiUtils';
+import { getUuidFromCookie, redirectToError, redirectTo, getAndValidateNoc } from './apiUtils';
 import { putDataInS3 } from '../../data/s3';
 import { getAtcoCodesByNaptanCodes, batchGetStopsByAtcoCode } from '../../data/auroradb';
 import { isSessionValid } from './apiUtils/validator';
@@ -173,7 +173,11 @@ export default async (req: NextApiRequestWithSession, res: NextApiResponse): Pro
                 return;
             }
             const fareZoneName = userFareZones[0].FareZoneName;
-            await putDataInS3(userFareZones, `${uuid}.json`, true);
+            const nocCode = getAndValidateNoc(req, res);
+            if (!nocCode) {
+                throw new Error('Could not retrieve nocCode from ID_TOKEN_COOKIE');
+            }
+            await putDataInS3(userFareZones, `fare-zone/${nocCode}/${uuid}.json`, true);
             updateSessionAttribute(req, FARE_ZONE_ATTRIBUTE, fareZoneName);
             const { fareType } = getSessionAttribute(req, FARE_TYPE_ATTRIBUTE) as FareType;
             if (fareType === 'multiOperator') {
