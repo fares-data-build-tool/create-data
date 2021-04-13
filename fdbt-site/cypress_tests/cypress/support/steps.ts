7c7172b220c0142d8ccc5c811dab2de49994ef21
RobCatton <41433657+RobCatton@users.noreply.github.com>
2021-04-13 15:34:37 +0100

    test: cypress csv upload single

--- fdbt-site/cypress_tests/cypress/support/steps.ts
+++ fdbt-site/cypress_tests/cypress/support/steps.ts
@@ -6,14 +6,27 @@ import {
     randomlyDecideTimeRestrictions,
     randomlyChooseAndSelectServices,
     getElementById,
+    getHomePage,
+    fareTypeToFareTypeIdMapper,
+    selectRandomOptionFromDropDown,
+    uploadFile,
+    submitButtonClick,
+    clickSelectedNumberOfCheckboxes,
+    completeProductDateInformationPage,
 } from './helpers';
 
-export const stepsToSelectFlatFareServiceSelection = (): void => {
-    startPageButtonClick();
-    clickElementById('fare-type-flatFare');
-    continueButtonClick();
+export const defineUserTypeAndTimeRestrictions = (): void => {
     randomlyDetermineUserType();
     randomlyDecideTimeRestrictions();
+    continueButtonClick();
+};
+
+export const selectFareType = (
+    fareType: 'single' | 'period' | 'return' | 'flatFare' | 'multiOperator' | 'schoolService',
+): void => {
+    getHomePage();
+    startPageButtonClick();
+    clickElementById(fareTypeToFareTypeIdMapper(fareType));
     continueButtonClick();
 };
 
@@ -23,5 +36,49 @@ export const completeFlatFarePages = (productName: string): void => {
     getElementById('product-details-name').type(productName);
     getElementById('product-details-price').type('50.50');
     continueButtonClick();
+    continueButtonClick();
+};
+
+export const completeServicePage = (): void => {
+    selectRandomOptionFromDropDown('service');
+    continueButtonClick();
+};
+
+const completeFareTrianglePages = (csvUpload: boolean): void => {
+    clickElementById(csvUpload ? 'csv-upload' : 'manual-entry');
+    continueButtonClick();
+    if (csvUpload) {
+        clickElementById('pence');
+        uploadFile('csv-upload', 'fareTriangle.csv');
+        submitButtonClick();
+    }
+};
+
+const completeMatchingPage = (): void => {
+    for (let i = 0; i < 4; i += 1) {
+        cy.get(`[id=option-${i}]`)
+            .find('option')
+            .then($elm => {
+                $elm.get(i + 1).setAttribute('selected', 'selected');
+            })
+            .parent()
+            .trigger('change');
+    }
+    submitButtonClick();
+};
+
+export const completeSinglePages = (csvUpload: boolean): void => {
+    completeServicePage();
+    selectRandomOptionFromDropDown('direction-journey-pattern');
+    continueButtonClick();
+    completeFareTrianglePages(csvUpload);
+    completeMatchingPage();
+    continueButtonClick();
+};
+
+export const completeSalesPages = (): void => {
+    clickSelectedNumberOfCheckboxes(false);
+    continueButtonClick();
+    completeProductDateInformationPage();
     continueButtonClick();
 };
