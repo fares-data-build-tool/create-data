26f85b6a7233d1b4e8d51f6227200fe2aa626120
Jacob Ellis <jacob.ellis@infinityworks.com>
2021-06-14 16:56:14 +0100

    test: CFD-283 UI Tests for carnets

---
+++ repos/fdbt-site/cypress_tests/cypress/integration/carnet/period.ts
@@ -0,0 +1,19 @@
+import { isUuidStringValid } from '../../support/helpers';
+import {
+    completePeriodMultiServicePages,
+    completeSalesPages,
+    defineUserTypeAndTimeRestrictions,
+    selectCarnetFareType,
+} from '../../support/steps';
+
+describe('the period carnet product journey', () => {
+    it('completes successfully for multi-service and multiple products', () => {
+        const productNamePrefix = 'Carnet period product';
+
+        selectCarnetFareType('period');
+        defineUserTypeAndTimeRestrictions();
+        completePeriodMultiServicePages(4, productNamePrefix, true);
+        completeSalesPages(4, productNamePrefix);
+        isUuidStringValid();
+    });
+});
