c13b922d2cc30724432169c8629110f85b1ef126
Jacob Ellis <jacob.ellis@infinityworks.com>
2021-06-16 15:14:42 +0100

    Fix babel config for tests

--- repos/fdbt-site/babel.config.js
+++ repos/fdbt-site/babel.config.js
@@ -1,6 +1,8 @@
-module.exports = {
-  presets: ['next/babel', ['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-react', '@babel/preset-typescript'],
+module.exports = api => api.env('test') ? {
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-react', '@babel/preset-typescript'],
   plugins: [
     '@babel/plugin-proposal-optional-chaining',
   ],
+} : {
+  presets: ['next/babel']
 };
