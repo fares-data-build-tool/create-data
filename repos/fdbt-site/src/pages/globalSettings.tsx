ad70bfadadb08c3c1dc933d8501a7779b09ac25c
Jwan Khalaf <jwan.khalaf@outlook.com>
2021-07-08 16:36:26 +0100

    style the global settings page according to gov uk styles

--- repos/fdbt-site/src/pages/globalSettings.tsx
+++ repos/fdbt-site/src/pages/globalSettings.tsx
@@ -6,64 +6,97 @@ import { checkIfMultipleOperators } from '../utils';
 const title = 'Operator Settings';
 const description = 'View and access your settings in one place.';
 
-const Home = ({ multipleOperators }: HomeProps): ReactElement => (
-    <BaseLayout title={title} description={description}>
-        <h1 className="govuk-heading-l">Operator settings</h1>
-        <p className="govuk-body">View and access your settings in one place.</p>
-        <nav className="app-navigation govuk-clearfix">
-            <ul className="app-navigation__list app-width-container">
-                <li className="list-item">
-                    <a href="/globalSettings" className="govuk-link govuk-!-font-size-19" id="created-link">
-                        My Settings
-                    </a>
-                </li>
-            </ul>
-        </nav>
-        <p>
-            <a href="/settingsOverview" className="govuk-link govuk-!-font-size-19" id="created-link">
-                Settings overview
-            </a>
-        </p>
-        <p>
-            <a href="/predefineSettings" className="govuk-link govuk-!-font-size-19" id="created-link">
-                Define settings for ...
-            </a>
-        </p>
-        <p>
-            <a href="/predefinePassengerType" className="govuk-link govuk-!-font-size-19" id="created-link">
-                Passenger types
-            </a>
-        </p>
-        <p>
-            <a href="/predefineServiceDay" className="govuk-link govuk-!-font-size-19" id="created-link">
-                Service day end
-            </a>
-        </p>
-        <p>
-            <a href="/predefinePurchaseMethods" className="govuk-link govuk-!-font-size-19" id="created-link">
-                Purchase methods
-            </a>
-        </p>
-        <p>
-            <a href="/predefineTimeRestrictions" className="govuk-link govuk-!-font-size-19" id="created-link">
-                Time restrictions
-            </a>
-        </p>
-        <p>
-            <a href="/predefinePurchaseMethods" className="govuk-link govuk-!-font-size-19" id="created-link">
-                Multi-operator groups
-            </a>
-        </p>
-        <p>
-            <a href="/predefineTravelZones" className="govuk-link govuk-!-font-size-19" id="created-link">
-                Travel Zones
-            </a>
-        </p>
+const GlobalSettings = (): ReactElement => (
+    <BaseLayout title={title} description={description} showNavigation={true}>
+        <div className="govuk-width-container">
+            <main className="govuk-main-wrapper">
+                <div className="govuk-grid-row">
+                    <div className="govuk-grid-column-one-third">
+                        <div className="app-pane__subnav">
+                            <nav className="app-subnav" aria-labelledby="app-subnav-heading">
+                                <h2 className="govuk-visually-hidden" id="app-subnav-heading">
+                                    Pages in this section
+                                </h2>
+
+                                <ul className="app-subnav__section">
+                                    <li className="app-subnav__section-item app-subnav__section-item--current">
+                                        <a
+                                            className="app-subnav__link govuk-link govuk-link--no-visited-state govuk-link--no-underline"
+                                            href="/components/accordion/"
+                                        >
+                                            Settings overview
+                                        </a>
+                                    </li>
+
+                                    <li className="app-subnav__section-item">
+                                        <a
+                                            className="app-subnav__link govuk-link govuk-link--no-visited-state govuk-link--no-underline"
+                                            href="/components/back-link/"
+                                        >
+                                            Passenger types
+                                        </a>
+                                    </li>
+
+                                    <li className="app-subnav__section-item">
+                                        <a
+                                            className="app-subnav__link govuk-link govuk-link--no-visited-state govuk-link--no-underline"
+                                            href="/components/breadcrumbs/"
+                                        >
+                                            Service day end
+                                        </a>
+                                    </li>
+
+                                    <li className="app-subnav__section-item">
+                                        <a
+                                            className="app-subnav__link govuk-link govuk-link--no-visited-state govuk-link--no-underline"
+                                            href="/components/button/"
+                                        >
+                                            Purchase methods
+                                        </a>
+                                    </li>
+
+                                    <li className="app-subnav__section-item">
+                                        <a
+                                            className="app-subnav__link govuk-link govuk-link--no-visited-state govuk-link--no-underline"
+                                            href="/components/character-count/"
+                                        >
+                                            Time restrictions
+                                        </a>
+                                    </li>
+
+                                    <li className="app-subnav__section-item">
+                                        <a
+                                            className="app-subnav__link govuk-link govuk-link--no-visited-state govuk-link--no-underline"
+                                            href="/components/checkboxes/"
+                                        >
+                                            Multi-operator groups
+                                        </a>
+                                    </li>
+
+                                    <li className="app-subnav__section-item">
+                                        <a
+                                            className="app-subnav__link govuk-link govuk-link--no-visited-state govuk-link--no-underline"
+                                            href="/components/cookie-banner/"
+                                        >
+                                            Travel Zones
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <div className="govuk-grid-column-two-thirds">
+                        <h1 className="govuk-heading-xl">Operator Settings</h1>
+                        <p className="govuk-body">This is a paragraph inside a two-thirds wide column</p>
+                    </div>
+                </div>
+            </main>
+        </div>
     </BaseLayout>
 );
 
-export const getServerSideProps = (ctx: NextPageContextWithSession): { props: HomeProps } => ({
+export const getServerSideProps = (ctx: NextPageContextWithSession): { props: NextPageContextWithSession} => ({
     props: { multipleOperators: checkIfMultipleOperators(ctx) },
 });
 
-export default Home;
+export default GlobalSettings;
