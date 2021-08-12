b824486d60029a903350a17ac19482e5858c6a53
Aaron-Collis-IW <aaron.collis@infinityworks.com>
2021-08-12 16:49:04 +0100

    wip no verify

--- repos/fdbt-site/tests/pages/viewPassengerTypes.test.tsx
+++ repos/fdbt-site/tests/pages/viewPassengerTypes.test.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { shallow } from 'enzyme';
 import ViewPassengerTypes from '../../src/pages/viewPassengerTypes';
 import { SinglePassengerType } from '../../src/interfaces';
+import DeleteConfirmationPopup from 'src/components/DeleteConfirmationPopup';
+import UnableToDeletePopup from 'src/components/UnableToDeletePopup';
 
 describe('pages', () => {
     describe('view passenger types', () => {
@@ -116,5 +118,121 @@ describe('pages', () => {
             );
             expect(tree).toMatchSnapshot();
         });
+    });
+
+    // <DeleteConfirmationPopup
+    // {...popUpState}
+    // csrfToken={csrfToken}
+    // cancelActionHandler={cancelActionHandler}
+    // />
+
+    describe('delete passenger types', () => {
+        it('should render DeleteConformation popup when trying to delete passenger not in group', () => {
+            const passengerTypes: SinglePassengerType[] = [
+                {
+                    id: 1,
+                    name: 'Regular Child',
+                    passengerType: {
+                        passengerType: 'child',
+                        ageRangeMin: '5',
+                        ageRangeMax: '16',
+                        proofDocuments: ['studentCard'],
+                    },
+                },
+                {
+                    id: 2,
+                    name: 'Regular Adult',
+                    passengerType: {
+                        passengerType: 'adult',
+                        ageRangeMin: '18',
+                    },
+                },
+            ];
+
+            const adultCompanion = {
+                passengerType: 'adult',
+                maxNumber: '2',
+            };
+
+            const passengerTypeGroups = [
+                {
+                    id: 3,
+                    name: 'family group',
+                    groupPassengerType: {
+                        name: 'family group',
+                        maxGroupSize: '3',
+                        companions: [adultCompanion],
+                    },
+                },
+            ];
+            const tree = shallow(
+                <DeleteConfirmationPopup
+                // csrfToken=
+                // isVisible=
+                // isGroup=
+                // passengerTypeName=
+                // passengerTypeId=
+                // cancelActionHandler=
+                />,
+            );
+            expect(tree).toMatchSnapshot();
+        });
+        it('should render UnableToDelete when trying to delete passenger in group one group, and name that group', () => {
+            const passengerTypes: SinglePassengerType[] = [
+                {
+                    id: 1,
+                    name: 'Regular Child',
+                    passengerType: {
+                        passengerType: 'child',
+                        ageRangeMin: '5',
+                        ageRangeMax: '16',
+                        proofDocuments: ['studentCard'],
+                    },
+                },
+                {
+                    id: 2,
+                    name: 'Regular Adult',
+                    passengerType: {
+                        passengerType: 'adult',
+                        ageRangeMin: '18',
+                    },
+                },
+            ];
+
+            const adultCompanion = {
+                passengerType: 'adult',
+                maxNumber: '2',
+            };
+
+            const childCompanion = {
+                passengerType: 'child',
+                maxNumber: '1',
+                ageRangeMin: '4',
+                ageRangeMax: '16',
+                proofDocuments: ['studentCard'],
+            };
+
+            const passengerTypeGroups = [
+                {
+                    id: 3,
+                    name: 'family group',
+                    groupPassengerType: {
+                        name: 'family group',
+                        maxGroupSize: '3',
+                        companions: [adultCompanion, childCompanion],
+                    },
+                },
+            ];
+            const tree = shallow(
+                <UnableToDeletePopup
+                // isVisible=
+                // groupsInUse=
+                // passengerTypeName=
+                // cancelActionHandler=
+                />,
+            );
+            expect(tree).toMatchSnapshot();
+        });
+        it('should render UnableToDelete when trying to delete passenger in multiple groups, and name those groups', () => {});
     });
 });
