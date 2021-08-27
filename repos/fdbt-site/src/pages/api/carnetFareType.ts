80c82e22481f2fc41d4dfbfa8bf2438202ac2317
Rob Catton <rob.catton@gmail.com>
2021-08-27 09:40:51 +0100

    Fixed import

--- repos/fdbt-site/src/pages/api/carnetFareType.ts
+++ repos/fdbt-site/src/pages/api/carnetFareType.ts
@@ -28,7 +28,7 @@ export default (req: NextApiRequestWithSession, res: NextApiResponse): void => {
             }
         } else {
             const errors: ErrorInfo[] = [
-                { id: 'fare-type-single', errorMessage: 'Choose a carnet fare type from the options', testing: 'true' },
+                { id: 'fare-type-single', errorMessage: 'Choose a carnet fare type from the options' },
             ];
             updateSessionAttribute(req, FARE_TYPE_ATTRIBUTE, {
                 errors,
