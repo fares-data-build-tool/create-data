33a819b12c7247e33571bce73443ca6adfe89aed
Jacob Ellis <jacob.ellis@infinityworks.com>
2021-08-31 10:46:56 +0100

    chore: CFD-365 fix next logging webpack issue (#147)

--- repos/fdbt-site/src/utils/nextLogger.ts
+++ repos/fdbt-site/src/utils/nextLogger.ts
@@ -1,8 +1,5 @@
 import logger from './internalLogger';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const nextLogger = require('../../node_modules/next/dist/build/output/log');
-
 const passLogger = (level: string): ((obj: AnyError) => void) => {
     const logMethod = getLogMethod(level);
 
@@ -36,11 +33,6 @@ const getMessage = (obj: AnyError): string => {
 
 type AnyError = (Error & { [key: string]: unknown }) | string[] | string;
 
-Object.entries(nextLogger).forEach(([key, value]) => {
-    if (typeof value === 'function') {
-        nextLogger[key] = passLogger(key);
-    }
-});
 const consoleMethods: (keyof Console)[] = ['log', 'debug', 'info', 'warn', 'error'];
 
 consoleMethods.forEach((method) => {
