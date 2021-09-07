f237b160b7592138bcd7901844c8d765818891a4
Jwan Khalaf <jwan.khalaf@outlook.com>
2021-09-07 15:19:40 +0100

    get ui test on passenger types passing

--- repos/fdbt-site/cypress_tests/cypress/support/passengerTypes.ts
+++ repos/fdbt-site/cypress_tests/cypress/support/passengerTypes.ts
@@ -102,7 +102,7 @@ export const createEditSinglePassengerTypes = (): void => {
 };
 
 export const createEditGroupPassengerTypes = (): void => {
-    addGroupPassengerType('my Group');
+    addGroupPassengerType('my group');
 
     const group = getElementByClass('card').eq(2);
     group.should('include.text', 'my group');
