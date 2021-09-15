dd0b8c8966a59d48aaf6da962be18a2d8fa94854
Jacob Ellis <jacob.ellis@infinityworks.com>
2021-09-15 17:25:49 +0100

    fix: Error when parsing unassigned fare stages

--- repos/fdbt-site/src/utils/apiUtils/matching.ts
+++ repos/fdbt-site/src/utils/apiUtils/matching.ts
@@ -1,5 +1,4 @@
 import { NextApiRequest } from 'next';
-import isArray from 'lodash/isArray';
 import { Stop, UserFareStages } from '../../interfaces';
 import { MatchingFareZones, MatchingFareZonesData } from '../../interfaces/matchingInterface';
 
@@ -30,18 +29,20 @@ export const getMatchingFareZonesAndUnassignedStopsFromForm = (
     const bodyValues: string[] = Object.values(req.body);
     const unassignedStops: Stop[] = [];
 
-    bodyValues.forEach((stopSelection: string) => {
-        const stageName = stopSelection[0];
-
-        if (isArray(stopSelection) && stageName === 'notApplicable') {
+    bodyValues.forEach((stopSelection: string | string[]) => {
+        if (stopSelection === 'yes') {
+            // skip the 'yes' value
+        } else if (Array.isArray(stopSelection) && stopSelection[0] === 'notApplicable') {
             unassignedStops.push(JSON.parse(stopSelection[1]));
-        } else if (!isArray(stopSelection)) {
+        } else if (!Array.isArray(stopSelection)) {
             const item = JSON.parse(stopSelection);
-            if ('naptanCode' in item) {
+            if ('stopName' in item) {
                 unassignedStops.push(item);
             }
-        } else if (stageName && typeof stageName === 'string' && isArray(stopSelection)) {
+        } else if (Array.isArray(stopSelection) && stopSelection[0]) {
             const stop = JSON.parse(stopSelection[1]);
+
+            const stageName = stopSelection[0];
 
             if (matchingFareZones[stageName]) {
                 matchingFareZones[stageName].stops.push(stop);
