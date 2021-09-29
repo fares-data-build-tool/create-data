dc9a3211fa777959c6b7de2de6a8ae384884ac12
Jacob Ellis <jacob.ellis@infinityworks.com>
2021-09-29 09:49:38 +0100

    chore: Add alarms to export lambda (#206)

--- repos/exporter/lib/exporter-stack.ts
+++ repos/exporter/lib/exporter-stack.ts
@@ -1,9 +1,12 @@
 import * as cdk from '@aws-cdk/core';
-import { Fn } from '@aws-cdk/core';
-import { Bucket } from '@aws-cdk/aws-s3';
+import { Fn, Duration } from '@aws-cdk/core';
+import { Bucket, BucketEncryption } from '@aws-cdk/aws-s3';
 import { NodejsFunction, SourceMapMode } from '@aws-cdk/aws-lambda-nodejs';
 import { PolicyStatement } from '@aws-cdk/aws-iam';
 import { SecurityGroup, Subnet, Vpc } from '@aws-cdk/aws-ec2';
+import { Topic } from '@aws-cdk/aws-sns';
+import { SnsAction } from '@aws-cdk/aws-cloudwatch-actions';
+import { TreatMissingData } from '@aws-cdk/aws-cloudwatch';
 
 export class ExporterStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -16,6 +19,7 @@ export class ExporterStack extends cdk.Stack {
 
         const productsBucket = new Bucket(this, `products-data-bucket-${stage}`, {
             bucketName: `fdbt-products-data-${stage}`,
+            encryption: BucketEncryption.KMS_MANAGED,
         });
 
         const matchingDataBucket = Bucket.fromBucketName(
@@ -51,7 +55,35 @@ export class ExporterStack extends cdk.Stack {
                 sourceMap: true,
                 sourceMapMode: SourceMapMode.DEFAULT,
             },
+            timeout: Duration.minutes(5),
         });
+
+        const alarmTopicArn = Fn.importValue(`${stage}:SlackAlertsTopicArn`);
+        const alarmTopic = Topic.fromTopicArn(this, 'alarm-topic', alarmTopicArn);
+        const snsAction = new SnsAction(alarmTopic);
+
+        const errorsAlarm = exporterFunction
+            .metricErrors({ period: Duration.minutes(1) })
+            .createAlarm(this, 'exporter-errors-alarm', {
+                threshold: 1,
+                evaluationPeriods: 1,
+                treatMissingData: TreatMissingData.NOT_BREACHING,
+            });
+        const durationAlarm = exporterFunction
+            .metricDuration({ period: Duration.minutes(1), statistic: 'max' })
+            .createAlarm(this, 'exporter-duration-alarm', {
+                threshold: 30000,
+                evaluationPeriods: 1,
+                treatMissingData: TreatMissingData.NOT_BREACHING,
+            });
+
+        durationAlarm.addAlarmAction(snsAction);
+        durationAlarm.addInsufficientDataAction(snsAction);
+        durationAlarm.addOkAction(snsAction);
+
+        errorsAlarm.addAlarmAction(snsAction);
+        errorsAlarm.addInsufficientDataAction(snsAction);
+        errorsAlarm.addOkAction(snsAction);
 
         exporterFunction.addToRolePolicy(new PolicyStatement({ actions: ['ssm:GetParameter'], resources: ['*'] }));
 
