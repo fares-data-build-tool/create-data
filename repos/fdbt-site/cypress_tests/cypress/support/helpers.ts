3b5e89b3dba588a24fa8cc2c8ab4c908a965a076
Aaron-Collis-IW <77591337+Aaron-Collis-IW@users.noreply.github.com>
2021-11-17 12:08:30 +0000

    fix/cfd 532 fix UI tests (#310)

--- repos/fdbt-site/cypress_tests/cypress/support/helpers.ts
+++ repos/fdbt-site/cypress_tests/cypress/support/helpers.ts
@@ -427,18 +427,20 @@ export const completeProductDateInformationPage = (): void => {
     continueButtonClick();
 };
 
-export const isFinished = (isScheme = false): void => {
-    if (isScheme) {
-        getElementById('uuid-ref-number')
-            .invoke('text')
-            .then((rawUuid) => {
-                const uuid = rawUuid.replace('Your reference number', '');
-                expect(uuid).to.contain('TESTSE');
-                expect(uuid.length).to.equal(14);
-            });
-    } else {
-        getElementById('thank-you-page-heading').should('have.text', 'Product created');
-    }
+export const isFinished = (): void => {
+    cy.get('body').then((body) => {
+        if (body.find('uuid-ref-number').length > 0) {
+            getElementById('uuid-ref-number')
+                .invoke('text')
+                .then((rawUuid) => {
+                    const uuid = rawUuid.replace('Your reference number', '');
+                    expect(uuid).to.contain('TESTSE');
+                    expect(uuid.length).to.equal(14);
+                });
+        } else {
+            getElementById('thank-you-page-heading').should('have.text', 'Product created');
+        }
+    });
 };
 
 export const uploadFile = (elementId: string, fileName: string): void => {
