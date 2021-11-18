9445685091317587e4ccb6de7553be597bd4aae9
Rob Catton <rob.catton@gmail.com>
2021-11-18 10:09:22 +0000

    PR comments

--- repos/fdbt-site/tests/pages/products/exports.test.tsx
+++ repos/fdbt-site/tests/pages/products/exports.test.tsx
@@ -4,7 +4,14 @@ import Exports from '../../../src/pages/products/exports';
 
 describe('pages', () => {
     describe('confirmRegistration', () => {
-        it('should render correctly without data', () => {
+        it('should render correctly without data when operator has no products', () => {
+            const tree = shallow(
+                <Exports csrf={''} myFaresEnabled={true} exportEnabled={true} operatorHasProducts={false} />,
+            );
+            expect(tree).toMatchSnapshot();
+        });
+
+        it('should render the export button correctly when operator has products', () => {
             const tree = shallow(
                 <Exports csrf={''} myFaresEnabled={true} exportEnabled={true} operatorHasProducts={true} />,
             );
