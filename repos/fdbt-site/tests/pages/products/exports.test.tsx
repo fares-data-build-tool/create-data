0b2dd79d3d217abdf6c31b60dd1f565507693b17
RobCatton <41433657+RobCatton@users.noreply.github.com>
2021-11-18 10:21:51 +0000

    feat: added functionality for the progress bar to update according to the export progress (#315)

--- repos/fdbt-site/tests/pages/products/exports.test.tsx
+++ repos/fdbt-site/tests/pages/products/exports.test.tsx
@@ -4,37 +4,16 @@ import Exports from '../../../src/pages/products/exports';
 
 describe('pages', () => {
     describe('confirmRegistration', () => {
-        it('should render correctly without data', () => {
+        it('should render correctly without data when operator has no products', () => {
             const tree = shallow(
-                <Exports
-                    exports={[]}
-                    csrf={''}
-                    myFaresEnabled={true}
-                    exportEnabled={true}
-                    operatorHasProducts={true}
-                />,
+                <Exports csrf={''} myFaresEnabled={true} exportEnabled={true} operatorHasProducts={false} />,
             );
             expect(tree).toMatchSnapshot();
         });
-        it('should render correctly with data', () => {
+
+        it('should render the export button correctly when operator has products', () => {
             const tree = shallow(
-                <Exports
-                    exports={[
-                        {
-                            matchingDataCount: 2,
-                            name: 'TestExport',
-                            netexCount: 2,
-                            signedUrl:
-                                'http://localhost:4572/fdbt-netex-data-dev/BLAC/zips/TestExport//TestExport.zip?AWSAccessKeyId=S3RVER&Expires=1636716236&Signature=sB9ssjS0bN72S6iydPuk43xkZZc%3D',
-                            exportDate: '14 Sep 2021',
-                            exportTime: '10:37',
-                        },
-                    ]}
-                    csrf={'ip2IlrBL-d4B3lqI70d5y7f5GKMs2wvXRsVU'}
-                    myFaresEnabled={true}
-                    exportEnabled={true}
-                    operatorHasProducts={true}
-                />,
+                <Exports csrf={''} myFaresEnabled={true} exportEnabled={true} operatorHasProducts={true} />,
             );
             expect(tree).toMatchSnapshot();
         });
