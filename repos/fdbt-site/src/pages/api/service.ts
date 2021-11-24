a3184ea1e4a4491a9bfff9868d2c65895a45504d
Aaron-Collis-IW <aaron.collis@infinityworks.com>
2021-11-24 16:53:25 +0000

    fix: CFD-558 circular return

--- repos/fdbt-site/src/pages/api/service.ts
+++ repos/fdbt-site/src/pages/api/service.ts
@@ -1,5 +1,5 @@
 import { NextApiResponse } from 'next';
-import { SERVICE_ATTRIBUTE, TXC_SOURCE_ATTRIBUTE } from '../../constants/attributes';
+import { FARE_TYPE_ATTRIBUTE, SERVICE_ATTRIBUTE, TXC_SOURCE_ATTRIBUTE } from '../../constants/attributes';
 import { redirectToError, redirectTo, getAndValidateNoc } from '../../utils/apiUtils';
 import { updateSessionAttribute, getRequiredSessionAttribute } from '../../utils/sessions';
 import { ErrorInfo, NextApiRequestWithSession } from '../../interfaces';
@@ -17,8 +17,27 @@ export default async (req: NextApiRequestWithSession, res: NextApiResponse): Pro
         }
 
         const dataSource = getRequiredSessionAttribute(req, TXC_SOURCE_ATTRIBUTE).source;
+        const fareTypeAttribute = getRequiredSessionAttribute(req, FARE_TYPE_ATTRIBUTE);
+        const isReturn = 'fareType' in fareTypeAttribute && fareTypeAttribute.fareType === 'return';
 
         const service = await getServiceByIdAndDataSource(getAndValidateNoc(req, res), serviceId, dataSource);
+        const directions = Array.from(
+            service.journeyPatterns.reduce((set, pattern) => {
+                set.add(pattern.direction);
+                return set;
+            }, new Set<string>()),
+        );
+
+        if (directions.length === 1 && isReturn) {
+            const errors: ErrorInfo[] = [
+                {
+                    id: 'service',
+                    errorMessage: 'The chosen a service only has one direction and can`t be used for a return',
+                },
+            ];
+            updateSessionAttribute(req, SERVICE_ATTRIBUTE, { errors });
+            redirectTo(res, '/service');
+        }
 
         updateSessionAttribute(req, SERVICE_ATTRIBUTE, {
             service: `${service.lineName}#${service.startDate}`,
