c3c1c7b628c8e99924e2b67e381af1f582f39ea7
Joe Stringer <joeds13@users.noreply.github.com>
2021-12-20 15:22:38 +0000

    fix: cfd-565 updating dependencies of FDBT-Admin (#356)
    In response to Dependabot alerts

---
+++ repos/fdbt-admin/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "fdbtadmin65b519f2": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "AppClientSecret": "string"
+        }
+    }
+}
