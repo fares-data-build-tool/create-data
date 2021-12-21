534922ccfac3f944417ace9645f6c9dc4c67b2cd
Joe Stringer <joeds13@users.noreply.github.com>
2021-12-21 09:56:20 +0000

    fix: cfd-587 temporary fix for docker build (#374)
    Local scoping of directories in build context doesn't pick up fdbt-types
    local package, resulting in next build failing

---
+++ repos/fdbt-site/fdbt-types/README.md
@@ -0,0 +1,4 @@
+# TODO: REMOVE THIS COPY
+
+We should just be using `repos/fdbt-types` but the Docker build broke due to directory scoping,
+so this copy was made as a temporary fix
---
+++ repos/fdbt-site/fdbt-types/matchingJsonTypes.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ExpiryUnit = exports.CarnetExpiryUnit = void 0;
+var CarnetExpiryUnit;
+(function (CarnetExpiryUnit) {
+    CarnetExpiryUnit["HOUR"] = "hour";
+    CarnetExpiryUnit["DAY"] = "day";
+    CarnetExpiryUnit["WEEK"] = "week";
+    CarnetExpiryUnit["MONTH"] = "month";
+    CarnetExpiryUnit["YEAR"] = "year";
+    CarnetExpiryUnit["NO_EXPIRY"] = "no expiry";
+})(CarnetExpiryUnit = exports.CarnetExpiryUnit || (exports.CarnetExpiryUnit = {}));
+var ExpiryUnit;
+(function (ExpiryUnit) {
+    ExpiryUnit["HOUR"] = "hour";
+    ExpiryUnit["DAY"] = "day";
+    ExpiryUnit["WEEK"] = "week";
+    ExpiryUnit["MONTH"] = "month";
+    ExpiryUnit["YEAR"] = "year";
+})(ExpiryUnit = exports.ExpiryUnit || (exports.ExpiryUnit = {}));
