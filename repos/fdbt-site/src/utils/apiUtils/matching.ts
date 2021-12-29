28e581c44df6b4c353d71511e41ab9a33d75b105
Jwan Khalaf <jwan.khalaf@outlook.com>
2021-12-29 12:27:07 +0000

    implement a custom master stop list algorithm when toposort fails

--- repos/fdbt-site/src/utils/apiUtils/matching.ts
+++ repos/fdbt-site/src/utils/apiUtils/matching.ts
@@ -109,7 +109,34 @@ export const sortingWithoutSequenceNumbers = (journeyPatterns: RawJourneyPattern
     } catch (error) {
         logger.error('failed to toposort', { error: error.stack, journeyPatterns: journeyPatterns });
 
-        return journeyPatterns.flatMap((x) => x.orderedStopPoints).map((x) => x.stopPointRef);
+        const orderedStopPoints = journeyPatterns.flatMap((x) => x.orderedStopPoints);
+
+        const stopsAndPossibleSequenceNumbers = orderedStopPoints.map((x) => ({
+            stopPointRef: x.stopPointRef,
+            sequenceNumber: x.sequenceNumber,
+        }));
+
+        const stopsWithSequenceNumbers = stopsAndPossibleSequenceNumbers.filter(
+            (x) => x.sequenceNumber !== undefined,
+        ) as { stopPointRef: string; sequenceNumber: number }[];
+
+        const stopRefs = new Set();
+
+        const stopsWithSequenceNumbersWithDuplicateStopRefsRemoved = stopsWithSequenceNumbers.filter((x) => {
+            if (stopRefs.has(x.stopPointRef)) {
+                return false;
+            } else {
+                stopRefs.add(x.stopPointRef);
+
+                return true;
+            }
+        });
+
+        const sortedStopsBySequenceNumbers = stopsWithSequenceNumbersWithDuplicateStopRefsRemoved.sort((a, b) => {
+            return a.sequenceNumber - b.sequenceNumber;
+        });
+
+        return sortedStopsBySequenceNumbers.map((x) => x.stopPointRef);
     }
 };
 
@@ -156,6 +183,7 @@ export const getMatchingProps = async (
 
     // find journey patterns for direction (inbound or outbound)
     const journeyPatterns = service.journeyPatterns.filter((it) => it.direction === directionAttribute.direction);
+
     // get an unordered list of stop points from journey patterns, then removing any duplicates on stopPointRef and sequence number
     const stops = journeyPatterns
         .flatMap((it) => it.orderedStopPoints)
@@ -183,6 +211,7 @@ export const getMatchingProps = async (
     const naptanInfo = await batchGetStopsByAtcoCode(
         masterStopList.filter((stop, index, self) => self.indexOf(stop) === index),
     );
+
     // removing any stops that aren't fully fleshed out
     const orderedStops = masterStopList
         .map((atco) => naptanInfo.find((s) => s.atcoCode === atco))
