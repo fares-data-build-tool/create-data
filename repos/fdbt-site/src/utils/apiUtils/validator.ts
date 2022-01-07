dba84a6b8d7152215a8470304817e50305482ef7
Jwan Khalaf <jwan.khalaf@outlook.com>
2022-01-07 12:06:41 +0000

    check for invalid characters on validator functions

--- repos/fdbt-site/src/utils/apiUtils/validator.ts
+++ repos/fdbt-site/src/utils/apiUtils/validator.ts
@@ -1,3 +1,5 @@
+import { invalidCharactersArePresent } from '.';
+
 export const removeExcessWhiteSpace = (input: undefined | string): string => {
     // this will remove all whitespace on the front and end of a string, and reduce internal whitespaces to one whitespace
     if (!input) {
@@ -28,6 +30,12 @@ export const checkProductNameIsValid = (inputtedProductName: string): string => 
         productNameError = `Product name cannot have more than 50 characters`;
     } else if (inputtedProductName.length < 2) {
         productNameError = `Product name cannot have less than 2 characters`;
+    }
+
+    const nameHasInvalidCharacters = invalidCharactersArePresent(inputtedProductName);
+
+    if (nameHasInvalidCharacters) {
+        productNameError = 'Product name has an invalid character';
     }
 
     if (productNameError) {
