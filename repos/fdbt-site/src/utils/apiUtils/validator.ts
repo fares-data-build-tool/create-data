0b8be84a065187c89885c6c6434fded867ee4a10
Jwan Khalaf <jwan.khalaf@outlook.com>
2022-01-10 08:54:08 +0000

    make import more specific

--- repos/fdbt-site/src/utils/apiUtils/validator.ts
+++ repos/fdbt-site/src/utils/apiUtils/validator.ts
@@ -1,4 +1,4 @@
-import { invalidCharactersArePresent } from '.';
+import { invalidCharactersArePresent } from './index';
 
 export const removeExcessWhiteSpace = (input: undefined | string): string => {
     // this will remove all whitespace on the front and end of a string, and reduce internal whitespaces to one whitespace
