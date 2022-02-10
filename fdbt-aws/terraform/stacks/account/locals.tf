5127c822f9ad2d96ddd0ff05b6534d4382122133
Joe Stringer <joe.stringer@infinityworks.com>
2022-02-10 22:41:58 +0000

    VPC resource names

--- fdbt-aws/terraform/stacks/account/locals.tf
+++ fdbt-aws/terraform/stacks/account/locals.tf
@@ -4,6 +4,11 @@ locals {
 
   log_retention_days = 180
 
+  vpc_name              = "fdbt-vpc-${local.stage}"
+  data_subnets_names    = ["fdbt-dataSubnetA-${local.stage}", "fdbt-dataSubnetB-${local.stage}"]
+  private_subnets_names = ["fdbt-privateSubnetA-${local.stage}", "fdbt-privateSubnetB-${local.stage}"]
+  public_subnets_names  = ["fdbt-publicSubnetA-${local.stage}", "fdbt-publicSubnetB-${local.stage}"]
+
   tags = {
     project = "create-fares-data"
     stage   = local.stage
--- fdbt-aws/terraform/stacks/service/locals.tf
+++ fdbt-aws/terraform/stacks/service/locals.tf
@@ -22,6 +22,11 @@ locals {
     prod = "fares-data.dft.gov.uk"
   }
 
+  vpc_name = "fdbt-vpc-${local.stage}"
+  data_subnets_names = ["fdbt-dataSubnetA-${local.stage}","fdbt-dataSubnetB-${local.stage}"]
+  private_subnets_names = ["fdbt-privateSubnetA-${local.stage}","fdbt-privateSubnetB-${local.stage}"]
+  public_subnets_names = ["fdbt-publicSubnetA-${local.stage}","fdbt-publicSubnetB-${local.stage}"]
+
   tags = {
     project = "create-fares-data"
     stage   = local.stage
