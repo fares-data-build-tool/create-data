832424809f3be8d1a8c73af23c76bfd774a58a09
Joe Stringer <joe.stringer@infinityworks.com>
2022-02-10 22:39:59 +0000

    Domains per env

--- fdbt-aws/terraform/stacks/service/locals.tf
+++ fdbt-aws/terraform/stacks/service/locals.tf
@@ -4,6 +4,24 @@ locals {
 
   log_retention_days = 180
 
+  admin_domain_name = {
+    test = "admin.test.dft-cfd.com"
+    preprod = "admin.preprod.dft-cfd.com"
+    prod = "admin.prod.dft-cfd.com"
+  }
+
+  monitoring_domain_name = {
+    test = "monitoring.test.dft-cfd.com"
+    preprod = "monitoring.preprod.dft-cfd.com"
+    prod = "monitoring.prod.dft-cfd.com"
+  }
+
+  site_domain_name = {
+    test = "fares-data.test.dft-cfd.com"
+    preprod = "fares-data.preprod.dft-cfd.com"
+    prod = "fares-data.dft.gov.uk"
+  }
+
   tags = {
     project = "create-fares-data"
     stage   = local.stage
