65aee9b458307aff720ac86a259f5027321685f4
qaraaslam <qara.aslam@kpmg.co.uk>
2022-09-06 08:54:42 +0100

    updating pipeline to remove run ui testing

--- repos/fdbt-site/run_tests_in_browserstack.sh
+++ repos/fdbt-site/run_tests_in_browserstack.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 set -e
-echo e
 # Exit script if you try to use an uninitialized variable.
 set -o nounset
 
@@ -15,9 +14,6 @@ set -o pipefail
 
 update_ip_set() {
     IP=$(wget -qO- http://checkip.amazonaws.com)
-    echo $IP
-    echo $WAF_IPSET_NAME
-    echo $WAF_IPSET_ID
     LOCK_TOKEN=$(aws wafv2 get-ip-set --scope CLOUDFRONT --region us-east-1 --name $WAF_IPSET_NAME --id $WAF_IPSET_ID | jq .LockToken | tr -d '"')
     NEXT_LOCK_TOKEN=$(aws wafv2 update-ip-set --scope CLOUDFRONT --region us-east-1 --name $WAF_IPSET_NAME --id $WAF_IPSET_ID --addresses $IP/32 --lock-token $LOCK_TOKEN 2>/dev/null | jq .NextLockToken | tr -d '"')
 
@@ -31,27 +27,19 @@ cleanup_ip_set() {
     aws wafv2 update-ip-set --scope CLOUDFRONT --region us-east-1 --name $WAF_IPSET_NAME --id $WAF_IPSET_ID --addresses --lock-token $LOCK_TOKEN
 }
 
-echo "started"
 sudo apt update
-echo "apt updated"
 sudo apt install jq unzip
-echo "jq installed"
 
 n=0
 
 until [ "$n" -ge 5 ]; do
-    echo "$n"
     update_ip_set && break
     n=$((n+1))
     sleep 5
 done
-echo "added ip's"
 
 make install-cypress-deps-ci
-echo "line 50"
 
 make run-cypress-browserstack
-echo "run cypress browserstacks"
 
 cleanup_ip_set
-echo "cleanup ip set"
