f4ed54ef254d89b840832765af38fad156fe6004
ajaffer1 <alaina.jaffer@kpmg.co.uk>
2022-10-04 17:24:36 +0100

    CFDS-37 - Fixed condition and xml

--- repos/fdbt-netex-output/src/netex-convertor/period-tickets/periodTicketNetexHelpers.ts
+++ repos/fdbt-netex-output/src/netex-convertor/period-tickets/periodTicketNetexHelpers.ts
@@ -570,7 +570,7 @@ export const getPreassignedFareProducts = (
             version: '1.0',
             id: `op:Pass@${product.productName}_${passengerType}`,
             Name: {
-                $t: isFlatFareType(userPeriodTicket) ? product.productName : `${product.productName} Pass - ${passengerType}`,
+                $t: isFlatFareType(userPeriodTicket) ? product.productName : `${product.productName} - ${passengerType}`,
             },
             ChargingMomentType: {
                 $t: 'beforeTravel',
