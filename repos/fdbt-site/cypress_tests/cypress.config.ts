cc71d7f85babc6da35d365ae8a9b2d86165ea7cf
SamerIssam <samer.salem@kpmg.co.uk>
2022-10-05 14:00:28 +0100

    disable typescript error in config file

--- repos/fdbt-site/cypress_tests/cypress.config.ts
+++ repos/fdbt-site/cypress_tests/cypress.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  retries: 3,
-  numTestsKeptInMemory: 0,
-  e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+    retries: 3,
+    numTestsKeptInMemory: 0,
+    e2e: {
+        // We've imported your old cypress plugins here.
+        // You may want to clean this up later by importing these.
+        setupNodeEvents(on, config) {
+            // eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
+            return require('./cypress/plugins/index.js')(on, config);
+        },
+        baseUrl: 'http://localhost:5555',
     },
-    baseUrl: 'http://localhost:5555',
-  },
-})
+});
