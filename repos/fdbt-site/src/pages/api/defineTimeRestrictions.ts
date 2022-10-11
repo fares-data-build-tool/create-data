9a9931590c87c7754c9c78d42e01e041b22a231b
NitishAroraKPMG <112861163+NitishAroraKPMG@users.noreply.github.com>
2022-10-11 14:28:15 +0530

    feat: edit products time restrictions
    * Added the edit time restriction functionality
    * Cleaned up define time restrictions API
    * Removed choose time restriction
    
    Co-authored-by: RobCattonKPMG <robert.catton@kpmg.co.uk>

--- repos/fdbt-site/src/pages/api/defineTimeRestrictions.ts
+++ repos/fdbt-site/src/pages/api/defineTimeRestrictions.ts
@@ -1,90 +1,89 @@
-import isArray from 'lodash/isArray';
 import { NextApiResponse } from 'next';
-import { FULL_TIME_RESTRICTIONS_ATTRIBUTE, TIME_RESTRICTIONS_DEFINITION_ATTRIBUTE } from '../../constants/attributes';
+import {
+    FULL_TIME_RESTRICTIONS_ATTRIBUTE,
+    TIME_RESTRICTIONS_DEFINITION_ATTRIBUTE,
+    MATCHING_JSON_ATTRIBUTE,
+    MATCHING_JSON_META_DATA_ATTRIBUTE,
+} from '../../constants/attributes';
 import { getTimeRestrictionByNameAndNoc, getFareDayEnd } from '../../data/auroradb';
-import { NextApiRequestWithSession, TimeRestriction, TimeRestrictionsDefinitionWithErrors } from '../../interfaces';
-import { updateSessionAttribute } from '../../utils/sessions';
+import {
+    NextApiRequestWithSession,
+    PremadeTimeRestriction,
+    TimeRestriction,
+    TimeRestrictionsDefinitionWithErrors,
+} from '../../interfaces';
+import { getSessionAttribute, updateSessionAttribute } from '../../utils/sessions';
 import { getAndValidateNoc, redirectTo, redirectToError } from '../../utils/apiUtils/index';
+import { putUserDataInProductsBucketWithFilePath } from '../../utils/apiUtils/userData';
+import { TimeRestrictionDay } from 'fdbt-types/matchingJsonTypes';
+
+const getTimeRestrictionContent = async (
+    timeRestriction: string | undefined,
+    noc: string,
+): Promise<
+    | {
+          dbTimeRestriction: PremadeTimeRestriction;
+          timeRestrictions: {
+              timeBands: {
+                  endTime: string;
+                  startTime: string;
+              }[];
+              day: TimeRestrictionDay;
+          }[];
+      }
+    | undefined
+> => {
+    if (!timeRestriction) {
+        return undefined;
+    }
+
+    const results = await getTimeRestrictionByNameAndNoc(timeRestriction, noc);
+
+    if (results.length > 1 || results.length === 0) {
+        throw new Error(
+            `${results.length} results found - ${
+                results.length > 0
+                    ? 'Multiple premade time restrictions with same name'
+                    : `No premade time restrictions saved under ${timeRestriction}`
+            }`,
+        );
+    }
+
+    const fareDayEnd = await getFareDayEnd(noc);
+
+    const dbTimeRestriction = results[0];
+    const timeRestrictions = dbTimeRestriction.contents.map((timeRestriction) => ({
+        ...timeRestriction,
+        timeBands: timeRestriction.timeBands.map((timeBand) => {
+            let endTime: string;
+            if (typeof timeBand.endTime === 'string') {
+                endTime = timeBand.endTime;
+            } else {
+                if (!fareDayEnd) {
+                    throw new Error('No fare day end set for time restriction');
+                }
+
+                endTime = fareDayEnd;
+            }
+
+            return {
+                ...timeBand,
+                endTime: endTime,
+            };
+        }),
+    }));
+
+    return {
+        dbTimeRestriction,
+        timeRestrictions,
+    };
+};
 
 export default async (req: NextApiRequestWithSession, res: NextApiResponse): Promise<void> => {
     try {
         const { timeRestrictionChoice, validDays, timeRestriction } = req.body;
-        if (timeRestrictionChoice === 'Premade') {
-            if (!timeRestriction) {
-                const timeRestrictionsDefinitionWithErrors: TimeRestrictionsDefinitionWithErrors = {
-                    validDays: [],
-                    timeRestrictionChoice,
-                    errors: [{ errorMessage: 'Choose one of the premade time restrictions', id: 'time-restriction' }],
-                };
-
-                updateSessionAttribute(
-                    req,
-                    TIME_RESTRICTIONS_DEFINITION_ATTRIBUTE,
-                    timeRestrictionsDefinitionWithErrors,
-                );
-
-                redirectTo(res, '/selectTimeRestrictions');
-
-                return;
-            }
-
-            const noc = getAndValidateNoc(req, res);
-
-            if (!noc) {
-                throw new Error('Could not find users NOC code.');
-            }
-
-            const results = await getTimeRestrictionByNameAndNoc(timeRestriction, noc);
-
-            if (results.length > 1 || results.length === 0) {
-                throw new Error(
-                    `${results.length} results found - ${
-                        results.length > 0
-                            ? 'Multiple premade time restrictions with same name'
-                            : `No premade time restrictions saved under ${timeRestriction}`
-                    }`,
-                );
-            }
-
-            const fareDayEnd = await getFareDayEnd(noc);
-
-            const dbTimeRestriction = results[0];
-            const timeRestrictions = dbTimeRestriction.contents.map((timeRestriction) => ({
-                ...timeRestriction,
-                timeBands: timeRestriction.timeBands.map((timeBand) => {
-                    let endTime: string;
-                    if (typeof timeBand.endTime === 'string') {
-                        endTime = timeBand.endTime;
-                    } else {
-                        if (!fareDayEnd) {
-                            throw new Error('No fare day end set for time restriction');
-                        }
-
-                        endTime = fareDayEnd;
-                    }
-
-                    return {
-                        ...timeBand,
-                        endTime: endTime,
-                    };
-                }),
-            }));
-
-            updateSessionAttribute(req, FULL_TIME_RESTRICTIONS_ATTRIBUTE, {
-                fullTimeRestrictions: timeRestrictions,
-                errors: [],
-                id: dbTimeRestriction.id,
-            });
-
-            updateSessionAttribute(req, TIME_RESTRICTIONS_DEFINITION_ATTRIBUTE, undefined);
-
-            redirectTo(res, '/fareConfirmation');
-            return;
-        }
-
-        const timeRestrictionsDefinition: TimeRestriction = {
-            validDays: [],
-        };
+        const ticket = getSessionAttribute(req, MATCHING_JSON_ATTRIBUTE);
+        const matchingJsonMetaData = getSessionAttribute(req, MATCHING_JSON_META_DATA_ATTRIBUTE);
 
         if (!timeRestrictionChoice) {
             const timeRestrictionsDefinitionWithErrors: TimeRestrictionsDefinitionWithErrors = {
@@ -94,36 +93,64 @@ export default async (req: NextApiRequestWithSession, res: NextApiResponse): Pro
             };
 
             updateSessionAttribute(req, TIME_RESTRICTIONS_DEFINITION_ATTRIBUTE, timeRestrictionsDefinitionWithErrors);
-
             redirectTo(res, '/selectTimeRestrictions');
-
             return;
         }
 
-        if (timeRestrictionChoice === 'Yes') {
-            if (!validDays || validDays.length === 0) {
-                const timeRestrictionsDefinitionWithErrors: TimeRestrictionsDefinitionWithErrors = {
-                    validDays,
-                    timeRestrictionChoice,
-                    errors: [{ errorMessage: 'Select at least one day', id: 'monday' }],
-                };
-                updateSessionAttribute(
-                    req,
-                    TIME_RESTRICTIONS_DEFINITION_ATTRIBUTE,
-                    timeRestrictionsDefinitionWithErrors,
-                );
-                redirectTo(res, '/defineTimeRestrictions');
-                return;
-            }
-            if (isArray(validDays)) {
-                timeRestrictionsDefinition.validDays = timeRestrictionsDefinition.validDays.concat(validDays);
-            } else {
-                timeRestrictionsDefinition.validDays.push(validDays);
-            }
-            updateSessionAttribute(req, TIME_RESTRICTIONS_DEFINITION_ATTRIBUTE, timeRestrictionsDefinition);
-            redirectTo(res, '/chooseTimeRestrictions');
+        const noc = getAndValidateNoc(req, res);
+
+        if (timeRestrictionChoice === 'Premade' && !timeRestriction) {
+            const timeRestrictionsDefinitionWithErrors: TimeRestrictionsDefinitionWithErrors = {
+                validDays: [],
+                timeRestrictionChoice,
+                errors: [{ errorMessage: 'Choose one of the premade time restrictions', id: 'time-restriction' }],
+            };
+
+            updateSessionAttribute(req, TIME_RESTRICTIONS_DEFINITION_ATTRIBUTE, timeRestrictionsDefinitionWithErrors);
+            redirectTo(res, '/selectTimeRestrictions');
             return;
         }
+
+        const selectedTimeRetriction = await getTimeRestrictionContent(timeRestriction, noc);
+
+        // if in edit mode
+        if (ticket && matchingJsonMetaData) {
+            const updatedTicket = {
+                ...ticket,
+                timeRestriction: selectedTimeRetriction
+                    ? { id: selectedTimeRetriction.dbTimeRestriction.id }
+                    : undefined,
+            };
+
+            await putUserDataInProductsBucketWithFilePath(updatedTicket, matchingJsonMetaData.matchingJsonLink);
+            updateSessionAttribute(req, FULL_TIME_RESTRICTIONS_ATTRIBUTE, undefined);
+            updateSessionAttribute(req, TIME_RESTRICTIONS_DEFINITION_ATTRIBUTE, undefined);
+
+            redirectTo(
+                res,
+                `/products/productDetails?productId=${matchingJsonMetaData?.productId}${
+                    matchingJsonMetaData.serviceId ? `&serviceId=${matchingJsonMetaData?.serviceId}` : ''
+                }`,
+            );
+            return;
+        }
+        // end of edit mode
+
+        if (timeRestrictionChoice === 'Premade' && selectedTimeRetriction) {
+            updateSessionAttribute(req, FULL_TIME_RESTRICTIONS_ATTRIBUTE, {
+                fullTimeRestrictions: selectedTimeRetriction.timeRestrictions,
+                errors: [],
+                id: selectedTimeRetriction.dbTimeRestriction.id,
+            });
+
+            updateSessionAttribute(req, TIME_RESTRICTIONS_DEFINITION_ATTRIBUTE, undefined);
+            redirectTo(res, '/fareConfirmation');
+            return;
+        }
+
+        const timeRestrictionsDefinition: TimeRestriction = {
+            validDays: [],
+        };
 
         updateSessionAttribute(req, FULL_TIME_RESTRICTIONS_ATTRIBUTE, { fullTimeRestrictions: [], errors: [] });
         updateSessionAttribute(req, TIME_RESTRICTIONS_DEFINITION_ATTRIBUTE, timeRestrictionsDefinition);
