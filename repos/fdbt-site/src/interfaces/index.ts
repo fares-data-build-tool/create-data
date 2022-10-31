6fafd08d87a4abfbb3be3e8ec9412fd906a2fae8
Nitish Arora <nitisharora@kpmg.com>
2022-10-31 20:39:23 +0530

    Added functionality for Operator Group

--- repos/fdbt-site/src/interfaces/index.ts
+++ repos/fdbt-site/src/interfaces/index.ts
@@ -311,6 +311,7 @@ export interface MultiOperatorMultipleServicesTicket extends PeriodMultipleServi
         nocCode: string;
         selectedServices: SelectedService[];
     }[];
+    operatorGroupId: number;
 }
 
 // Matching Data (created by the user on the site)
