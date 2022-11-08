af5835dc14c37906d12f82f03f358f085ebbbc3a
NitishAroraKPMG <112861163+NitishAroraKPMG@users.noreply.github.com>
2022-11-08 15:55:28 +0530

    fix: Fix the multi operator ticket

--- repos/fdbt-site/tests/pages/viewOperatorGroups.test.tsx
+++ repos/fdbt-site/tests/pages/viewOperatorGroups.test.tsx
@@ -1,13 +1,19 @@
 import { shallow } from 'enzyme';
 import * as React from 'react';
-import { OperatorGroup } from '../../src/interfaces';
+import { ErrorInfo, OperatorGroup } from '../../src/interfaces';
 import ViewOperatorGroups from '../../src/pages/viewOperatorGroups';
 
 describe('pages', () => {
     describe('view operator groups', () => {
         it('should render correctly when no operator groups', () => {
             const tree = shallow(
-                <ViewOperatorGroups operatorGroups={[]} csrfToken={''} referer={null} isDevOrTest={false} />,
+                <ViewOperatorGroups
+                    operatorGroups={[]}
+                    csrfToken={''}
+                    referer={null}
+                    isDevOrTest={false}
+                    viewOperatorGroupErrors={[]}
+                />,
             );
 
             expect(tree).toMatchSnapshot();
@@ -31,6 +37,36 @@ describe('pages', () => {
                     csrfToken={''}
                     referer={'hello'}
                     isDevOrTest={false}
+                    viewOperatorGroupErrors={[]}
+                />,
+            );
+
+            expect(tree).toMatchSnapshot();
+        });
+
+        it('should render correctly when there is some error', () => {
+            const operatorGroup: OperatorGroup = {
+                id: 1,
+                name: 'first operator group',
+                operators: [
+                    {
+                        name: 'First operator',
+                        nocCode: 'FOP',
+                    },
+                ],
+            };
+
+            const errorMessage = 'You cannot delete Operator operator group because it is used in a product(s).';
+
+            const errors: ErrorInfo[] = [{ id: '/viewOperatorGroups', errorMessage }];
+
+            const tree = shallow(
+                <ViewOperatorGroups
+                    operatorGroups={[operatorGroup]}
+                    csrfToken={''}
+                    referer={'hello'}
+                    isDevOrTest={false}
+                    viewOperatorGroupErrors={errors}
                 />,
             );
 
