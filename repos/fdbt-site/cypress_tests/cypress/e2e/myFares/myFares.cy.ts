130fe26e3935dc76f9b8e62f9805a146dea29dbb
ajaffer1 <alaina.jaffer@kpmg.co.uk>
2022-11-11 17:34:29 +0000

    formatting

--- repos/fdbt-site/cypress_tests/cypress/e2e/myFares/myFares.cy.ts
+++ repos/fdbt-site/cypress_tests/cypress/e2e/myFares/myFares.cy.ts
@@ -4,7 +4,17 @@ import {
     clickElementByText,
     getHomePage,
 } from '../../support/helpers';
-import { completeMyFaresOtherProductsPages, completeMyFaresPointToPointProductsPages, editEndDateOtherProductsPage, editPassengerTypeOtherProductsPage, editProductNameOtherProductsPage, editPurchaseMethodOtherProductsPage, editServicesOtherProductsPage, editStartDateOtherProductsPage, editTimeRestrictionOtherProductsPage } from '../../support/steps';
+import {
+    completeMyFaresOtherProductsPages,
+    completeMyFaresPointToPointProductsPages,
+    editEndDateOtherProductsPage,
+    editPassengerTypeOtherProductsPage,
+    editProductNameOtherProductsPage,
+    editPurchaseMethodOtherProductsPage,
+    editServicesOtherProductsPage,
+    editStartDateOtherProductsPage,
+    editTimeRestrictionOtherProductsPage,
+} from '../../support/steps';
 
 describe('The my fares point to point products pages', () => {
     it('allows for navigation through the point to point products pages', () => {
@@ -40,42 +50,42 @@ describe('The my fares other products pages', () => {
         getHomePage();
         clickElementById('account-link');
         clickElementByText('Other products');
-        editServicesOtherProductsPage()
+        editServicesOtherProductsPage();
     });
     it('allows the user the edit other product passenger type', () => {
         getHomePage();
         clickElementById('account-link');
         clickElementByText('Other products');
-        editPassengerTypeOtherProductsPage()
+        editPassengerTypeOtherProductsPage();
     });
     it('allows the user the edit other product start date', () => {
         getHomePage();
         clickElementById('account-link');
         clickElementByText('Other products');
-        editStartDateOtherProductsPage()
+        editStartDateOtherProductsPage();
     });
     it('allows the user the edit other product end date', () => {
         getHomePage();
         clickElementById('account-link');
         clickElementByText('Other products');
-        editEndDateOtherProductsPage()
+        editEndDateOtherProductsPage();
     });
     it('allows the user the edit other product purchase method', () => {
         getHomePage();
         clickElementById('account-link');
         clickElementByText('Other products');
-        editPurchaseMethodOtherProductsPage()
+        editPurchaseMethodOtherProductsPage();
     });
     it('allows the user the edit other product time restriction', () => {
         getHomePage();
         clickElementById('account-link');
         clickElementByText('Other products');
-        editTimeRestrictionOtherProductsPage()
+        editTimeRestrictionOtherProductsPage();
     });
     it('allows the user the edit other product name', () => {
         getHomePage();
         clickElementById('account-link');
         clickElementByText('Other products');
-        editProductNameOtherProductsPage()
+        editProductNameOtherProductsPage();
     });
 });
