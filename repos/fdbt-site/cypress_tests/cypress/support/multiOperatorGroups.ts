12df09537b5feefa605842158ec226cd0d8224c3
ajaffer1 <alaina.jaffer@kpmg.co.uk>
2022-11-18 15:06:33 +0000

    wip - trying to get clicked operators

--- repos/fdbt-site/cypress_tests/cypress/support/multiOperatorGroups.ts
+++ repos/fdbt-site/cypress_tests/cypress/support/multiOperatorGroups.ts
@@ -13,7 +13,13 @@ const addSingleMultiOperatorGroup = (name: string) => {
     clickElementByText('Add an operator group');
     clickElementById('search-input').clear().type('bus');
     clickElementById('search-button');
-    randomlySelectMultiServices();
+    const input: string[] = [];
+    cy.get('[class=govuk-checkboxes__item]').each((checkbox, index) => {
+        cy.wrap(checkbox).click();
+        input[index] = checkbox.text();
+        cy.log(index.toString(), checkbox.text());
+        cy.wrap(input).as('inputMultiOp');
+    });
     clickElementById('operator-group-name').clear().type(name);
     continueButtonClick();
 };
@@ -22,13 +28,17 @@ export const createEditMultiOperatorGroups = () => {
     const multiOperatorGroup2 = 'MultiOperator Group 2';
 
     addSingleMultiOperatorGroup(multiOperatorGroup1);
-    addSingleMultiOperatorGroup(multiOperatorGroup2);
 
     const firstCard = getElementByClass('card').eq(0);
     firstCard.should('include.text', multiOperatorGroup1);
-    // firstCard.should('include.text', 'Proof document(s): Membership card, Identity document');
-
+    cy.get('@inputMultiOp').then((input) => {
+        cy.log(input.toString());
+        const valuesToCompare = input.toString().split(',');
+        valuesToCompare.forEach((value) => {
+            firstCard.should('include.text', value);
+        });
+    });
+    addSingleMultiOperatorGroup(multiOperatorGroup2);
     const secondCard = getElementByClass('card').eq(1);
     secondCard.should('include.text', multiOperatorGroup2);
-    // secondCard.should('include.text', 'Proof document(s): N/A');
 };
