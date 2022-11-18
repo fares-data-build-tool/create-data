72c21e95be30d34a05d86901e8849ea8aaa21824
Alaina Jaffer <59929701+alainajaffer@users.noreply.github.com>
2022-11-18 14:08:21 +0000

    feat: UI Test - Edit point to point ticket (#641)

--- repos/fdbt-site/cypress_tests/cypress/e2e/myFares/myFares.cy.ts
+++ repos/fdbt-site/cypress_tests/cypress/e2e/myFares/myFares.cy.ts
@@ -1,5 +1,6 @@
 import {
     addFlatFareProductIfNotPresent,
+    addSingleProductIfNotPresent,
     clickElementById,
     clickElementByText,
     getHomePage,
@@ -8,15 +9,25 @@ import {
     completeMyFaresOtherProductsPages,
     completeMyFaresPointToPointProductsPages,
     editEndDateOtherProductsPage,
+    editEndDatePointToPointPage,
+    editFareTrianglePointToPointPage,
     editPassengerTypeOtherProductsPage,
+    editPassengerTypePointToPointPage,
     editProductNameOtherProductsPage,
+    editProductNamePointToPointPage,
     editPurchaseMethodOtherProductsPage,
+    editPurchaseMethodPointToPointPage,
     editServicesOtherProductsPage,
     editStartDateOtherProductsPage,
+    editStartDatePointToPointPage,
     editTimeRestrictionOtherProductsPage,
+    editTimeRestrictionPointToPointPage,
 } from '../../support/steps';
 
 describe('The my fares point to point products pages', () => {
+    before(() => {
+       addSingleProductIfNotPresent()
+    });
     it('allows for navigation through the point to point products pages', () => {
         getHomePage();
         clickElementById('manage-fares-link');
@@ -27,6 +38,56 @@ describe('The my fares point to point products pages', () => {
         clickElementById('account-link');
         clickElementByText('Services');
         completeMyFaresPointToPointProductsPages();
+    });
+    it('allows the user the edit point to point product passenger type', () => {
+        getHomePage();
+        clickElementById('account-link');
+        clickElementByText('Services');
+        editPassengerTypePointToPointPage();
+    });
+    it('allows the user the edit point to point product start date', () => {
+        getHomePage();
+        clickElementById('account-link');
+        clickElementByText('Services');
+        editStartDatePointToPointPage();
+    });
+    it('allows the user the edit point to point product end date', () => {
+        getHomePage();
+        clickElementById('account-link');
+        clickElementByText('Services');
+        editEndDatePointToPointPage();
+    });
+    it('allows the user the edit point to point product time restriction', () => {
+        getHomePage();
+        clickElementById('account-link');
+        clickElementByText('Services');
+        editTimeRestrictionPointToPointPage();
+    });
+    it('allows the user the edit point to point product fare triangle', () => {
+        getHomePage();
+        clickElementById('account-link');
+        clickElementByText('Services');
+        editFareTrianglePointToPointPage();
+        cy.get('[id=fare-triangle]')
+            .invoke('text')
+            .then((text) => {
+                const dateTextNew = text.split(' ')[1];
+                const [dayNew, monthNew, yearNew] = dateTextNew.split('/');
+                const newDate = new Date(Number(yearNew), Number(monthNew) - 1, Number(dayNew));
+                expect(newDate).to.be.lte(new Date());
+            });
+    });
+    it('allows the user the edit point to point product name', () => {
+        getHomePage();
+        clickElementById('account-link');
+        clickElementByText('Services');
+        editProductNamePointToPointPage();
+    });
+    it('allows the user the edit point to point product purchase method', () => {
+        getHomePage();
+        clickElementById('account-link');
+        clickElementByText('Services');
+        editPurchaseMethodPointToPointPage();
     });
 });
 
