ae6c1ce11419031d51f60202f3c90512be5c84b0
Alaina Jaffer <59929701+alainajaffer@users.noreply.github.com>
2022-11-22 14:18:56 +0000

    feat: UI Test - Edit MultiOperator Group (#650)

--- repos/fdbt-site/cypress_tests/cypress/support/multiOperatorGroups.ts
+++ repos/fdbt-site/cypress_tests/cypress/support/multiOperatorGroups.ts
@@ -1,29 +1,36 @@
 import { clickElementById, clickElementByText, continueButtonClick, getElementByClass } from './helpers';
 
-export const addSingleMultiOperatorGroup = (name: string, addExtra: boolean) => {
-    const input: string[] = [];
-    cy.wrap(input).as('inputMultiOp');
+const addExtraOperator = (): void => {
+    clickElementById('search-input').clear().type('Pil');
+    clickElementById('search-button');
+    clickElementByText('Pilkingtonbus - NWBT');
+};
+
+export const addSingleMultiOperatorGroup = (name: string, addExtra: boolean, addMulti: boolean): void => {
     clickElementByText('Add an operator group');
-    clickElementById('search-input').clear().type('blackpool');
-    clickElementById('search-button');
-    clickElementByText('Blackpool Transport - BLAC');
-    clickElementById('search-input').clear().type('bus');
-    clickElementById('search-button');
-    clickElementByText("Warrington's Own Buses - WBTR");
-    if (addExtra) {
-        clickElementById('search-input').clear().type('Pil');
+    if (addMulti) {
+        clickElementById('search-input').clear().type('blackpool');
         clickElementById('search-button');
-        clickElementByText('Pilkingtonbus - NWBT');
+        clickElementByText('Blackpool Transport - BLAC');
+        clickElementById('search-input').clear().type('bus');
+        clickElementById('search-button');
+        clickElementByText("Warrington's Own Buses - WBTR");
+    }
+    if (addExtra || !addMulti) {
+        addExtraOperator();
     }
     clickElementById('operator-group-name').clear().type(name);
     continueButtonClick();
 };
 
-const checkCardBody = (card: Cypress.Chainable<JQuery<HTMLElement>>, eq: number) => {
-    const valuesToCompare = ['Blackpool Transport - BLAC', "Warrington's Own Buses - WBTR"];
+const checkCardBody = (
+    card: Cypress.Chainable<JQuery<HTMLElement>>,
+    indexOfCard: number,
+    valuesToCompare: string[],
+) => {
     valuesToCompare.forEach((value, index) => {
         card.get(`[id=operator-${index}]`)
-            .eq(eq)
+            .eq(indexOfCard)
             .should(($p) => {
                 expect($p.text()).equal(value);
             });
@@ -34,14 +41,32 @@ export const createEditMultiOperatorGroups = () => {
     const multiOperatorGroup1 = 'MultiOperator Group 1';
     const multiOperatorGroup2 = 'MultiOperator Group 2';
 
-    addSingleMultiOperatorGroup(multiOperatorGroup1, false);
+    addSingleMultiOperatorGroup(multiOperatorGroup1, false, true);
 
+    const valuesToCompareFirst = ['Blackpool Transport - BLAC', "Warrington's Own Buses - WBTR"];
     const firstCard = getElementByClass('card').eq(0);
     firstCard.should('contain.text', multiOperatorGroup1);
-    checkCardBody(firstCard, 0);
+    checkCardBody(firstCard, 0, valuesToCompareFirst);
+    getElementByClass('card').eq(0).contains('Edit').click();
+    clickElementById('search-input').clear().type('Pil');
+    clickElementById('search-button');
+    clickElementByText('Pilkingtonbus - NWBT');
+    continueButtonClick();
+    valuesToCompareFirst.push('Pilkingtonbus - NWBT');
+    checkCardBody(firstCard, 0, valuesToCompareFirst);
 
-    addSingleMultiOperatorGroup(multiOperatorGroup2, true);
+    const valuesToCompareSecond = [
+        'Blackpool Transport - BLAC',
+        "Warrington's Own Buses - WBTR",
+        'Pilkingtonbus - NWBT',
+    ];
+    addSingleMultiOperatorGroup(multiOperatorGroup2, true, true);
     const secondCard = getElementByClass('card').eq(1);
     secondCard.should('contain.text', multiOperatorGroup2);
-    checkCardBody(secondCard, 1);
+    checkCardBody(secondCard, 1, valuesToCompareSecond);
+    getElementByClass('card').eq(1).contains('Edit').click();
+    clickElementById('remove-0');
+    continueButtonClick();
+    valuesToCompareSecond.shift();
+    checkCardBody(secondCard, 1, valuesToCompareSecond);
 };
