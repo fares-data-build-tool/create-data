1e11583837c7635e52fd93ececd9bb3a6d1f6550
Nitish Arora <nitisharora@kpmg.com>
2022-12-12 19:58:35 +0530

    Adding new changes

--- repos/fdbt-site/src/pages/capConfirmation.tsx
+++ repos/fdbt-site/src/pages/capConfirmation.tsx
@@ -1,34 +1,18 @@
 import React, { ReactElement } from 'react';
 import {
-    FARE_TYPE_ATTRIBUTE,
-    PASSENGER_TYPE_ATTRIBUTE,
-    FULL_TIME_RESTRICTIONS_ATTRIBUTE,
-    SCHOOL_FARE_TYPE_ATTRIBUTE,
-    TERM_TIME_ATTRIBUTE,
-    GROUP_PASSENGER_INFO_ATTRIBUTE,
-    CARNET_FARE_TYPE_ATTRIBUTE,
     TYPE_OF_CAP_ATTRIBUTE,
     CAPS_ATTRIBUTE,
     CAPPED_PRODUCT_GROUP_ID_ATTRIBUTE,
     CAP_EXPIRY_ATTRIBUTE,
 } from '../constants/attributes';
-import {
-    NextPageContextWithSession,
-    TermTimeAttribute,
-    FullTimeRestrictionAttribute,
-    ConfirmationElement,
-    SchoolFareTypeAttribute,
-    Cap,
-} from '../interfaces';
+import { NextPageContextWithSession, ConfirmationElement, Cap } from '../interfaces';
 import TwoThirdsLayout from '../layout/Layout';
 import CsrfForm from '../components/CsrfForm';
 import ConfirmationTable from '../components/ConfirmationTable';
 import { getSessionAttribute } from '../utils/sessions';
-import { isPassengerTypeAttributeWithErrors, isFareType, isWithErrors, isCapExpiry } from '../interfaces/typeGuards';
+import { isCapExpiry } from '../interfaces/typeGuards';
 import { getCsrfToken, sentenceCaseString, getAndValidateNoc } from '../utils';
-import { getPassengerTypeNameByIdAndNoc, getProductGroupByNocAndId } from '../data/auroradb';
-import { PassengerType } from '../interfaces/dbTypes';
-import { CompanionInfo, FullTimeRestriction } from '../interfaces/matchingJsonTypes';
+import { getProductGroupByNocAndId } from '../data/auroradb';
 
 const title = 'Cap Confirmation - Create Fares Data Service';
 const description = 'Cap Confirmation page of the Create Fares Data Service';
@@ -41,7 +25,7 @@ interface CapConfirmationProps {
     csrfToken: string;
 }
 
-export const buildCapConfirmationElements = (    
+export const buildCapConfirmationElements = (
     typeOfCap: string,
     productGroupName: string,
     caps: Cap[],
@@ -52,15 +36,14 @@ export const buildCapConfirmationElements = (
             name: 'Cap type',
             content: `Pricing by ${typeOfCap.substring(2)} `,
             href: 'typeOfCap',
-        }
+        },
     ];
 
-    
     if (productGroupName) {
         confirmationElements.push({
             name: 'Product Group Name',
             content: productGroupName,
-            href: '/selectCappedProductGroup'
+            href: '/selectCappedProductGroup',
         });
     }
 
@@ -68,15 +51,14 @@ export const buildCapConfirmationElements = (
         confirmationElements.push({
             name: 'Cap Details',
             content: cap.name + ' || ' + cap.price + ' || ' + cap.durationAmount + ' ' + cap.durationUnits,
-            href: '/createCaps'
+            href: '/createCaps',
         });
-    })
+    });
 
     confirmationElements.push({
         name: 'Cap Expiry',
         content: sentenceCaseString(capValidity),
-        href: '/selectCapValidity'
-
+        href: '/selectCapValidity',
     });
     return confirmationElements;
 };
@@ -94,12 +76,7 @@ const CapConfirmation = ({
                 <h1 className="govuk-heading-l">Check your answers before sending your fares information</h1>
                 <ConfirmationTable
                     header="Fare Information"
-                    confirmationElements={buildCapConfirmationElements(                        
-                        typeOfCap,
-                        productGroupName,
-                        caps,
-                        capValidity                   
-                    )}
+                    confirmationElements={buildCapConfirmationElements(typeOfCap, productGroupName, caps, capValidity)}
                 />
                 <input type="submit" value="Continue" id="continue-button" className="govuk-button" />
             </>
@@ -108,28 +85,26 @@ const CapConfirmation = ({
 );
 
 export const getServerSideProps = async (ctx: NextPageContextWithSession): Promise<{ props: CapConfirmationProps }> => {
-    
     const csrfToken = getCsrfToken(ctx);
     const noc = getAndValidateNoc(ctx);
 
     const typeOfCapAttribute = getSessionAttribute(ctx.req, TYPE_OF_CAP_ATTRIBUTE);
     const capAttribute = getSessionAttribute(ctx.req, CAPS_ATTRIBUTE);
     const productGroupIdAttribute = getSessionAttribute(ctx.req, CAPPED_PRODUCT_GROUP_ID_ATTRIBUTE);
-    
-    const productGroupName = productGroupIdAttribute && typeof productGroupIdAttribute === 'string' ? (await getProductGroupByNocAndId(noc, Number.parseInt(productGroupIdAttribute)))?.name : '';
+
+    const productGroupName =
+        productGroupIdAttribute && typeof productGroupIdAttribute === 'string'
+            ? (await getProductGroupByNocAndId(noc, Number.parseInt(productGroupIdAttribute)))?.name
+            : '';
     const caps = capAttribute ? capAttribute.caps : [];
-    
+
     const capValidityAttribute = getSessionAttribute(ctx.req, CAP_EXPIRY_ATTRIBUTE);
-    const capValidity = capValidityAttribute && isCapExpiry(capValidityAttribute) ? capValidityAttribute.productValidity: '';
-    
-    if (
-        !typeOfCapAttribute ||
-        !('typeOfCap' in typeOfCapAttribute)
-        || !capAttribute
-    ) {
+    const capValidity =
+        capValidityAttribute && isCapExpiry(capValidityAttribute) ? capValidityAttribute.productValidity : '';
+
+    if (!typeOfCapAttribute || !('typeOfCap' in typeOfCapAttribute) || !capAttribute) {
         throw new Error('Could not extract the correct attributes for the user journey.');
     }
-
 
     return {
         props: {
