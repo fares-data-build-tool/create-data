5a7125960edf2af17a1911ff03cdd8d5e7771d2a
NitishAroraKPMG <112861163+NitishAroraKPMG@users.noreply.github.com>
2022-12-22 14:54:17 +0530

    fix: Fixing the UI test and adding for capped purchase method

--- repos/fdbt-site/cypress_tests/cypress/support/purchaseMethods.ts
+++ repos/fdbt-site/cypress_tests/cypress/support/purchaseMethods.ts
@@ -15,21 +15,23 @@ const enterPurchaseMethodDetails = ({ purchaseLocations, paymentMethods, ticketF
     getElementByName('name').clear().type(name);
 };
 
-export const addPurchaseMethod = (purchaseMethod: PurchaseMethod): void => {
-    clickElementByText('Add a purchase method');
+export const addPurchaseMethod = (purchaseMethod: PurchaseMethod, isCapped = false): void => {
+    isCapped ? clickElementByText('Add a capped purchase method') : clickElementByText('Add a purchase method');
     enterPurchaseMethodDetails(purchaseMethod);
     clickElementByText('Add purchase method');
 };
 
-export const createEditPurchaseMethod = (): void => {
+export const createEditPurchaseMethod = (isCapped = false): void => {
     const purchaseMethod = {
         purchaseLocations: ['checkbox-0-on-board'],
-        paymentMethods: ['checkbox-0-cash', 'checkbox-1-debit-card'],
-        ticketFormats: ['checkbox-3-electronic-document'],
+        paymentMethods: isCapped
+            ? ['checkbox-0-debit-card', 'checkbox-1-credit-card']
+            : ['checkbox-0-cash', 'checkbox-1-debit-card'],
+        ticketFormats: isCapped ? ['checkbox-0-mobile-app'] : ['checkbox-3-electronic-document'],
         name: 'Onboard',
     };
 
-    addPurchaseMethod(purchaseMethod);
+    addPurchaseMethod(purchaseMethod, isCapped);
 
     // Click on edit and back button
     getElementByClass('card').eq(0).contains('Edit').click();
@@ -38,15 +40,21 @@ export const createEditPurchaseMethod = (): void => {
     const purchaseMethodCard = getElementByClass('card').eq(0);
     purchaseMethodCard.should('include.text', purchaseMethod.name);
     purchaseMethodCard.should('include.text', 'Purchase locations: On board');
-    purchaseMethodCard.should('include.text', 'Payment methods: Cash, Debit card');
-    purchaseMethodCard.should('include.text', 'Ticket formats: Digital');
+    isCapped
+        ? purchaseMethodCard.should('include.text', 'Payment methods: Debit card, Credit card')
+        : purchaseMethodCard.should('include.text', 'Payment methods: Cash, Debit card');
+    isCapped
+        ? purchaseMethodCard.should('include.text', 'Ticket formats: Mobile app')
+        : purchaseMethodCard.should('include.text', 'Ticket formats: Digital');
 
     purchaseMethodCard.contains('Edit').click();
 
     const editedPurchaseMethod = {
-        purchaseLocations: ['checkbox-0-on-board', 'checkbox-1-online'],
-        paymentMethods: ['checkbox-2-credit-card'],
-        ticketFormats: ['checkbox-0-paper-ticket'],
+        purchaseLocations: isCapped
+            ? ['checkbox-0-on-board', 'checkbox-1-mobile-device']
+            : ['checkbox-0-on-board', 'checkbox-1-online'],
+        paymentMethods: isCapped ? ['checkbox-2-mobile-phone'] : ['checkbox-2-credit-card'],
+        ticketFormats: isCapped ? ['checkbox-1-smart-card'] : ['checkbox-0-paper-ticket'],
         name: 'Online',
     };
 
@@ -56,7 +64,13 @@ export const createEditPurchaseMethod = (): void => {
 
     const editedCard = getElementByClass('card').eq(0);
     editedCard.should('include.text', editedPurchaseMethod.name);
-    editedCard.should('include.text', 'Purchase locations: Online');
-    editedCard.should('include.text', 'Payment methods: Cash, Debit card, Credit card');
-    editedCard.should('include.text', 'Ticket formats: Paper ticket, Digital');
+    isCapped
+        ? editedCard.should('include.text', 'Purchase locations: Mobile device')
+        : editedCard.should('include.text', 'Purchase locations: Online');
+    isCapped
+        ? editedCard.should('include.text', 'Payment methods: Debit card, Credit card, Mobile phone')
+        : editedCard.should('include.text', 'Payment methods: Cash, Debit card, Credit card');
+    isCapped
+        ? editedCard.should('include.text', 'Ticket formats: Mobile app, Smart card')
+        : editedCard.should('include.text', 'Ticket formats: Paper ticket, Digital');
 };
