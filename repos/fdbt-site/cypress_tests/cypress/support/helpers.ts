fd16da62b2cfc16cc9e7c911921e4cb4164dd0ed
Alaina Jaffer <59929701+alainajaffer@users.noreply.github.com>
2023-01-04 13:05:04 +0000

    fix: Work around clear cookies issue (#722)

--- repos/fdbt-site/cypress_tests/cypress/support/helpers.ts
+++ repos/fdbt-site/cypress_tests/cypress/support/helpers.ts
@@ -29,10 +29,12 @@ export const clickElementByText = (text: string): Cypress.Chainable<JQuery> => g
 export const getRandomNumber = (min: number, max: number): number => Cypress._.random(min, max);
 
 export const getHomePage = (noc = 'LNUD'): void => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    cy.clearCookies({ domain: null });
     cy.visit(`?disableAuth=${noc}`);
+    cy.url().then((url) => {
+        if (url.includes('?disableAuth')) {
+            clickElementById('start-now-button');
+        }
+    });
 };
 
 export const fareTypeToFareTypeIdMapper = (fareType: FareType): string => `radio-option-${fareType}`;
