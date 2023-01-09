f180df22a7b0db5c9a775e7bf024d60734e2bfa3
Nitish Arora <nitisharora@kpmg.com>
2023-01-09 19:26:48 +0530

    Removed the extra code

--- repos/fdbt-site/src/pages/api/multipleOperators.ts
+++ repos/fdbt-site/src/pages/api/multipleOperators.ts
@@ -1,11 +1,10 @@
 import { NextApiResponse } from 'next';
 import { NextApiRequestWithSession } from '../../interfaces';
 import { redirectToError, redirectTo } from '../../utils/apiUtils';
-import { MULTI_MODAL_ATTRIBUTE, OPERATOR_ATTRIBUTE } from '../../constants/attributes';
+import { OPERATOR_ATTRIBUTE } from '../../constants/attributes';
 import { updateSessionAttribute, regenerateSession } from '../../utils/sessions';
-import { getAllServicesByNocCode } from '../../data/auroradb';
 
-export default async (req: NextApiRequestWithSession, res: NextApiResponse): Promise<void> => {
+export default (req: NextApiRequestWithSession, res: NextApiResponse): void => {
     try {
         if (req.body.operator) {
             const splitOperator = (req.body.operator as string).split('|');
@@ -17,28 +16,6 @@ export default async (req: NextApiRequestWithSession, res: NextApiResponse): Pro
                 name: splitOperator[0],
                 nocCode: noc,
             });
-
-            let uniqueModes: string[] = [];
-            const services = await getAllServicesByNocCode(noc);
-            const hasBodsServices = services.some((service) => service.dataSource && service.dataSource === 'bods');
-            const tndsServices = services.filter((service) => service.dataSource && service.dataSource === 'tnds');
-
-            if (!hasBodsServices && tndsServices.length > 0) {
-                const modes = tndsServices
-                    .map((service) => {
-                        return service.mode ? service.mode : '';
-                    })
-                    .filter((mode) => mode !== '');
-
-                uniqueModes = Array.from(new Set(modes));
-            }
-
-            updateSessionAttribute(
-                req,
-                MULTI_MODAL_ATTRIBUTE,
-                uniqueModes.length > 0 ? { modes: uniqueModes } : undefined,
-            );
-
             redirectTo(res, '/home');
         } else {
             updateSessionAttribute(req, OPERATOR_ATTRIBUTE, {
--- repos/fdbt-site/tests/pages/api/multipleOperators.test.ts
+++ repos/fdbt-site/tests/pages/api/multipleOperators.test.ts
@@ -1,64 +1,23 @@
 import { getMockRequestAndResponse } from '../../testData/mockData';
 import multipleOperators from '../../../src/pages/api/multipleOperators';
 import * as sessions from '../../../src/utils/sessions';
-import { MULTI_MODAL_ATTRIBUTE, OPERATOR_ATTRIBUTE } from '../../../src/constants/attributes';
-import * as aurora from '../../../src/data/auroradb';
-
-const multiModalServices = [
-    {
-        id: 11,
-        lineName: '123',
-        lineId: '3h3vb32ik',
-        startDate: '05/02/2020',
-        description: 'IW Bus Service 123',
-        serviceCode: 'NW_05_BLAC_123_1',
-        origin: 'Manchester',
-        destination: 'Leeds',
-        dataSource: 'tnds',
-        mode: 'ferry',
-    },
-    {
-        id: 12,
-        lineName: 'X1',
-        lineId: '3h3vb32ik',
-        startDate: '06/02/2020',
-        description: 'Big Blue Bus Service X1',
-        serviceCode: 'NW_05_BLAC_X1_1',
-        origin: 'Bolton',
-        destination: 'Wigan',
-        dataSource: 'tnds',
-        mode: 'coach',
-    },
-    {
-        id: 13,
-        lineName: 'Infinity Line',
-        lineId: '3h3vb32ik',
-        startDate: '07/02/2020',
-        description: 'This is some kind of bus service',
-        serviceCode: 'WY_13_IWBT_07_1',
-        origin: 'Manchester',
-        destination: 'York',
-        dataSource: 'tnds',
-        mode: 'tram',
-    },
-];
+import { OPERATOR_ATTRIBUTE } from '../../../src/constants/attributes';
 
 describe('multipleOperators', () => {
     const updateSessionAttributeSpy = jest.spyOn(sessions, 'updateSessionAttribute');
-    const checkForServicesSpy = jest.spyOn(aurora, 'getAllServicesByNocCode');
+
     afterEach(() => {
         jest.resetAllMocks();
     });
 
-    it('should return 302 redirect to /fareType when an operator is provided, and sets operator attribute', async () => {
+    it('should return 302 redirect to /fareType when an operator is provided, and sets operator attribute', () => {
         const { req, res } = getMockRequestAndResponse({
             cookieValues: {},
             body: { operator: 'Infinity Line|TEST' },
             uuid: {},
         });
 
-        checkForServicesSpy.mockResolvedValueOnce([]);
-        await multipleOperators(req, res);
+        multipleOperators(req, res);
 
         expect(updateSessionAttributeSpy).toHaveBeenCalledWith(req, OPERATOR_ATTRIBUTE, {
             name: 'Infinity Line',
@@ -69,36 +28,13 @@ describe('multipleOperators', () => {
         });
     });
 
-    it('should return 302 redirect to /fareType when an operator is provided, and sets operator attribute, multi modal attribute', async () => {
-        const { req, res } = getMockRequestAndResponse({
-            cookieValues: {},
-            body: { operator: 'Infinity Line|TEST' },
-            uuid: {},
-        });
-
-        checkForServicesSpy.mockResolvedValueOnce(multiModalServices);
-        await multipleOperators(req, res);
-
-        expect(updateSessionAttributeSpy).toHaveBeenCalledWith(req, OPERATOR_ATTRIBUTE, {
-            name: 'Infinity Line',
-            nocCode: 'TEST',
-        });
-        expect(updateSessionAttributeSpy).toHaveBeenCalledWith(req, MULTI_MODAL_ATTRIBUTE, {
-            modes: ['ferry', 'coach', 'tram'],
-        });
-
-        expect(res.writeHead).toBeCalledWith(302, {
-            Location: '/home',
-        });
-    });
-
-    it('should return 302 redirect to /multipleOperators when operator not provided', async () => {
+    it('should return 302 redirect to /multipleOperators when operator not provided', () => {
         const { req, res } = getMockRequestAndResponse({
             cookieValues: {},
             body: null,
         });
 
-        await multipleOperators(req, res);
+        multipleOperators(req, res);
 
         expect(updateSessionAttributeSpy).toHaveBeenCalledWith(req, OPERATOR_ATTRIBUTE, {
             errors: [
