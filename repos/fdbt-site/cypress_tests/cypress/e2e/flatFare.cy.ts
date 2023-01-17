929e8a75d666904a917d8bd0226adb3c6f95ea3b
ajaffer1 <alaina.jaffer@kpmg.co.uk>
2023-01-17 15:48:25 +0000

    remove test

--- repos/fdbt-site/cypress_tests/cypress/e2e/flatFare.cy.ts
+++ repos/fdbt-site/cypress_tests/cypress/e2e/flatFare.cy.ts
@@ -26,14 +26,4 @@ describe('The flat fare faretype product journey', () => {
         completeSalesPages();
         isFinished();
     });
-
-    it('completes successfully for pricing by distance', () => {
-        selectFareType('flatFare', false);
-        defineUserTypeAndTimeRestrictions();
-        clickElementById('radio-option-multipleServicesPricedByDistance');
-        continueButtonClick();
-        completeFlatFarePages('Flat Fare Test Product', false, false, false, true);
-        completeSalesPages();
-        isFinished();
-    });
 });
