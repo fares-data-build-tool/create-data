0a05571c32e0a8fb5219cd7606d176fa8159363f
NitishAroraKPMG <112861163+NitishAroraKPMG@users.noreply.github.com>
2023-02-01 20:01:46 +0530

    feat: Add the caps table (#784)
    * Adding new cap table
    
    * Changed type for the sql

--- fdbt-aws/sql/q.caps.sql
+++ fdbt-aws/sql/q.caps.sql
@@ -1,12 +1,13 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 
-DROP TABLE IF EXISTS fareDayEnd;
+DROP TABLE IF EXISTS caps;
 
-CREATE TABLE fareDayEnd(
+CREATE TABLE caps(
     `id` int(11) NOT NULL AUTO_INCREMENT,
-    `nocCode` varchar(255) NOT NULL,
-    `time` char(4) NOT NULL,
-    INDEX idx_nocCode (nocCode),
+    `noc` varchar(255) NOT NULL,
+    `contents` text NOT NULL,
+    `isExpiry` boolean NOT NULL,
+    INDEX idx_noc (noc),
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB CHARACTER SET = utf8;
 
