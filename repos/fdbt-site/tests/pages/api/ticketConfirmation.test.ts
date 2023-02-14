747f493975f1bfc28f33a502c29411aedb29aa89
ajaffer1 <alaina.jaffer@kpmg.co.uk>
2023-02-14 12:59:15 +0000

    fixed tests

--- repos/fdbt-site/tests/pages/api/ticketConfirmation.test.ts
+++ repos/fdbt-site/tests/pages/api/ticketConfirmation.test.ts
@@ -17,24 +17,55 @@ describe('ticketConfirmation', () => {
         jest.resetAllMocks();
     });
 
+    const cap: FromDb<CapInfo> = {
+        cap: {
+            name: 'Cap 1',
+            price: '4',
+            durationAmount: '2',
+            durationUnits: 'hour' as ExpiryUnit,
+        },
+        id: 2,
+    };
+
     it('should return 302 redirect to /selectCaps when the fareType if single and caps exist', async () => {
         const { req, res } = getMockRequestAndResponse({ body: {}, mockWriteHeadFn: writeHeadMock });
         getFareTypeSpy.mockReturnValue('single');
-        const cap: FromDb<CapInfo> = {
-            cap: {
-                name: 'Cap 1',
-                price: '4',
-                durationAmount: '2',
-                durationUnits: 'hour' as ExpiryUnit,
-            },
-            id: 2,
-        };
 
         getCapsSpy.mockResolvedValueOnce([cap]);
         await ticketConfirmation(req, res);
 
         expect(writeHeadMock).toBeCalledWith(302, {
             Location: '/selectCaps',
+        });
+    });
+
+    it('should return 302 redirect to /selectPurchaseMethods when the fareType is single and no cap exists', async () => {
+        const { req, res } = getMockRequestAndResponse({ body: null, mockWriteHeadFn: writeHeadMock });
+        getFareTypeSpy.mockReturnValue('single');
+        getCapsSpy.mockResolvedValueOnce([]);
+        await ticketConfirmation(req, res);
+        expect(writeHeadMock).toBeCalledWith(302, {
+            Location: '/selectPurchaseMethods',
+        });
+    });
+
+    it('should return 302 redirect to /selectPurchaseMethods when the fareType is period and no cap exists', async () => {
+        const { req, res } = getMockRequestAndResponse({ body: null, mockWriteHeadFn: writeHeadMock });
+        getFareTypeSpy.mockReturnValue('period');
+        getCapsSpy.mockResolvedValueOnce([]);
+        await ticketConfirmation(req, res);
+        expect(writeHeadMock).toBeCalledWith(302, {
+            Location: '/selectPurchaseMethods',
+        });
+    });
+
+    it('should return 302 redirect to /selectPurchaseMethods when the fareType is period and caps exists', async () => {
+        const { req, res } = getMockRequestAndResponse({ body: null, mockWriteHeadFn: writeHeadMock });
+        getFareTypeSpy.mockReturnValue('period');
+        getCapsSpy.mockResolvedValueOnce([cap]);
+        await ticketConfirmation(req, res);
+        expect(writeHeadMock).toBeCalledWith(302, {
+            Location: '/selectPurchaseMethods',
         });
     });
 });
