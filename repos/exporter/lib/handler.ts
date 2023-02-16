1409ddcc54e70b2ead5fc861e6d4739935d03150
Rob Catton <111580926+RobCattonKPMG@users.noreply.github.com>
2023-02-16 15:21:20 +0000

    fix: Exporter type checking

--- repos/exporter/lib/handler.ts
+++ repos/exporter/lib/handler.ts
@@ -117,8 +117,12 @@ export const handler: Handler<ExportLambdaBody> = async ({ paths, noc, exportPre
                 }),
             );
 
-            const setFareDayEnd =
-                isBasePeriodTicket(ticketWithIds) && ticketWithIds.products[0].productValidity === 'fareDayEnd';
+            let setFareDayEnd = false;
+            const product = fullProducts[0];
+
+            if ('productValidity' in product) {
+                setFareDayEnd = true;
+            }
 
             /* eslint-disable */
             const baseTicket: BaseTicket | BaseSchemeOperatorTicket = {
