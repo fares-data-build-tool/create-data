b791a2a43352b73151117445837bd79c6a61dc82
Rob Catton <111580926+RobCattonKPMG@users.noreply.github.com>
2023-02-21 10:36:32 +0000

    Update repos/fdbt-site/tests/utils/index.test.ts

--- repos/fdbt-site/tests/utils/index.test.ts
+++ repos/fdbt-site/tests/utils/index.test.ts
@@ -268,7 +268,7 @@ describe('index', () => {
         });
     });
 
-    describe.only('removeDuplicateServices', () => {
+    describe('removeDuplicateServices', () => {
         const mockServiceTypeServices: ServiceType[] = [
             {
                 id: 11,
