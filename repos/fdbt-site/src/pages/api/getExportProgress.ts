ce18400b6cfbe4c0416eff488d7c40d8d78bc8e8
Rob Catton <111580926+RobCattonKPMG@users.noreply.github.com>
2023-03-14 16:38:08 +0000

    fix: stopping users firing 2 exports at once

--- repos/fdbt-site/src/pages/api/getExportProgress.ts
+++ repos/fdbt-site/src/pages/api/getExportProgress.ts
@@ -1,5 +1,5 @@
 import { NextApiResponse } from 'next';
-import { dateIsOverThirtyMinutesAgo, exportHasStarted, getAndValidateNoc, redirectToError } from '../../utils/apiUtils';
+import { dateIsOverThirtyMinutesAgo, getAndValidateNoc, redirectToError } from '../../utils/apiUtils';
 import { NextApiRequestWithSession } from '../../interfaces';
 import {
     checkIfMetaDataExists,
@@ -12,8 +12,6 @@ import {
 import { MATCHING_DATA_BUCKET_NAME, NETEX_BUCKET_NAME } from '../../constants';
 import logger from '../../utils/logger';
 import { difference } from 'lodash';
-import { getSessionAttribute, updateSessionAttribute } from '../../utils/sessions';
-import { SELECT_EXPORTS_ATTRIBUTE } from '../../constants/attributes';
 
 export interface Export {
     name: string;
@@ -24,66 +22,67 @@ export interface Export {
     signedUrl?: string;
 }
 
+export const getAllExports = async (noc: string): Promise<Export[]> => {
+    const exportNames = await getS3Exports(noc);
+
+    const exports: Export[] = await Promise.all(
+        exportNames.map(async (name) => {
+            const prefix = `${noc}/exports/${name}/`;
+
+            logger.info('', {
+                context: 'api.getExportProgress',
+                message: 'Getting counts for matching data and netex',
+                exportName: name,
+            });
+
+            const matchingDataCount = await getS3FolderCount(MATCHING_DATA_BUCKET_NAME, prefix);
+            const netexCount = await getS3FolderCount(NETEX_BUCKET_NAME, prefix);
+
+            let numberOfFilesExpected = matchingDataCount;
+
+            let metadata = undefined;
+            const metaDataExists = await checkIfMetaDataExists(`${noc}/exports/${name}.json`);
+
+            if (metaDataExists) {
+                metadata = await getExportMetaData(`${noc}/exports/${name}.json`);
+                numberOfFilesExpected = metadata.numberOfExpectedNetexFiles;
+            }
+
+            let exportFailed = false;
+            let failedValidationFilenames: string[] = [];
+
+            if (
+                metadata &&
+                dateIsOverThirtyMinutesAgo(new Date(metadata.date)) &&
+                metadata.numberOfExpectedNetexFiles !== netexCount
+            ) {
+                exportFailed = true;
+            }
+
+            if (exportFailed) {
+                const unvalidatedNetexFileNames = await getNetexFileNames(prefix, false);
+                const validatedNetexFileNames = await getNetexFileNames(prefix, true);
+                failedValidationFilenames = difference(unvalidatedNetexFileNames, validatedNetexFileNames);
+            }
+
+            const complete = matchingDataCount === netexCount;
+            const signedUrl = complete ? await retrieveExportZip(noc, name) : undefined;
+
+            return { name, numberOfFilesExpected, netexCount, signedUrl, exportFailed, failedValidationFilenames };
+        }),
+    );
+
+    return exports.reverse();
+};
+
 export default async (req: NextApiRequestWithSession, res: NextApiResponse): Promise<void> => {
     try {
         const noc = getAndValidateNoc(req, res);
 
-        const exportNames = await getS3Exports(noc);
-        const selectExportAttribute = getSessionAttribute(req, SELECT_EXPORTS_ATTRIBUTE);
-
-        if (!!selectExportAttribute && exportHasStarted(selectExportAttribute.exportStarted)) {
-            updateSessionAttribute(req, SELECT_EXPORTS_ATTRIBUTE, undefined);
-        }
-
-        const exports: Export[] = await Promise.all(
-            exportNames.map(async (name) => {
-                const prefix = `${noc}/exports/${name}/`;
-
-                logger.info('', {
-                    context: 'api.getExportProgress',
-                    message: 'Getting counts for matching data and netex',
-                    exportName: name,
-                });
-
-                const matchingDataCount = await getS3FolderCount(MATCHING_DATA_BUCKET_NAME, prefix);
-                const netexCount = await getS3FolderCount(NETEX_BUCKET_NAME, prefix);
-
-                let numberOfFilesExpected = matchingDataCount;
-
-                let metadata = undefined;
-                const metaDataExists = await checkIfMetaDataExists(`${noc}/exports/${name}.json`);
-
-                if (metaDataExists) {
-                    metadata = await getExportMetaData(`${noc}/exports/${name}.json`);
-                    numberOfFilesExpected = metadata.numberOfExpectedNetexFiles;
-                }
-
-                let exportFailed = false;
-                let failedValidationFilenames: string[] = [];
-
-                if (
-                    metadata &&
-                    dateIsOverThirtyMinutesAgo(new Date(metadata.date)) &&
-                    metadata.numberOfExpectedNetexFiles !== netexCount
-                ) {
-                    exportFailed = true;
-                }
-
-                if (exportFailed) {
-                    const unvalidatedNetexFileNames = await getNetexFileNames(prefix, false);
-                    const validatedNetexFileNames = await getNetexFileNames(prefix, true);
-                    failedValidationFilenames = difference(unvalidatedNetexFileNames, validatedNetexFileNames);
-                }
-
-                const complete = matchingDataCount === netexCount;
-                const signedUrl = complete ? await retrieveExportZip(noc, name) : undefined;
-
-                return { name, numberOfFilesExpected, netexCount, signedUrl, exportFailed, failedValidationFilenames };
-            }),
-        );
+        const exports = await getAllExports(noc);
 
         res.status(200).json({
-            exports: exports.reverse(),
+            exports,
         });
     } catch (error) {
         redirectToError(res, 'There was a problem getting the export progress', 'api.getExportProgress', error);
--- repos/fdbt-site/tests/pages/products/exports.test.tsx
+++ repos/fdbt-site/tests/pages/products/exports.test.tsx
@@ -5,12 +5,17 @@ import Exports from '../../../src/pages/products/exports';
 describe('pages', () => {
     describe('exports', () => {
         it('should render correctly without data when operator has no products', () => {
-            const tree = shallow(<Exports csrf={''} operatorHasProducts={false} exportStarted />);
+            const tree = shallow(<Exports csrf={''} operatorHasProducts={false} initialExportIsInProgress={false} />);
             expect(tree).toMatchSnapshot();
         });
 
         it('should render the export button correctly when operator has products', () => {
-            const tree = shallow(<Exports csrf={''} operatorHasProducts={true} exportStarted />);
+            const tree = shallow(<Exports csrf={''} operatorHasProducts={true} initialExportIsInProgress={false} />);
+            expect(tree).toMatchSnapshot();
+        });
+
+        it('should render the export button correctly when operator has products and an export is in progress', () => {
+            const tree = shallow(<Exports csrf={''} operatorHasProducts={true} initialExportIsInProgress />);
             expect(tree).toMatchSnapshot();
         });
     });
