730273a15e88ff566c77f0a73f1941482b0d0c60
ajaffer1 <alaina.jaffer@kpmg.co.uk>
2023-07-03 14:23:15 +0100

    try catch added

--- repos/exporter/lib/referenceData/tableRenameHandler.ts
+++ repos/exporter/lib/referenceData/tableRenameHandler.ts
@@ -5,33 +5,40 @@ import { getConnection, checkReferenceDataImportHasCompleted, deleteAndRenameTab
 import { getSsmValue, putParameter } from '../ssm';
 
 export const handler: Handler<ZipperLambdaBody> = async () => {
-    console.log('triggered reference data tables rename lambda... ');
-
-    const connection = await getConnection();
-
-    let disableRenamer: string | undefined = 'true';
     try {
-        disableRenamer = await getSsmValue('/scheduled/disable-table-renamer');
-    } catch (error) {
-        if (error instanceof Error) {
-            throw new Error(`Failed to get parameter from ssm: ${error.stack || ''}`);
+        console.log('triggered reference data tables rename lambda... ');
+
+        const connection = await getConnection();
+
+        let disableRenamer: string | undefined = 'true';
+        try {
+            disableRenamer = await getSsmValue('/scheduled/disable-table-renamer');
+        } catch (error) {
+            if (error instanceof Error) {
+                throw new Error(`Failed to get parameter from ssm: ${error.stack || ''}`);
+            }
+
+            throw error;
         }
 
-        throw error;
+        if (disableRenamer === 'false') {
+            await checkReferenceDataImportHasCompleted('txcOperatorLine', connection);
+            await checkReferenceDataImportHasCompleted('nocTable', connection);
+            await checkReferenceDataImportHasCompleted('naptanStop', connection);
+
+            await deleteAndRenameTables(connection);
+
+            await connection.end();
+        } else {
+            throw new Error(
+                'The SSM Parameter used to check for errors in the scheduled job has returned TRUE indicating an issue',
+            );
+        }
+        await putParameter('/scheduled/disable-table-renamer', 'false', 'String', true);
+    } catch (e) {
+        if (e instanceof Error) {
+            console.log(e);
+            await putParameter('/scheduled/disable-table-renamer', 'false', 'String', true);
+        }
     }
-
-    if (disableRenamer === 'false') {
-        await checkReferenceDataImportHasCompleted('txcOperatorLine', connection);
-        await checkReferenceDataImportHasCompleted('nocTable', connection);
-        await checkReferenceDataImportHasCompleted('naptanStop', connection);
-
-        await deleteAndRenameTables(connection);
-
-        await connection.end();
-    } else {
-        throw new Error(
-            'The SSM Parameter used to check for errors in the scheduled job has returned TRUE indicating an issue',
-        );
-    }
-    await putParameter('/scheduled/disable-table-renamer', 'false', 'String', true);
 };
