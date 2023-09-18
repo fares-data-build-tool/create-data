3bcc2a4396ee8ba6173a9410fbf1b97803a463f3
Laurence Jones <laurence.jones@doza.consulting>
2023-09-18 10:10:05 +0100

    get admin working (#885)

--- repos/fdbt-admin-old/src/utils/env.ts
+++
@@ -1,5 +0,0 @@
-const getEnvironment = (): string => {
-    return document.location.host.split('.')[1] ?? 'test';
-};
-
-export default getEnvironment;
--- repos/fdbt-admin/src/main.tsx
+++ repos/fdbt-admin/src/main.tsx
@@ -1,16 +1,20 @@
-import ReactDOM from 'react-dom'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom';
+import App from './App.tsx';
+import './index.css';
 import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { StrictMode } from 'react';
+import { Authenticator } from '@aws-amplify/ui-react';
+
+import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(awsconfig);
 
-
 ReactDOM.render(
-  <StrictMode>
-      <App />
-  </StrictMode>,
-  document.getElementById('root'),
+    <StrictMode>
+        <Authenticator hideSignUp>
+            <App />
+        </Authenticator>
+    </StrictMode>,
+    document.getElementById('root'),
 );
