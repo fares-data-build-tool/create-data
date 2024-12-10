a30fa82ce9c19c1a45e9ba1712318d3af1a2c1c1
Nathan Betts <13869955+nbetts@users.noreply.github.com>
2024-12-10 12:12:54 +0000

    fix: dynamo client options

--- repos/fdbt-site/server/middleware/sessions.ts
+++ repos/fdbt-site/server/middleware/sessions.ts
@@ -8,10 +8,7 @@ export default (server: Express): void => {
 
     const options = {
         table: 'sessions',
-        client: new DynamoDBClient({
-            region: 'eu-west-2',
-            ...(process.env.NODE_ENV === 'development' ? { endpoint: 'http://localhost:4569' } : {}),
-        }),
+        client: new DynamoDBClient({ region: 'eu-west-2' }),
     };
 
     const sessionOptions: SessionOptions = {
