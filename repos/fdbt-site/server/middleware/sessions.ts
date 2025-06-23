bdfff10b585d4c62343a88c31bf835db6ab7d830
Deanna Sharma <deanna.sharma@doza.consulting>
2025-06-23 09:06:30 +0100

    use memorystore instead of dynamodb

--- repos/fdbt-site/server/middleware/sessions.ts
+++ repos/fdbt-site/server/middleware/sessions.ts
@@ -1,34 +1,9 @@
-import AWS, { DynamoDB } from 'aws-sdk';
-import connectDynamoDb from 'connect-dynamodb';
 import { Express } from 'express';
-import session, { SessionOptions, Store } from 'express-session';
-
-interface DynamoDbOptions {
-    table: string;
-    AWSConfigJSON: {
-        region: string;
-    };
-    client?: DynamoDB;
-}
+import session, { SessionOptions } from 'express-session';
+import memorystore from 'memorystore';
 
 export default (server: Express): void => {
-    const DynamoDbStore: new (options: object) => Store = connectDynamoDb(session);
-
-    const dynamoDbOptions: DynamoDbOptions = {
-        table: 'sessions',
-        AWSConfigJSON: {
-            region: 'eu-west-2',
-        },
-    };
-
-    if (process.env.NODE_ENV === 'development') {
-        dynamoDbOptions.client = new AWS.DynamoDB({
-            endpoint: 'http://127.0.0.1:4566',
-            region: 'eu-west-2',
-            accessKeyId: 'DUMMY',
-            secretAccessKey: 'DUMMY',
-        });
-    }
+    const MemoryStore = memorystore(session);
 
     const sessionOptions: SessionOptions = {
         cookie: {
@@ -39,7 +14,9 @@ export default (server: Express): void => {
         saveUninitialized: false,
         resave: false,
         secret: process.env.SESSION_SECRET || 'secret',
-        store: new DynamoDbStore(dynamoDbOptions),
+        store: new MemoryStore({
+            checkPeriod: 86400000,
+        }),
     };
 
     server.use(session(sessionOptions));
